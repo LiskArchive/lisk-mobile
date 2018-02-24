49b332365db898a097ba1c6106bb7cecca243c03
reyraa <ali@lightcurve.io>
2018-02-24 21:41:22 +0100

    Simplify the actions for initialization

--- src/actions/peers.js
+++ src/actions/peers.js
@@ -20,41 +20,10 @@ const peerSet = (data, config) => ({
  * @param {Object} data - Active peer data and the passphrase of account
  * @returns {Object} Action object
  */
-export const activePeerSet = data =>
-  (dispatch) => {
-    const addHttp = (url) => {
-      const reg = /^(?:f|ht)tps?:\/\//i;
-      return reg.test(url) ? url : `http://${url}`;
-    };
-    const config = data.network || {};
-    if (config.code === networks.mainnet.code) {
-      config.node = getMainNetNode();
-    }
-
-
-    if (config.address) {
-      const { hostname, port, protocol } = new URL(addHttp(config.address));
-
-      config.node = hostname;
-      config.ssl = protocol === 'https:';
-      config.port = port || (config.ssl ? 443 : 80);
-    }
-    if (config.testnet === undefined && config.port !== undefined) {
-      config.testnet = config.port === '7000';
-    }
-    if (config.custom) {
-      getNetHash(Lisk.api(config)).then((response) => {
-        config.testnet = response.nethash === netHashes.testnet;
-        if (!config.testnet && response.nethash !== netHashes.mainnet) {
-          config.nethash = response.nethash;
-        }
-        dispatch(peerSet(data, config));
-      }).catch(() => {
-      });
-    } else {
-      dispatch(peerSet(data, config));
-    }
-  };
+export const activePeerSet = data => ({
+  data,
+  type: actionTypes.activePeerSet,
+});
 
 
 /**
@@ -64,7 +33,7 @@ export const activePeerSet = data =>
  * @param {Object} data - Active peer data
  * @returns {Object} Action object
  */
-export const activePeerUpdate = data => ({
+export const activePeerUpdated = data => ({
   data,
-  type: actionTypes.activePeerUpdate,
+  type: actionTypes.activePeerUpdated,
 });
