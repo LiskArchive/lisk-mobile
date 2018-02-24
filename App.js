6690a4019739ae76a5a1b9eec2cd1b5cc8599496
reyraa <ali@lightcurve.io>
2018-02-24 21:20:04 +0100

    Add Redux provider

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Transactions from './src/components/transactions';
+import { Provider } from 'react-redux';
+import store from './src/store/index';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Transactions />
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <Transactions />
+        </View>
+      </Provider>
     );
   }
 }
