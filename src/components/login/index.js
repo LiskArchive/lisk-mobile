1e812e624d68f96b526a7f4d61b88ab305664ff4
reyraa <ali@lightcurve.io>
2018-02-24 17:43:07 +0100

    Create Login component
    Signed-off-by: reyraa <ali@lightcurve.io>

---
+++ src/components/login/index.js
@@ -0,0 +1,23 @@
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+import Login from './login';
+import { activePeerSet } from '../../actions/peers';
+import { accountSaved } from '../../actions/accounts';
+
+/**
+ * Using react-redux connect to pass state and dispatch to Login
+ */
+const mapStateToProps = state => ({
+  peers: state.peers,
+  accounts: state.accounts,
+});
+
+const mapDispatchToProps = dispatch => ({
+  activePeerSet: data => dispatch(activePeerSet(data)),
+  accountSaved: () => dispatch(accountSaved()),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(withRouter(Login));
---
+++ src/components/login/login.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Logon = () =>
+  <Text>Logon</Text>;
+
+export default Logon;
