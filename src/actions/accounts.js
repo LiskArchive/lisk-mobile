e40f82234d9440f06dec8a6b12bb6a839e0e26e4
reyraa <ali@lightcurve.io>
2018-03-01 09:01:56 +0100

    Create accountRetrieved and AccountStored actions

--- src/actions/accounts.js
+++ src/actions/accounts.js
@@ -1,1 +1,15 @@
-export const accountSaved = () => {};
+import actionTypes from "../constants/actions";
+import { retrieveAccounts, storeAccounts } from '../utilities/storage';
+
+export const accountsStored = () => ({
+  type: actionTypes.accountsStored,
+  data: { list: [], active: -1 },
+});
+
+export const accountsRetrieved = () =>
+  (dispatch) => {
+    retrieveAccounts()
+      .then((accounts) => {
+        dispatch(storeAccounts(accounts));
+      });
+  }
