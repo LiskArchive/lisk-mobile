78f2d64daceaf7d82bf7bcc416d12eefdf166325
yasharAyari <yashar@lightcurve.io>
2018-03-11 02:56:43 +0100

    Remove styleSheet form App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StyleSheet, Text, View } from 'react-native';
 import Landing from './src/components/landing';
 import { Provider } from 'react-redux';
 import store from './src/store/index';
@@ -8,19 +8,17 @@ export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <View style={styles.container}>
-          <Landing />
-        </View>
+        <Landing />
       </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
