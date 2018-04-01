4694811132a549636f28f1812db2a45e0f1d8c2b
reyraa <ali@lightcurve.io>
2018-04-01 16:04:18 +0200

    Improve the style of Login component

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,21 +1,114 @@
-import { connect } from 'react-redux';
-import Login from './login';
+import React from 'react';
+import connect from 'redux-connect-decorator';
+import { View } from 'react-native';
+import { Button, FormLabel,
+  FormInput, FormValidationMessage } from 'react-native-elements'
 import { accountSaved, accountLoggedIn } from '../../actions/accounts';
+import styles from './styles';
+import { validatePassphrase } from '../../utilities/passphrase';
+import Logo from '../logo';
 
-/**
- * Using react-redux connect to pass state and dispatch to Login
- */
-const mapStateToProps = state => ({
+@connect(state => ({
   peers: state.peers,
   accounts: state.accounts,
-});
+}), {
+  accountLoggedIn,
+  accountSaved,
+})
 
-const mapDispatchToProps = dispatch => ({
-  accountLoggedIn: data => dispatch(accountLoggedIn(data)),
-  accountSaved: () => dispatch(accountSaved()),
-});
+/**
+ * The container component containing login and create account functionality
+ * 
+ * @todo 
+ */
+class Login extends React.Component {
+  constructor() {
+    super();
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(Login);
+    this.state = {
+      passphrase: {
+        value: '',
+        validity: validatePassphrase(''),
+      },
+      messages: [
+        '',
+        'No non-mnemonic or duplicated words',
+        'Passphrase is short',
+      ],
+    };
+  }
+
+  componentDidUpdate() {
+    if (this.props.accounts.list.length > 0) {
+      this.props.navigation.navigate('Main');
+    }
+  }
+
+  trim(passphrase) {
+    return passphrase.trim().replace(/\s+/g, ' ');
+  }
+
+  /**
+   * Will be called when login form submits
+   * fires the activePeerSet action
+   * 
+   * @param {String} passphrase - valid mnemonic passphrase
+   */
+  onLoginSubmission(passphrase) {
+    console.log('onLoginSubmission',passphrase.validity);
+    if (passphrase.validity !== 0 && passphrase.validity !== 3) {
+      this.passphraseInput.shake()
+    } else {
+      console.log('logging in');
+      this.props.accountLoggedIn({
+        // passphrase: this.trim(passphrase.value),
+        passphrase: 'reopen parrot fever infant general life surface this frame village color position',
+      });
+    }
+  }
+
+  /**
+   * General change handler to get bound to react component event listeners
+   * 
+   * @param {String} key - The key in react component state to be altered
+   * @param {any} value - The corresponding value. interface depends on the key
+   * 
+   * @todo Implement error status/message
+   */
+  changeHandler(key, value) {
+    this.setState({
+      [key]: {
+        value,
+        validity: validatePassphrase(value),
+      },
+    });
+  }
+
+  render() {
+    const { passphrase, messages } = this.state;
+    return (<View style={styles.content}>
+      <View style={styles.container}>
+        <Logo />
+        <FormLabel>Passphrase</FormLabel>
+        <FormInput
+          ref={ref => this.passphraseInput = ref}
+          value={passphrase.value}
+          onChangeText={this.changeHandler.bind(this, 'passphrase')}/>
+        <FormValidationMessage labelStyle={styles.errorMessage}>
+          {
+            (passphrase.validity === 1 ||
+            passphrase.validity === 2) ?
+            messages[passphrase.validity] : ''
+          }
+        </FormValidationMessage>
+        <Button
+          style={styles.button}
+          backgroundColor='#ff6236'
+          onPress={this.onLoginSubmission.bind(this, passphrase)}
+          title="Login"/>
+      </View>
+    </View>);
+  }
+}
+
+export default Login;
