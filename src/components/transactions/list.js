4c842b09c60c7b314d140faa9df569a3c63c7286
yasharAyari <yashar@lightcurve.io>
2018-04-02 18:03:56 +0200

    Add icon to transaction list

--- src/components/transactions/list.js
+++ src/components/transactions/list.js
@@ -1,13 +1,13 @@
 import React, { Fragment } from 'react';
-import { List } from 'react-native-elements'
+import { Lis, ListView } from 'react-native-elements'
 import Item from './item';
 
 export default ({ transactions, account, navigation, pending }) =>
-  (<List>
+  (<List containerStyle={{marginTop: 0}}>
     {
-      pending.map(tx => <Item navigation={navigation} account={account} tx={tx} />)
+      pending.map(tx => <Item navigation={navigation} account={account} key={tx} tx={tx} />)
     }
     {
-      transactions.map(tx => <Item navigation={navigation} account={account} tx={tx} />)
+      transactions.map(tx => <Item navigation={navigation} account={account} key={tx.id} tx={tx} />)
     }
   </List>);
