70d2ca6f2ccb8297bc70c8b03d7179da994b7599
yasharAyari <yashar@lightcurve.io>
2018-04-02 18:04:36 +0200

    Improve style of transaction detail page

--- src/components/txDetail/index.js
+++ src/components/txDetail/index.js
@@ -1,17 +1,25 @@
 import React, { Fragment } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Divider } from 'react-native-elements';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#FFFFFF'
   },
   title: {
     // alignItems: 'left',
     fontWeight: 'bold',
     fontSize: 16,
+    padding: 20
   },
   value: {
-    // alignItems: 'right',
+    paddingLeft: 20
+  },
+  divider: {
+    margin: 20,
+    marginBottom: 0,
+    backgroundColor: '#ccc'
   }
 });
 
@@ -19,13 +27,16 @@ export default ({ navigation }) => {
   const tx = navigation.getParam('tx', null);
 
   return (<View style={styles.container}>
-    <Text h4 style={styles.title}>Transaction ID</Text>
+    <Text h4 style={styles.title}>Transaction ID :</Text>
     <Text style={styles.value}>{ tx.id }</Text>
-    <Text h4 style={styles.title}>Sender</Text>
+    <Divider style={styles.divider} />
+    <Text h4 style={styles.title}>Sender :</Text>
     <Text style={styles.value}>{ tx.senderId }</Text>
-    <Text h4 style={styles.title}>Recipient</Text>
+    <Divider style={styles.divider} />
+    <Text h4 style={styles.title}>Recipient :</Text>
     <Text style={styles.value}>{ tx.recipientId }</Text>
-    <Text h4 style={styles.title}>Amount</Text>
+    <Divider style={styles.divider} />
+    <Text h4 style={styles.title}>Amount :</Text>
     <Text style={styles.value}>{ tx.amount }</Text>
   </View>);
 }
