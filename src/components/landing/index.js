0de1ce1257c9a9518e6bf3f8d6a1cedb8b49d68b
reyraa <ali@lightcurve.io>
2018-04-02 17:56:08 +0200

    Use connect decorator

--- src/components/landing/index.js
+++ src/components/landing/index.js
@@ -1,22 +1,65 @@
-import { connect } from 'react-redux';
-import Landing from './landing';
+import React from 'react';
+import connect from 'redux-connect-decorator';
+import { Text, View } from 'react-native';
+import BackgroundImage from '../background';
+import Logo from '../logo';
+import { getNetwork, networks } from '../../utilities/networks';
+import Router from '../router';
 import { activePeerSet } from '../../actions/peers';
 import { accountsRetrieved } from '../../actions/accounts';
 
-/**
- * Using react-redux connect to pass state and dispatch to Login
- */
-const mapStateToProps = state => ({
+@connect(state => ({
   peers: state.peers,
   accounts: state.accounts,
-});
+}), {
+  activePeerSet,
+  accountsRetrieved,
+})
 
-const mapDispatchToProps = dispatch => ({
-  activePeerSet: data => dispatch(activePeerSet(data)),
-  accountsRetrieved: () => dispatch(accountsRetrieved()),
-});
+/**
+ * This component would be mounted first and would be used to config and redirect
+ * the application to referer page or Login
+ *
+ * @todo Implement saved language detection
+ * @todo Implement release notification
+ * @todo Implement custom message: this can be used in case we need to notify the user
+ * about any unforeseen issue/change
+ */
+class Landing extends React.Component {
+  constructor() {
+    super();
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(Landing);
+    this.state = {
+      address: '',
+      network: networks.mainNet.code,
+    };
+  }
+
+  componentWillMount() {
+    this.network = getNetwork(this.state.network);
+    this.props.accountsRetrieved();
+  }
+
+  componentDidUpdate() {
+    if (this.props.accounts.list.length > 0) {
+      console.log('Redirect to Referrer or Transactions page');
+    } else {
+      console.log('Redirect to Login page');
+      this.props.navigation.navigate('Login');
+    }
+  }
+
+  render() {
+    // ToDo : this Text need to be replaced by a snipper component
+    return (<View style={{
+        flex: 1,
+        backgroundColor: '#666',
+      }}>
+      <BackgroundImage />
+      <Logo />
+      <Text>Landing...</Text>
+    </View>);
+  }
+}
+
+export default Landing;
