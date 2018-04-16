52790ab0e44420e3102e4e40ab2de113ec222d91
reyraa <ali@lightcurve.io>
2018-04-16 19:40:23 +0200

    Move the styles of TxDetails component to dedicated file

--- src/components/txDetail/index.js
+++ src/components/txDetail/index.js
@@ -1,28 +1,8 @@
 import React, { Fragment } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text } from 'react-native';
 import { Divider } from 'react-native-elements';
 import FormattedDate from '../formattedDate';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFFFFF'
-  },
-  title: {
-    // alignItems: 'left',
-    fontWeight: 'bold',
-    fontSize: 16,
-    padding: 20
-  },
-  value: {
-    paddingLeft: 20
-  },
-  divider: {
-    margin: 20,
-    marginBottom: 0,
-    backgroundColor: '#ccc'
-  }
-});
+import styles from './styles';
 
 export default ({ navigation }) => {
   const tx = navigation.getParam('tx', null);
---
+++ src/components/txDetail/styles.js
@@ -0,0 +1,23 @@
+import {  StyleSheet } from 'react-native';
+
+const styles = {
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF'
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    padding: 20
+  },
+  value: {
+    paddingLeft: 20
+  },
+  divider: {
+    margin: 20,
+    marginBottom: 0,
+    backgroundColor: '#ccc'
+  }
+};
+
+export default StyleSheet.create(styles);
