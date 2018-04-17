32074fb05d5f57cc8f6ac63b7fb753e140f7c4fc
reyraa <ali@lightcurve.io>
2018-04-17 20:41:59 +0200

    Brushing accountSummary

--- src/components/accountSummary/index.js
+++ src/components/accountSummary/index.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import { View, Text } from 'react-native';
-import { Divider } from 'react-native-elements';
+import { PricingCard } from 'react-native-elements';
+import Avatar from '../avatar';
 import FormattedDate from '../formattedDate';
 import { fromRawLsk } from '../../utilities/conversions';
 import styles from './styles';
@@ -8,12 +9,15 @@ import styles from './styles';
 export default ({ account }) => (<View>
     {
       account ?
-      <Fragment>
-        <Text h4>Lisk ID :</Text>
-        <Text>{ account.address }</Text>
-        <Text h4>Balance :</Text>
-        <Text>{ fromRawLsk(account.balance) }</Text>
-      </Fragment> :
+      <View style={styles.container}>
+        <Avatar address={account.address}/>
+        <Text style={styles.address}>{ account.address }</Text>
+        <Text style={styles.title}>Balance :</Text>
+        <Text style={styles.balance}>
+          <Text style={styles.value}>{ fromRawLsk(account.balance) }</Text>
+          <Text style={styles.unit}>LSK</Text>
+        </Text>
+      </View> :
       <Text h4>Fetching account info</Text>
     }
   </View>);
--- src/components/accountSummary/styles.js
+++ src/components/accountSummary/styles.js
@@ -1,5 +1,37 @@
 import { StyleSheet } from 'react-native';
 
-const styles = {};
+const styles = {
+  container: {
+    backgroundColor: '#fff',
+    width: '94%',
+    marginTop: 10,
+    marginBottom: 10,
+    marginRight: '3%',
+    marginLeft: '3%',
+    padding: 20,
+  },
+  balance: {
+    textAlign: 'center',
+  },
+  unit: {
+    fontSize: 16,
+    color: '#000',
+  },
+  value: {
+    fontSize: 28,
+    color: '#000',
+  },
+  address: {
+    fontSize: 20,
+    color: '#ff6236',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 12,
+    color: '#666',
+    textAlign: 'center',
+  }
+};
 
 export default StyleSheet.create(styles);
