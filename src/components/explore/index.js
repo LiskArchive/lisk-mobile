ae3fe7b350784eead3ecd7598ff1e76f8fb19288
reyraa <ali@lightcurve.io>
2018-04-20 13:03:07 +0200

    Show the list of followed accounts in Explorer

--- src/components/explore/index.js
+++ src/components/explore/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import Search from '../search';
+import FollowedAccounts from '../followedAccounts';
 
 const Explore = ({ navigation }) =>
   <View>
     <Search navigation={navigation} />
-    {/* FollowedAccounts */}
+    <FollowedAccounts navigation={navigation} />
   </View>
 
 export default Explore;
