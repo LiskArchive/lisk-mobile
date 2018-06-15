c5614ce81648f6becec2470e6423b5a60ace5cbb
reyraa <ali@lightcurve.io>
2018-06-15 16:46:10 +0200

    Remove unknown props from SvgUrl component

--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { View } from 'react-native';
 import SvgUri from 'react-native-svg-uri';
 import logoImage from '../../assets/images/lisk.svg';
 import styles from './styles';
 
 const Logo = () =>
-  <SvgUri
-    style={styles.image}
-    width='128'
-    height='128'
-    source={logoImage}
-  />;
+  <View style={styles.image}>
+    <SvgUri
+      width='128'
+      height='128'
+      source={logoImage}
+    />
+  </View>;
 
 export default Logo;
