baa80228aa54af73312e19b16a4a956d6c8a416f
reyraa <ali@lightcurve.io>
2018-06-21 08:38:28 +0200

    Update constants

---
+++ src/constants/styleGuide/boxes.js
@@ -0,0 +1,13 @@
+export default {
+  // heights
+  buttonHeight: 36,
+  borderedButtonHeight: 36,
+  // paddings
+  boxPadding: 20,
+  elementPadding: 10,
+  // borders
+  buttonBorderWidth: 1,
+  // border radius
+  buttonBorderRadius: 2,
+  boxBorderRadius: 10,
+};
