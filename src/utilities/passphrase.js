1c2441214a3f2dcfaa4305a9389f91cd4a0e8ab2
reyraa <ali@lightcurve.io>
2018-06-22 17:03:55 +0200

    Use Lisk Elements to validate the passphrase

--- src/utilities/passphrase.js
+++ src/utilities/passphrase.js
@@ -1,65 +1,27 @@
-/**
- * @todo Import mnemonic and remove this mock
- */
-const mnemonic = word => [
-  'ali',
-  'angela',
-  'hamid',
-  'tania',
-  'john',
-  'jenny',
-  'tim',
-  'sara',
-  'mike',
-  'eve',
-  'simon',
-  'ashley',
-  'marcel',
-  'rose',
-  'luis'].includes(word);
+import Lisk from 'lisk-elements';
 
 /**
  * Checks validity of passphrase using to mnemonic
  *
  * detects validity of each word individually
- * the resulting array is a number instead of each word indicating its validity status
- * 0 -> valid
- * 1 -> not a mnemonic word
- * 2 -> duplicated
+ * the result consist of [multiple] object[s]
+ * which have a member with a 'code' key whose value
+ * is one of the below
+ * INVALID_AMOUNT_OF_WORDS
+ * INVALID_AMOUNT_OF_WHITESPACES
+ * INVALID_AMOUNT_OF_UPPERCASE_CHARACTER
+ * INVALID_MNEMONIC
  *
- * over status:
- * valid {Number}
- * 0 -> valid
- * 1 -> short
- * 2 -> has invalid words
+ * and a message which indicates the issue
+ * in a more human readable fashion
  *
  * @param {string} passphrase
- * @returns {Object} the validation result containing:
- *  {Number[]} words - The v. status of each word
- *  {Boolean} valid - The v. status of the given passphrase
+ * @returns {Array} the validation result containing object[s] with:
+ *  {String} code - specified the possible codes in the description above
+ *  {String} message - A descriptive message for what went wrong
  */
 // eslint-disable-next-line import/prefer-default-export
 export const validatePassphrase = (passphrase) => {
-  const rawWords = passphrase.trim().replace(/\s+/g, ' ').split(' ').filter(item => item !== '');
-  if (rawWords.length === 0) {
-    return 3; // empty string
-  }
-  const words = new Array(rawWords.length);
-
-  const wordsStatus = words.map((word) => {
-    if (!mnemonic(word)) {
-      return 1; // not a mnemonic word
-    } else if (words.indexOf(word) !== words.lastIndexOf(word)) {
-      return 2; // duplicated
-    }
-    return 0; // valid and unique
-  });
-
-  let valid = wordsStatus.reduce((acc, item) => acc + item, 0);
-  if (valid > 0) {
-    valid = 2;
-  } else if (valid === 0 && words.length < 12) {
-    valid = 1;
-  }
-  return valid;
+  if (passphrase.trim().length === 0) return [{ code: 'empty_value' }];
+  return Lisk.passphrase.validation.getPassphraseValidationErrors(passphrase);
 };
