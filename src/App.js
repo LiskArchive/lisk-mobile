1e52ef8413033bfa32d7cb668465ecc0d196ba66
reyraa <ali@lightcurve.io>
2018-07-03 17:59:11 +0200

    Mount Landing in App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Provider } from 'react-redux';
 import Router from './components/router';
+import Loading from './components/loading';
 import store from './store/index';
 
 const App = () => (
   <Provider store={store}>
-    <Router />
+    <Fragment>
+      <Router />
+      <Loading />
+    </Fragment>
   </Provider>
 );
 
