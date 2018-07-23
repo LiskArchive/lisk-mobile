265f0b82fc80b9ab6b7df0258b603f9d9036e05a
reyraa <ali@lightcurve.io>
2018-07-23 12:19:02 +0200

    Remove extra spaces

--- src/components/ownWallet/styles.js
+++ src/components/ownWallet/styles.js
@@ -4,13 +4,10 @@ import styleGuide from '../../constants/styleGuide';
 const styles = {
   container: {
     backgroundColor: styleGuide.colors.white,
-    paddingBottom: 70,
     flex: 1,
     alignContent: 'space-around',
   },
   scrollView: {
-    marginTop: -70,
-    paddingTop: 70,
   },
   accountSummary: {
     zIndex: 2,
