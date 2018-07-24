b49c4975290b2ce2b73a66ed16ea75ef5d964989
reyraa <ali@lightcurve.io>
2018-07-24 10:36:17 +0200

    Remove unused Icon component

--- src/components/txDetail/index.js
+++ src/components/txDetail/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { View } from 'react-native';
-import Icon from '../toolBox/icon';
 import FormattedDate from '../formattedDate';
 import { fromRawLsk } from '../../utilities/conversions';
 import FormattedNumber from '../formattedNumber';
