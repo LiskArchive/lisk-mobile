595a0209854593aa5662ebfef43927d155ec19a6
reyraa <ali@lightcurve.io>
2018-07-25 16:25:41 +0200

    Update the toolbox Icon componet to use new fonts

--- src/components/toolBox/icon.js
+++ src/components/toolBox/icon.js
@@ -1,44 +1,23 @@
 import React from 'react';
-import { Icon as NativeIcon } from 'react-native-elements';
-import SvgUri from 'react-native-svg-uri';
+import { createIconSetFromIcoMoon } from 'react-native-vector-icons';
+import iconsConfig from '../../assets/fonts/icons/selection.json';
+
+const Icomoon = createIconSetFromIcoMoon(iconsConfig);
 
 /**
- * Creates an Icon element, normalizing the property names to match
- * the required properties of SvgUri or Icon components.
- * If name is passed as string, this works as a thematic wrapper over
- * the Icon component of react native elements
- * and If the src is passed as svg content, it exports an SvgUri component
+ * Creates icon from Icomoon selection
+ * in order to show an icon, the icon should exist in the font file
  *
  * @param {Object} props
- * @param {String} props.tintColor - A valid Hex color code
+ * @param {String} props.color - A valid Hex color code
  * @param {String} props.name - An icon name existing in our icons list
  * @param {Number} props.size - THe size of the icon in pixels, defaults to 35
- * @param {Object} props.src - SVG image asset
+ * @param {Object} props.style - styles from stylesheet
+ * @param {Function} props.onPress - onPress event handler function
  */
 const Icon = ({
-  tintColor, name, size, style, onPress, src,
-}) => {
-  let Element = null;
-  let props = null;
-  if (typeof name === 'string') {
-    Element = NativeIcon;
-    props = {
-      name,
-      size: (size || 35),
-      color: tintColor,
-      iconStyle: style,
-    };
-  } else {
-    Element = SvgUri;
-    props = {
-      width: (size || 35),
-      height: (size || 35),
-      source: src,
-      style,
-    };
-  }
-
-  return (<Element {...props} onPress={onPress} />);
-};
+  name, size, color, style, onPress,
+}) =>
+  <Icomoon name={name} size={size || 35} color={color} style={style} onPress={onPress} />;
 
 export default Icon;
