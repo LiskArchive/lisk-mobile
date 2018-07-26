1fd3c5fbd40a7c21b8db5147b633a55ac3efcb66
reyraa <ali@lightcurve.io>
2018-07-26 14:27:54 +0200

    Add Lottie to package, Link it to project and update the Android sdk to work with it

--- android/app/src/main/java/com/liskmobile/MainApplication.java
+++ android/app/src/main/java/com/liskmobile/MainApplication.java
@@ -3,6 +3,7 @@ package com.liskmobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.lottie.LottiePackage;
 import com.ocetnik.timer.BackgroundTimerPackage;
 import com.horcrux.svg.SvgPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -31,6 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LottiePackage(),
             new BackgroundTimerPackage(),
             new SvgPackage(),
             new VectorIconsPackage(),
