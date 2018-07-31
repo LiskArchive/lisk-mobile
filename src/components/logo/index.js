4efec0f238f98c4e27b3f2db975728fcf3fef1f5
reyraa <ali@lightcurve.io>
2018-07-31 12:32:18 +0200

    Use Icon font for rendering the Logo

--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { View } from 'react-native';
-import SvgUri from 'react-native-svg-uri';
-import logoImage from '../../assets/images/lisk.svg';
 import styles from './styles';
+import Icon from '../toolBox/icon';
+import colors from '../../constants/styleGuide/colors';
 
-const Logo = () =>
+const Logo = ({ color }) =>
   <View style={styles.image}>
-    <SvgUri
-      width='128'
-      height='128'
-      source={logoImage}
-    />
+    <Icon name='lisk' color={color || colors.black} size={100} />
   </View>;
 
 export default Logo;
