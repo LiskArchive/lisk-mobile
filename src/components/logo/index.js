e4c5fe6d81a55e0085224b99e78c3d3ec665c2ac
reyraa <ali@lightcurve.io>
2018-08-01 14:15:13 +0200

    Add size property to logo

--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -4,9 +4,9 @@ import styles from './styles';
 import Icon from '../toolBox/icon';
 import colors from '../../constants/styleGuide/colors';
 
-const Logo = ({ color }) =>
+const Logo = ({ color, size }) =>
   <View style={styles.image}>
-    <Icon name='lisk' color={color || colors.black} size={100} />
+    <Icon name='lisk' color={color || colors.black} size={size || 100} />
   </View>;
 
 export default Logo;
