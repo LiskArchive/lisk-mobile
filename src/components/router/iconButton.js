e670604d4661c8d3854e2c7ab0e7515258572ce0
reyraa <ali@lightcurve.io>
2018-08-08 17:21:41 +0200

    Add custom button to login header

---
+++ src/components/router/iconButton.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+import { TouchableHighlight, Text } from 'react-native';
+import Icon from '../toolBox/icon';
+import styles from './styles';
+
+const IconButton = ({
+  icon, title, target, color, navigation,
+}) =>
+  <TouchableHighlight onPress={() => navigation.replace(target)} style={styles.iconButton}>
+    <Fragment>
+      <Icon name={icon} size={24} color={color} />
+      <Text style={styles.iconButtonTitle}>{ title }</Text>
+    </Fragment>
+  </TouchableHighlight>;
+
+export default IconButton;
