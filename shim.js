8d763907f9de3467e2840b32df00a33c4123c128
reyraa <ali@lightcurve.io>
2018-08-08 12:32:52 +0200

    Improve the code styling

--- shim.js
+++ shim.js
@@ -2,20 +2,20 @@ import BackgroundTimer from 'react-native-background-timer';
 import { Buffer } from 'buffer';
 const env = require('./env.json');
 
-global.setTimeout = BackgroundTimer.setTimeout.bind(BackgroundTimer)
-global.setInterval = BackgroundTimer.setInterval.bind(BackgroundTimer)
-global.clearTimeout = BackgroundTimer.clearTimeout.bind(BackgroundTimer)
-global.clearInterval = BackgroundTimer.clearInterval.bind(BackgroundTimer)
+global.setTimeout = BackgroundTimer.setTimeout.bind(BackgroundTimer);
+global.setInterval = BackgroundTimer.setInterval.bind(BackgroundTimer);
+global.clearTimeout = BackgroundTimer.clearTimeout.bind(BackgroundTimer);
+global.clearInterval = BackgroundTimer.clearInterval.bind(BackgroundTimer);
 
-if (typeof __dirname === 'undefined') global.__dirname = '/'
-if (typeof __filename === 'undefined') global.__filename = ''
+if (typeof __dirname === 'undefined') global.__dirname = '/';
+if (typeof __filename === 'undefined') global.__filename = '';
 if (typeof process === 'undefined') {
-  global.process = require('process')
+  global.process = require('process');
 } else {
-  const bProcess = require('process')
+  const bProcess = require('process');
   for (var p in bProcess) {
     if (!(p in process)) {
-      process[p] = bProcess[p]
+      process[p] = bProcess[p];
     }
   }
 }
@@ -24,16 +24,16 @@ for (var p in env) {
   process.env[p] = env[p];
 }
 
-process.browser = false
+process.browser = false;
 global.Buffer = Buffer;
 global.Buffer.prototype.reverse = function() {
   return require('buffer-reverse')(this , arguments);
 }
 
-const isDev = typeof __DEV__ === 'boolean' && __DEV__
-process.env['NODE_ENV'] = isDev ? 'development' : 'production'
+const isDev = typeof __DEV__ === 'boolean' && __DEV__;
+process.env['NODE_ENV'] = isDev ? 'development' : 'production';
 if (typeof localStorage !== 'undefined') {
-  localStorage.debug = isDev ? '*' : ''
+  localStorage.debug = isDev ? '*' : '';
 }
 
 if (global.navigator && global.navigator.product === 'ReactNative') {
