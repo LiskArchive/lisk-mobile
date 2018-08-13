868d63d53672ad3e5fe0e00bfe03d383911d4639
reyraa <ali@lightcurve.io>
2018-08-13 13:31:46 +0200

    Fix logo in Android
    Signed-off-by: reyraa <ali@lightcurve.io>

--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -4,8 +4,8 @@ import styles from './styles';
 import Icon from '../toolBox/icon';
 import colors from '../../constants/styleGuide/colors';
 
-const Logo = ({ color, size }) =>
-  <View style={styles.image}>
+const Logo = ({ color, size, style }) =>
+  <View style={[styles.image, style]}>
     <Icon name='lisk' color={color || colors.black} size={size || 100} />
   </View>;
 
