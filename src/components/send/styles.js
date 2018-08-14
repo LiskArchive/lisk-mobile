b4a6cc6d20ad0e8a1c66147e8f36b41be3ce8ae9
yasharAyari <yashar@lightcurve.io>
2018-08-14 11:53:32 +0200

    Fix the back button alignment bug

--- src/components/send/styles.js
+++ src/components/send/styles.js
@@ -1,6 +1,11 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Platform } from 'react-native';
 
 const styles = {
+  back: {
+    width: 60,
+    height: 40,
+    marginTop: (Platform.OS === 'ios') ? 0 : 6,
+  },
   wrapper: {
     backgroundColor: '#ffffff',
     flex: 1,
