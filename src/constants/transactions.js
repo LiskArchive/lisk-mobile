7076625687bef9cd5d2cabca1778d069a7e7a797
reyraa <ali@lightcurve.io>
2018-08-17 17:21:44 +0200

    Add the transactions titles to constants

--- src/constants/transactions.js
+++ src/constants/transactions.js
@@ -2,17 +2,21 @@ export default {
   send: {
     type: 0,
     fee: 1e7,
+    title: 'Transfer',
   },
   setSecondPassphrase: {
     type: 1,
     fee: 5e8,
+    title: 'Second passphrase registration',
   },
   registerDelegate: {
     type: 2,
     fee: 25e8,
+    title: 'Delegate registration',
   },
   vote: {
     type: 3,
     fee: 1e8,
+    title: 'Vote',
   },
 };
