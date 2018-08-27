f64904881459f1bc496969b79515dc9b0bde9f29
reyraa <ali@lightcurve.io>
2018-08-27 11:27:42 +0200

    Improve the style of the navigator

--- src/components/register/index.js
+++ src/components/register/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { View, Text } from 'react-native';
 import MultiStep from '../multiStep';
 import Confirm from './confirm';
 import Success from './success';
 import SafeKeeping from './safeKeeping';
 import Intro from './intro';
 import styles from './styles';
+
+const NavButton = props => <View {...props} style={styles.navButton}></View>;
 
 class Register extends React.Component {
   componentDidMount() {
@@ -13,18 +16,21 @@ class Register extends React.Component {
 
   render() {
     return (
-      <MultiStep
-        ref={(el) => { this.nav = el; }}
-        showNav={true}
-        styles={styles}
-        hideSteps={true}
-        interactive={true}
-        backButtonTitle='Back'>
-        <Intro title='step 1' group='page 1'></Intro>
-        <SafeKeeping title='step 2' group='page 2'></SafeKeeping>
-        <Confirm title='step 3' group='page 3'></Confirm>
-        <Success title='step 4' group='page 4'></Success>
-      </MultiStep>
+      <View style={styles.container}>
+        <MultiStep
+          ref={(el) => { this.nav = el; }}
+          showNav={true}
+          styles={styles}
+          hideSteps={true}
+          interactive={true}
+          groupButton={<NavButton />}
+          backButtonTitle='Back'>
+          <Intro title='step 1' group='page 1'></Intro>
+          <SafeKeeping title='step 2' group='page 2'></SafeKeeping>
+          <Confirm title='step 3' group='page 3'></Confirm>
+          <Success title='step 4' group='page 4'></Success>
+        </MultiStep>
+      </View>
     );
   }
 }
--- src/components/register/styles.js
+++ src/components/register/styles.js
@@ -1,20 +1,35 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Platform, DeviceInfo } from 'react-native';
 
 const styles = {
+  container: {
+    height: '100%',
+  },
   back: {
     color: 'black',
   },
   multiStepWrapper: {
-    color: 'black',
+    height: '100%',
   },
   multiStepNavWrapper: {
-    color: 'black',
+    height: 40 + (Platform.OS === 'ios' && DeviceInfo.isIPhoneX_deprecated ? 23 : 0),
+    width: '100%',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
   },
   multiStepGroupWrapper: {
-    color: 'black',
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   multiStepGroup: {
-    color: 'black',
+    height: '100%',
+  },
+  navButton: {
+    width: 32,
+    marginRight: 14,
+    marginLeft: 14,
   },
 };
 
