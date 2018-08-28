8afbe0e0a7103d67ce42641dc05fc26178dff7cb
yasharAyari <yashar@lightcurve.io>
2018-08-28 17:26:10 +0200

    Add label to CopyToClipBoard component

--- src/components/copyToClipboard/index.js
+++ src/components/copyToClipboard/index.js
@@ -14,19 +14,21 @@ class CopyToClipBoard extends React.Component {
   }
   render() {
     const {
-      icon, value, style, type,
+      icon, value, style, type, label,
     } = this.props;
     const Element = type || Text;
+    const text = label || value;
 
     return (<View style={styles.container}
     >
     <Element
       style={style}
       onPress={this.copy}>
-        {this.state.copied ? 'copied to clipboard' : value}
+        {this.state.copied ? 'copied to clipboard' : text }
     </Element>
     {(icon && !this.state.copied) ?
       <Icon
+        style={style}
         iconStyle={styles.icon}
         onPress={this.copy}
         type='font-awesome' name='copy' size={18} /> : null }
