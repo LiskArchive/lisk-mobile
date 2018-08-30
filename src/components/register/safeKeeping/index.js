c41ba94be83d3e686b7c15657a634083259298df
yasharAyari <yashar@lightcurve.io>
2018-08-30 16:56:51 +0200

    Fix an unnecessary variable for the state of component

--- src/components/register/safeKeeping/index.js
+++ src/components/register/safeKeeping/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import SvgUri from 'react-native-svg-uri';
 import { View } from 'react-native';
 import styles from './styles';
-import { H1, B, H4, P } from '../../toolBox/typography';
+import { H1, B, P } from '../../toolBox/typography';
 import CopyToClipboard from '../../copyToClipboard';
 import { SecondaryButton } from '../../toolBox/button';
 import vault from '../../../assets/images/vault.svg';
@@ -11,7 +11,6 @@ import vault from '../../../assets/images/vault.svg';
 class SafeKeeping extends React.Component {
   state = {
     passphrase: '',
-    buttonStatus: false,
   }
   componentDidMount() {
     this.props.navigation.setParams({ action: this.props.prevStep });
@@ -47,15 +46,13 @@ class SafeKeeping extends React.Component {
             height="148"
             source={vault}
           />
-          <H4 style={styles.imageDescription}>Keep it safe!</H4>
         </View>
         <View>
           <SecondaryButton
-            disabled={this.state.buttonStatus}
             style={styles.button}
             onClick={() => {
               this.props.nextStep({
-                passphrase: this.state.passphrase,
+                passphrase: this.props.passphrase,
               });
             }}
             title='I wrote it down' />
