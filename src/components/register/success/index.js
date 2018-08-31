f0222df4de5dc0294c86bfc5da199387c7d87d7d
yasharAyari <yashar@lightcurve.io>
2018-08-31 12:50:57 +0200

    Implement the success step of registration

--- src/components/register/success/index.js
+++ src/components/register/success/index.js
@@ -1,6 +1,44 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import SvgUri from 'react-native-svg-uri';
+import { View } from 'react-native';
+import styles from './styles';
+import { H1, P } from '../../toolBox/typography';
+import { SecondaryButton } from '../../toolBox/button';
+import image from '../../../assets/images/success.svg';
 
-const Success = () => <View><Text>Success</Text></View>;
+class Success extends React.Component {
+  componentDidMount() {
+    this.props.hideNav();
+    this.props.navigation.setParams({ action: false });
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <View>
+          <View style={styles.titleContainer}>
+            <H1 style={styles.header}>Perfect! You’re all set.</H1>
+            <P style={styles.subtitle}>
+              All the steps needed for creating an ID are{'\n'}
+              complete. You can now start sending and{'\n'}
+              receiving LSK tokens.
+            </P>
+          </View>
+          <View style={styles.image} >
+            <SvgUri
+              width="198"
+              height="198"
+              source={image}
+            />
+          </View>
+        </View>
+        <View>
+          <SecondaryButton
+            style={styles.button}
+            onClick={() => this.props.navigation.navigate('Login')}
+            title='Sign in now' />
+        </View>
+      </View>);
+  }
+}
 
 export default Success;
---
+++ src/components/register/success/styles.js
@@ -0,0 +1,33 @@
+import { StyleSheet, DeviceInfo } from 'react-native';
+import { colors } from '../../../constants/styleGuide';
+
+const styles = {
+  container: {
+    flexDirection: 'column',
+    marginRight: 20,
+    marginLeft: 20,
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  header: {
+    marginTop: 8,
+    paddingBottom: 12,
+  },
+  subTitle: {
+    color: colors.grayScale2,
+  },
+  subHeader: {
+    marginTop: 14,
+    marginBottom: 25,
+    color: colors.grayScale1,
+  },
+  image: {
+    marginTop: 52,
+    alignItems: 'center',
+  },
+  button: {
+    marginBottom: DeviceInfo.isIPhoneX_deprecated ? 35 : 25,
+  },
+};
+
+export default StyleSheet.create(styles);
