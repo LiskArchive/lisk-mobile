dea256591dae15bdf812de716203f60343ff2ee6
yasharAyari <yashar@lightcurve.io>
2018-09-07 16:33:49 +0200

    Replace svg images with png ones in empty transaction components

--- src/components/transactions/empty.js
+++ src/components/transactions/empty.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { View } from 'react-native';
-import SvgUri from 'react-native-svg-uri';
-import noActivity from '../../assets/images/noActivity.svg';
+import { View, Image } from 'react-native';
+import noActivity from '../../assets/images/noActivity/noActivity2x.png';
 import { P } from '../toolBox/typography';
 import styles from './styles';
 
 const EmptyState = () =>
   <View style={styles.emptyState}>
     <View style={styles.noActivity}>
-      <SvgUri width={110} source={noActivity} />
+      <Image style={styles.image} source={noActivity} />
     </View>
     <P style={styles.noTxTitle}>You do not have any recent activity.</P>
   </View>;
