6004396ab204ce373facb352e490395b7e04bf26
reyraa <ali@lightcurve.io>
2018-09-11 18:22:54 +0200

    Remove the commented our codes (Socket)

--- src/store/middlewares/socket.js
+++ src/store/middlewares/socket.js
@@ -1,19 +1,15 @@
-// import io from 'socket.io-client';
 import actionTypes from '../../constants/actions';
 import { blockUpdated } from '../../actions/accounts';
 import { getAccount } from '../../utilities/api/account';
 
-/* To-Do We have to disable socket connection because of lisk core problem. we will enable
-socket connection when lisk core fix the issue */
-// let connection;
+/** To-Do We have to disable socket connection because of
+ * Lisk core problem. we will enable socket connection
+ * when Lisk core fix the issue
+ */
 let interval;
 
 const closeConnection = () => {
   clearInterval(interval);
-  // To-Do th
-  // if (connection) {
-  //   connection.close();
-  // }
 };
 
 const socketSetup = (store) => {
@@ -26,35 +22,19 @@ const socketSetup = (store) => {
       }
     });
   }, 60000);
-  // connection = io.connect(store.getState().peers.activePeer.currentNode, {
-  //   transports: ['websocket'],
-  // });
-  // connection.on('blocks/change', (block) => {
-  //   store.dispatch(blockUpdated(block));
-  // });
-  // To-Do enable this lines when offline mode is implemented
-  // connection.on('disconnect', () => {
-  //   if (!forcedClosing) {
-  //     store.dispatch(activePeerUpdate({ online: false }));
-  //   }
-  // });
-  // connection.on('reconnect', () => {
-  //   store.dispatch(activePeerUpdate({ online: true }));
-  // });
 };
 
-const socketMiddleware = store => (
-  next => (action) => {
-    next(action);
-    switch (action.type) {
-      case actionTypes.accountLoggedIn:
-        socketSetup(store, action);
-        break;
-      case actionTypes.accountLoggedOut:
-        closeConnection();
-        break;
-      default: break;
-    }
-  });
+const socketMiddleware = store => next => (action) => {
+  next(action);
+  switch (action.type) {
+    case actionTypes.accountLoggedIn:
+      socketSetup(store);
+      break;
+    case actionTypes.accountLoggedOut:
+      closeConnection();
+      break;
+    default: break;
+  }
+};
 
 export default socketMiddleware;
