069b8353135a3fdd7b14c8c372bab19621d95f97
reyraa <ali@lightcurve.io>
2018-09-12 11:29:28 +0200

    Use stateless component for Settings

--- src/components/settings/index.js
+++ src/components/settings/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import { ScrollView } from 'react-native';
+import { View } from 'react-native';
 import { H1 } from '../toolBox/typography';
 import styles from './styles';
 
-class Settings extends React.Component {
-  title = 'Settings';
-  render() {
-    return (<ScrollView>
-       <H1 style={styles.title}>{this.title}</H1>
-    </ScrollView>);
-  }
-}
+const Settings = () => (
+  <View style={styles.container}>
+    <H1>Settings</H1>
+  </View>
+);
 
 export default Settings;
