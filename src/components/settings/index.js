294b68fa548b0d4dcb608c5cefcb1884e0c907e9
reyraa <ali@lightcurve.io>
2018-09-13 16:14:40 +0200

    Improve the styles of the setting page

--- src/components/settings/index.js
+++ src/components/settings/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import { View } from 'react-native';
-import { H1 } from '../toolBox/typography';
+import { H1, H4 } from '../toolBox/typography';
+import ItemTitle from './itemTitle';
 import styles from './styles';
 
-const Settings = () => (
+const Settings = ({ navigation }) => (
   <View style={styles.container}>
     <H1>Settings</H1>
+
+    <View style={styles.group}>
+      <H4>Privacy</H4>
+    </View>
+
+    <View style={styles.group}>
+      <H4>About</H4>
+      <View style={styles.item}>
+        <ItemTitle
+          navigation={navigation}
+          icon='about'
+          iconSize={21}
+          title='About Lisk'/>
+      </View>
+    </View>
   </View>
 );
 
--- src/components/settings/styles.js
+++ src/components/settings/styles.js
@@ -7,6 +7,38 @@ const styles = {
     backgroundColor: colors.white,
     padding: 20,
   },
+  group: {
+    marginTop: 20,
+  },
+  item: {
+    borderBottomWidth: 1,
+    borderBottomColor: colors.grayScale5,
+    paddingTop: 16,
+    paddingBottom: 6,
+  },
+  itemTitle: {
+    flexDirection: 'row',
+    width: '100%',
+    height: 36,
+    paddingTop: 6,
+    paddingBottom: 6,
+  },
+  itemName: {
+    flex: 1,
+    height: 24,
+  },
+  itemNameText: {
+    color: colors.grayScale1,
+  },
+  itemIcon: {
+    width: 26,
+    height: 24,
+    paddingRight: 2,
+  },
+  itemArrow: {
+    width: 24,
+    height: 24,
+  },
 };
 
 export default StyleSheet.create(styles);
