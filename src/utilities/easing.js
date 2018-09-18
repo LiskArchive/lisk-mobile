86f3d7afeee6d7469f803538d9c5c7a967d3d1f6
reyraa <ali@lightcurve.io>
2018-09-18 13:15:53 +0200

    Add easing function to the utilities

---
+++ src/utilities/easing.js
@@ -0,0 +1,34 @@
+/*
+ * Easing Functions - inspired from http://gizma.com/easing/
+ * only considering the t value for the range [0, 1] => [0, 1]
+ */
+const easings = {
+  // no easing, no acceleration
+  linear: t => t,
+  // accelerating from zero velocity
+  easeInQuad: t => t ** 2,
+  // decelerating to zero velocity
+  easeOutQuad: t => (t * (2 - t)),
+  // acceleration until halfway, then deceleration
+  easeInOutQuad: t => (t < 0.5 ? 2 * (t ** 2) : -1 + ((4 - (2 * t)) * t)),
+  // accelerating from zero velocity
+  easeInCubic: t => t ** 3,
+  // decelerating to zero velocity
+  easeOutCubic: t => (((--t) * (t ** 2)) + 1),
+  // acceleration until halfway, then deceleration
+  easeInOutCubic: t => (t < 0.5 ? 4 * (t ** 3) : ((t - 1) * ((2 * t) - 2) * ((2 * t) - 2)) + 1),
+  // accelerating from zero velocity
+  easeInQuart: t => t ** 4,
+  // decelerating to zero velocity
+  easeOutQuart: t => (1 - ((--t) * (t ** 3))),
+  // acceleration until halfway, then deceleration
+  easeInOutQuart: t => (t < 0.5 ? 8 * (t ** 4) : 1 - (8 * (--t) * (t ** 3))),
+  // accelerating from zero velocity
+  easeInQuint: t => t ** 5,
+  // decelerating to zero velocity
+  easeOutQuint: t => (1 + ((--t) * (t ** 4))),
+  // acceleration until halfway, then deceleration
+  easeInOutQuint: t => (t < 0.5 ? 16 * (t ** 5) : 1 + (16 * (--t) * (t ** 4))),
+};
+
+export default easings;
