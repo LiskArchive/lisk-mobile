061d58e704db6bfe93558bedb63371cff3ddfa86
yasharAyari <yashar@lightcurve.io>
2018-10-12 16:35:57 +0200

    Add jest-transform-stub to the project

--- jest/test-config.js
+++ jest/test-config.js
@@ -2,6 +2,7 @@ module.exports = {
   displayName: 'Test',
   transform: {
     '^.+\\.js$': 'babel-jest',
+    '.+\\.(css|png|jpg|ttf)$': 'jest-transform-stub',
   },
   // preset: 'react-native',
   collectCoverage: true,
