fed5f89f8b7e8825eea6a19376d8c78ea74bf654
reyraa <ali@lightcurve.io>
2018-10-12 12:20:29 +0200

    Remove unused components

--- src/components/followedAccounts/empty.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { FormLabel } from 'react-native-elements';
-
-const EmptyState = () =>
-  <FormLabel>No Followed Accounts</FormLabel>;
-
-export default EmptyState;
