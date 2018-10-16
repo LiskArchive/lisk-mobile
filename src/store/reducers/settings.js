68a5d5d1516bf5c34801e89e80663f0cd0ea2f29
reyraa <ali@lightcurve.io>
2018-10-16 11:40:33 +0200

    Create Settings reducers and actions

---
+++ src/store/reducers/settings.js
@@ -0,0 +1,21 @@
+import actionTypes from '../../constants/actions';
+
+/**
+ * This reducer is designed to store and retrieve the required data
+ * for displaying a loader spinner in a desired order
+ *
+ * @param {Array} state - An array of strings as ID value of each
+ * action calling the loader
+ *
+ * @returns {Object} The latest state
+ */
+const settings = (state = {}, action = {}) => {
+  switch (action.type) {
+    case actionTypes.settingsUpdated:
+      return Object.assign({}, state, action.data);
+    default:
+      return state;
+  }
+};
+
+export default settings;
