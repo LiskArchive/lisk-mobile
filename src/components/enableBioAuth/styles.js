cb6804466d7a8489b56b931b7c335d9876d2d93f
reyraa <ali@lightcurve.io>
2018-10-16 11:42:36 +0200

    Create enableBioAuth component

---
+++ src/components/enableBioAuth/styles.js
@@ -0,0 +1,54 @@
+
+import { StyleSheet, Dimensions, Platform, DeviceInfo } from 'react-native';
+import { colors } from '../../constants/styleGuide';
+
+const { height } = Dimensions.get('window');
+
+const styles = {
+  wrapper: {
+    backgroundColor: colors.white,
+    flex: 1,
+  },
+  container: {
+    flexDirection: 'column',
+    flex: 1,
+    padding: 20,
+    justifyContent: 'space-between',
+    paddingBottom: (Platform.OS === 'ios' && DeviceInfo.isIPhoneX_deprecated) ? 30 : 20,
+  },
+  header: {
+    marginTop: 8,
+  },
+  subHeader: {
+    marginTop: 14,
+    marginBottom: 25,
+    color: colors.grayScale1,
+  },
+  row: {
+    flexDirection: 'row',
+    paddingRight: 50,
+    marginBottom: height > 640 ? 10 : 3,
+    paddingBottom: height > 640 ? 14 : 3,
+  },
+  separator: {
+    borderBottomColor: colors.grayScale5,
+    borderBottomWidth: 1,
+  },
+  rowTitle: {
+    marginTop: 5,
+  },
+  icon: {
+    marginRight: 12,
+    marginTop: 10,
+  },
+  description: {
+    color: colors.grayScale2,
+    marginTop: 5,
+  },
+  label: {
+    color: colors.grayScale2,
+    marginLeft: 12,
+  },
+};
+
+export default StyleSheet.create(styles);
