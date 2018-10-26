824b3b70cb510eb8fdb055b8238a82e513084308
yasharAyari <yashar@lightcurve.io>
2018-10-26 13:58:17 +0200

    Remove SplashActivity.java and Move it's methods to MainActivity.java

--- android/app/src/main/java/io/lisk/mobile/MainActivity.java
+++ android/app/src/main/java/io/lisk/mobile/MainActivity.java
@@ -1,8 +1,15 @@
 package io.lisk.mobile;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);
+        super.onCreate(savedInstanceState);
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
