3c2b546e7eee88b9b3c66030f1951101c10a6a3a
reyraa <ali@lightcurve.io>
2018-10-29 11:44:08 +0100

    Tweak the replace statements to cover more situations

--- androidSdkFix.js
+++ androidSdkFix.js
@@ -3,16 +3,19 @@ const fs = require('fs');
 
 const finder = find('./node_modules');
 
-const sdkVersion = 'compileSdkVersion 26';
-const buildToolsVersion = 'buildToolsVersion "26.0.1"';
+const sdkVersion = 'compileSdkVersion 27';
+const buildToolsVersion = 'buildToolsVersion "27.0.3"';
 
 finder.on('file', (file) => {
   if (file.match(/.build.gradle/)) {
     fs.readFile(file, 'utf8', (err, data) => {
       if (err) throw err;
       if (data.match(/compileSdkVersion\s\d+/)) {
-        const newData = data.replace(/compileSdkVersion\s\d*/, sdkVersion)
-          .replace(/buildToolsVersion.*"/, buildToolsVersion);
+        const newData = data
+          .replace(/compileSdkVersion\s\d*/, sdkVersion)
+          .replace(/targetSdkVersion\s\d*/, sdkVersion)
+          .replace(/buildToolsVersion.*('|")/, buildToolsVersion)
+          .replace(/compile\s/g, 'implementation ');
         fs.writeFileSync(file, newData, { encoding: 'utf8', flag: 'w' });
       }
     });
