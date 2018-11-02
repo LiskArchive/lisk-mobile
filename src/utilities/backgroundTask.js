f92f254a110020d635af21df35fce3f9a52ca1e1
yasharAyari <yashar@lightcurve.io>
2018-11-02 18:04:18 +0100

    Create backgroundTaskInit utility

---
+++ src/utilities/backgroundTask.js
@@ -0,0 +1,32 @@
+import queueFactory from 'react-native-queue';
+import BackgroundTask from 'react-native-background-task'; // eslint-disable-line
+import { sendNotifications } from './notifications';
+import { fetchData } from './storage';
+
+export const backgroundTaskInit = () => {
+  BackgroundTask.define(async () => {
+    // Init queue
+    const queue = await queueFactory();
+    // Register job worker
+    queue.addWorker('check-balance', (id, payload) => {
+      fetch(`${payload.serverUrl}/api/accounts?address=${payload.address}`)
+        .then(res => res.json())
+        .then(async (res) => {
+          const { balance } = res.data[0];
+          const oldBalance = await fetchData('balance');
+          if (balance !== oldBalance) {
+            const changes = res.data[0].balance - oldBalance;
+            sendNotifications(changes, res.data[0].balance);
+          }
+        });
+    });
+    // Start the queue with a lifespan
+    await queue.start(25000); // Run queue for at most 25 seconds.
+    // finish() must be called before OS hits timeout.
+    BackgroundTask.finish();
+  });
+};
+
+export const initQueue = () => queueFactory();
+
+// 7056261880661230236L
