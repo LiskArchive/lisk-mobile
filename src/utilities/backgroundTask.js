93aa1653ff611624702784998d46180a091da1b6
yasharAyari <yashar@lightcurve.io>
2018-11-07 13:52:51 +0100

    run queue.createJob when the queue is finished

--- src/utilities/backgroundTask.js
+++ src/utilities/backgroundTask.js
@@ -1,28 +1,31 @@
 import queueFactory from 'react-native-queue';
 import BackgroundTask from 'react-native-background-task'; // eslint-disable-line
-import { sendNotifications } from './notifications';
+import { createNotification } from './notifications';
 import { fetchData } from './storage';
 
 export const backgroundTaskInit = () => {
   BackgroundTask.define(async () => {
     // Init queue
     const queue = await queueFactory();
+    let data;
     // Register job worker
     queue.addWorker('check-balance', (id, payload) => {
+      data = payload;
       fetch(`${payload.serverUrl}/api/accounts?address=${payload.address}`)
         .then(res => res.json())
         .then(async (res) => {
           const { balance } = res.data[0];
           const oldBalance = await fetchData('balance') || payload.balance;
-          if (balance !== oldBalance) {
-            const changes = res.data[0].balance - oldBalance;
-            sendNotifications(changes, res.data[0].balance);
+          const changes = balance - oldBalance.match(/\d+/g)[0];
+          if (changes !== 0) {
+            createNotification(changes, balance);
           }
         });
     });
     // Start the queue with a lifespan
     await queue.start(25000); // Run queue for at most 25 seconds.
     // finish() must be called before OS hits timeout.
+    queue.createJob('check-balance', data, { attempts: 1, timeout: 15000 }, false);
     BackgroundTask.finish();
   });
 };
