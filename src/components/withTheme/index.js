952988d96155135710208c49c0aed42e9b7383b8
Altay Aydemir <altay.aydemir@lightcurve.io>
2018-11-07 09:53:12 +0100

    Add comment to withTheme HOC

--- src/components/withTheme/index.js
+++ src/components/withTheme/index.js
@@ -3,7 +3,19 @@ import hoistNonReactStatics from 'hoist-non-react-statics';
 import ThemeContext from '../../contexts/theme';
 import { createThemedStyles } from '../../utilities/helpers';
 
-export default (Component, styles, forwardRefs = false) => {
+const getDisplayName = Component =>
+  Component.displayName || Component.name || 'Component';
+
+/**
+ * React Navigation has a problem with rendering components as
+ * route handlers when they're wrapped with React's forwardRef API.
+ * Because of this, we need to avoid using refs in them.
+ * https://github.com/react-navigation/react-navigation/issues/5193
+ *
+ * However, it's safe to use refs in child components and forwardRefs flag
+ * should be true when wrapping them with this HOC.
+ */
+export default (WrappedComponent, styles, forwardRefs = false) => {
   if (forwardRefs) {
     class WithTheme extends React.Component {
       render() {
@@ -12,7 +24,7 @@ export default (Component, styles, forwardRefs = false) => {
         return (
           <ThemeContext.Consumer>
             {theme => (
-              <Component
+              <WrappedComponent
                 ref={forwardedRef}
                 styles={createThemedStyles(theme, styles)}
                 theme={theme}
@@ -24,8 +36,8 @@ export default (Component, styles, forwardRefs = false) => {
       }
     }
 
-    WithTheme.displayName = `withTheme(${Component.displayName || Component.name}`;
-    hoistNonReactStatics(WithTheme, Component);
+    WithTheme.displayName = `withTheme(${getDisplayName(WrappedComponent)}`;
+    hoistNonReactStatics(WithTheme, WrappedComponent);
     return React.forwardRef((props, ref) => <WithTheme {...props} forwardedRef={ref} />);
   }
 
@@ -34,7 +46,7 @@ export default (Component, styles, forwardRefs = false) => {
       return (
         <ThemeContext.Consumer>
           {theme => (
-            <Component
+            <WrappedComponent
               {...this.props}
               styles={createThemedStyles(theme, styles)}
               theme={theme}
@@ -45,6 +57,6 @@ export default (Component, styles, forwardRefs = false) => {
     }
   }
 
-  WithTheme.displayName = `withTheme(${Component.displayName || Component.name}`;
-  return hoistNonReactStatics(WithTheme, Component);
+  WithTheme.displayName = `withTheme(${getDisplayName(WrappedComponent)}`;
+  return hoistNonReactStatics(WithTheme, WrappedComponent);
 };
