cfc8c2f4b140cc09fdafc830278ab43071073c9d
Altay Aydemir <altay.aydemir@lightcurve.io>
2018-11-08 12:24:35 +0100

    Refactor SignOutButton

--- src/components/signOutButton/index.js
+++ src/components/signOutButton/index.js
@@ -1,49 +1,42 @@
 import React from 'react';
-import connect from 'redux-connect-decorator';
 import { NavigationActions } from 'react-navigation';
-import {
-  accountSignedOut as accountSignedOutAction,
-} from '../../actions/accounts';
 import { IconButton } from '../toolBox/button';
 import { colors } from '../../constants/styleGuide';
 import withTheme from '../withTheme';
 import getStyles from './styles';
 
-const onClick = (navigation, accountSignedOut) => {
-  // clean active account
-  accountSignedOut();
-  // navigate to the signIn page
-  navigation
-    .dispatch(NavigationActions.reset({
-      index: 0,
-      actions: [NavigationActions.navigate({
-        routeName: 'SignIn',
-        params: {
-          signOut: true,
-        },
-      })],
-    }));
-};
-
-@connect(state => ({
-  peers: state.peers,
-  accounts: state.accounts,
-}), {
-  accountSignedOut: accountSignedOutAction,
-})
 class SignOutButton extends React.Component {
+  onClick = () => {
+    // clean active account
+    this.props.signOut();
+
+    // navigate to the signIn page
+    this.props.navigation
+      .dispatch(NavigationActions.reset({
+        index: 0,
+        actions: [NavigationActions.navigate({
+          routeName: 'SignIn',
+          params: {
+            signOut: true,
+          },
+        })],
+      }));
+  }
+
   render() {
-    const {
-      navigation, styles, titleStyle, style, accountSignedOut,
-    } = this.props;
-    return <IconButton
-      icon='logout'
-      iconSize={18}
-      title='Sign out'
-      color={colors.light.blue}
-      onClick={() => onClick(navigation, accountSignedOut)}
-      titleStyle={[styles.title, titleStyle]}
-      style ={[styles.button, style]} />;
+    const { theme, styles } = this.props;
+
+    return (
+      <IconButton
+        icon='logout'
+        iconSize={18}
+        title='Sign out'
+        color={colors[theme].blue}
+        onClick={this.onClick}
+        titleStyle={[styles.title, styles.theme.title]}
+        style={styles.button}
+      />
+    );
   }
 }
 
