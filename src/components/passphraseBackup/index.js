89153854b138d632c99c0298ae9528141371b5c1
Altay Aydemir <altay.aydemir@lightcurve.io>
2018-11-08 18:07:25 +0100

    Update PassphraseBackup component

--- src/components/passphraseBackup/index.js
+++ src/components/passphraseBackup/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import connect from 'redux-connect-decorator';
-import { View, Image } from 'react-native';
-import { deviceHeight } from '../../utilities/device';
-import { H1, B, P } from '../toolBox/typography';
-import CopyToClipboard from '../copyToClipboard';
-import { themes } from '../../constants/styleGuide';
-import passphraseImg from '../../assets/images/registrationProcess/passphrase3x.png';
-import passphraseImgDark from '../../assets/images/registrationProcess/passphrase-dark3x.png';
+import { View } from 'react-native';
+import { H1, P } from '../toolBox/typography';
+import PassphraseCopy from '../passphraseCopy';
 import withTheme from '../withTheme';
 import getStyles from './styles';
 
@@ -14,60 +10,21 @@ import getStyles from './styles';
   account: state.accounts.active,
 }), {})
 class PassphraseBackup extends React.Component {
-  constructor() {
-    super();
-    this.deviceHeight = deviceHeight();
-  }
-
   render() {
-    const { theme, styles, account: { passphrase } } = this.props;
+    const { styles, account: { passphrase } } = this.props;
 
     return (
       <View style={[styles.wrapper, styles.theme.wrapper]}>
         <View style={styles.container}>
-          <View>
-            <View style={styles.titleContainer}>
-              <H1 style={[styles.header, styles.theme.header]}>
-                Backup your passphrase
-              </H1>
-              <P style={[styles.subHeader, styles.theme.subHeader]}>
-                Carefully write it down or copy to the clipboard.
-              </P>
-            </View>
-            <View style={[styles.passphraseContainer, styles.theme.passphraseContainer]}>
-              <P style={styles.theme.passphraseTitle}>
-                Store your passphrase:
-              </P>
-              <B style={[styles.passphrase, styles.theme.passphrase]}>
-                {passphrase}
-              </B>
-            </View>
-            <View style={styles.copyContainer}>
-              <CopyToClipboard
-                style={styles.copyContainer}
-                labelStyle={styles.theme.copy}
-                iconStyle={styles.theme.copy}
-                label='Copy to clipboard'
-                showIcon={true}
-                iconSize={14}
-                value={passphrase}
-                type={P}
-              />
-            </View>
+          <View style={styles.titleContainer}>
+            <H1 style={[styles.header, styles.theme.header]}>
+              Backup your passphrase
+            </H1>
+            <P style={[styles.subHeader, styles.theme.subHeader]}>
+              Carefully write it down or copy to the clipboard.
+            </P>
           </View>
-          {
-            this.deviceHeight > 640 &&
-              <View style={styles.imageContainer}>
-                <Image
-                  style={styles.image}
-                  source={theme === themes.light ? passphraseImg : passphraseImgDark}
-                />
-                <P style={[styles.caption, styles.theme.caption]}>
-                  Keep it safe!
-                </P>
-              </View>
-          }
-          <View style={styles.placeholder} />
+          <PassphraseCopy passphrase={passphrase} />
         </View>
       </View>
     );
--- src/components/passphraseBackup/styles.js
+++ src/components/passphraseBackup/styles.js
@@ -7,11 +7,10 @@ export default () => ({
       flex: 1,
     },
     container: {
-      flexDirection: 'column',
       padding: 20,
-      flex: 1,
-      justifyContent: 'space-between',
       paddingBottom: (Platform.OS === 'ios' && DeviceInfo.isIPhoneX_deprecated) ? 30 : 20,
+    },
+    titleContainer: {
     },
     header: {
       marginTop: 8,
@@ -19,35 +18,6 @@ export default () => ({
     subHeader: {
       marginTop: 8,
       marginBottom: 25,
-    },
-    passphraseContainer: {
-      padding: 20,
-      shadowOpacity: 0.1,
-      shadowRadius: 20,
-      marginTop: 10,
-      elevation: 1,
-      shadowColor: '#0279b6',
-    },
-    passphrase: {
-      marginTop: 8,
-    },
-    copyContainer: {
-      alignItems: 'center',
-      marginTop: 8,
-    },
-    imageContainer: {
-      alignItems: 'center',
-    },
-    image: {
-      width: 111,
-      height: 111,
-    },
-    caption: {
-      marginTop: 15,
-    },
-    placeholder: {
-      height: 1,
-      width: '100%',
     },
   },
 
@@ -61,21 +31,6 @@ export default () => ({
     subHeader: {
       color: colors.light.gray2,
     },
-    passphraseContainer: {
-      backgroundColor: colors.light.white,
-    },
-    passphraseTitle: {
-      color: colors.light.gray2,
-    },
-    passphrase: {
-      color: colors.light.black,
-    },
-    copy: {
-      color: colors.light.blue,
-    },
-    caption: {
-      color: colors.light.gray2,
-    },
   },
 
   [themes.dark]: {
@@ -87,21 +42,6 @@ export default () => ({
     },
     subHeader: {
       color: colors.dark.gray1,
-    },
-    passphraseContainer: {
-      backgroundColor: '#011730',
-    },
-    passphraseTitle: {
-      color: colors.dark.gray4,
-    },
-    passphrase: {
-      color: colors.dark.white,
-    },
-    copy: {
-      color: colors.dark.blue,
-    },
-    caption: {
-      color: colors.dark.gray4,
     },
   },
 });
