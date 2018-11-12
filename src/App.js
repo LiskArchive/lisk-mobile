e3528399b5003e5ced50411ed5ff017218ac7910
reyraa <ali@lightcurve.io>
2018-11-12 13:53:02 +0100

    Connect the App Theme provider to the store settings

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import React, { Fragment } from 'react';
 import { StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
+import connect from 'redux-connect-decorator';
 import Router from './components/router';
 import Loading from './components/loading';
 import store from './store/index';
 import ThemeContext from './contexts/theme';
-import { themes } from './constants/styleGuide';
 
-class App extends React.Component {
-  state = {
-    theme: themes.light,
-  }
-
+@connect(state => ({
+  settings: state.settings,
+}), {})
+class ThemedApp extends React.Component {
   render() {
-    return (
-      <Provider store={store}>
-        <ThemeContext.Provider value={this.state.theme}>
-          <Fragment>
-            <StatusBar barStyle="light-content" />
-            <Loading />
-            <Router />
-          </Fragment>
-       </ThemeContext.Provider>
-      </Provider>
-    );
+    return (<ThemeContext.Provider value={this.props.settings.theme}>
+      <Fragment>
+        <StatusBar barStyle="light-content" />
+        <Loading />
+        <Router />
+      </Fragment>
+    </ThemeContext.Provider>);
   }
 }
+
+const App = () => (
+  <Provider store={store}>
+    <ThemedApp />
+  </Provider>);
 
 export default App;
