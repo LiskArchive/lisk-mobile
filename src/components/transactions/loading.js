8a8bc7b16ce52c7fb746d44bf489c33c2499d113
yasharAyari <yashar@lightcurve.io>
2018-11-13 17:59:36 +0100

    Implement the dark mode of LoadingState component

--- src/components/transactions/loading.js
+++ src/components/transactions/loading.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { View, Image } from 'react-native';
-import noActivity from '../../assets/images/loading3x.png';
+import noActivityLight from '../../assets/images/loading3xLight.png';
+import noActivityDark from '../../assets/images/loading3xDark.png';
 import withTheme from '../withTheme';
 import getStyles from './styles';
+import { themes } from '../../constants/styleGuide';
 
-const LoadingState = ({ styles }) =>
+const LoadingState = ({ theme, styles }) => (
   <View style={styles.emptyState}>
-    <Image source={noActivity} style={styles.loading} />
-  </View>;
+    {
+      theme === themes.light ?
+        <Image source={noActivityLight} style={styles.loading} /> :
+        <Image source={noActivityDark} style={styles.loading} />
+    }
+  </View>);
 
 export default withTheme(LoadingState, getStyles());
