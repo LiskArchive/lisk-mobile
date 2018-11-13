d1586c6100d587736778259911ffd4d39176ddd0
reyraa <ali@lightcurve.io>
2018-11-13 12:42:41 +0100

    Fine tune the switch

--- src/components/settings/switchButton.js
+++ src/components/settings/switchButton.js
@@ -2,9 +2,12 @@ import React from 'react';
 import Switch from 'react-native-switch-pro';
 import { colors } from '../../constants/styleGuide';
 
-const switchButton = ({ value, height, onSyncPress }) => <Switch
+const switchButton = ({
+  value, height, onSyncPress, style,
+}) => <Switch
   value={value}
   height={height || 26}
+  style={style}
   width={43}
   onSyncPress={onSyncPress}
   backgroundActive={colors.dark.blue}
