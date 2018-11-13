4c788dda9b38503a6e766a1482284b8bc6a7833b
yasharAyari <yashar@lightcurve.io>
2018-11-13 15:23:45 +0100

    Implement the dark mode of the headerBackground component

--- src/components/headerBackground/index.js
+++ src/components/headerBackground/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Image, View } from 'react-native';
-import stripes from '../../assets/images/stripes.png';
+import stripesLight from '../../assets/images/stripesLight.png';
+import stripesDark from '../../assets/images/stripesDark.png';
 import topBubbles from '../../assets/images/topBubbles3x.png';
 import withTheme from '../withTheme';
 import getStyles from './styles';
 
-const Bg = ({ bgType, styles }) => {
+const Bg = ({ bgType, styles, theme }) => {
   let numericDimensions = {};
-  let source = stripes;
+  let source = theme === 'light' ? stripesLight : stripesDark;
   if (bgType === 'bubbles') {
     numericDimensions = { width: 375, height: 97 };
     source = topBubbles;
