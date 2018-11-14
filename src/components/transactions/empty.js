a4c845e2ba347283fde8514a00663259d164c9c5
yasharAyari <yashar@lightcurve.io>
2018-11-14 10:24:48 +0100

    Implement the dark mode of EmptyState component

--- src/components/transactions/empty.js
+++ src/components/transactions/empty.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { View, Image } from 'react-native';
-import noActivity from '../../assets/images/noActivity/noActivity2x.png';
+import noActivityLight from '../../assets/images/noActivity/noActivity3xLight.png';
+import noActivityDark from '../../assets/images/noActivity/noActivity3xDark.png';
 import { P } from '../toolBox/typography';
 import withTheme from '../withTheme';
 import getStyles from './styles';
+import { themes } from '../../constants/styleGuide';
 
-const EmptyState = ({ styles }) =>
+const EmptyState = ({ theme, styles }) =>
   <View style={styles.emptyState}>
     <View style={styles.noActivity}>
-      <Image style={styles.empty} source={noActivity} />
+      {
+        theme === themes.light ?
+          <Image style={styles.empty} source={noActivityLight} /> :
+          <Image style={styles.empty} source={noActivityDark} />
+      }
     </View>
     <P style={styles.noTxTitle}>You do not have any recent activity.</P>
   </View>;
