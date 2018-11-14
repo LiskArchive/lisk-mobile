77d5698de457d507f4ddcfc351b8b3dc89cc1baf
yasharAyari <yashar@lightcurve.io>
2018-11-14 17:10:42 +0100

    Implement the dark mode of the CameraAccess component

--- src/components/send/form/cameraAccess.js
+++ src/components/send/form/cameraAccess.js
@@ -2,21 +2,31 @@ import React, { Fragment } from 'react';
 import { Image, TouchableHighlight } from 'react-native';
 import OpenAppSettings from 'react-native-app-settings';
 import { P, H4 } from '../../toolBox/typography';
-import cameraPermissionIcon from '../../../assets/images/camera3x.png';
+import cameraPermissionIconLight from '../../../assets/images/camera3xLight.png';
+import cameraPermissionIconDark from '../../../assets/images/camera3xDark.png';
 import withTheme from '../../withTheme';
 import getStyles from './styles';
+import { themes } from '../../../constants/styleGuide';
 
-const CameraAccess = ({ styles }) => (
+const CameraAccess = ({ theme, styles }) => (
   <TouchableHighlight
     onPress={() => { OpenAppSettings.open(); }}
     underlayColor='transparent'
-    style={styles.permissionRequestWrapper}>
+    style={[styles.permissionRequestWrapper, styles.theme.permissionRequestWrapper]}>
     <Fragment>
-      <Image
-        style={styles.permissionIcon}
-        source={cameraPermissionIcon} />
-      <H4>Allow camera access</H4>
-      <P style={styles.permissionDescription}>
+      {
+        theme === themes.light ?
+          <Image
+            style={styles.permissionIcon}
+            source={cameraPermissionIconLight} /> :
+          <Image
+            style={styles.permissionIcon}
+            source={cameraPermissionIconDark} />
+      }
+      <H4 style={[styles.permissionTitle, styles.theme.permissionTitle]}>
+        Allow camera access
+      </H4>
+      <P style={[styles.permissionDescription, styles.theme.permissionDescription]}>
         Lisk needs to access your camera for scanning QR codes.
         Tap on the icon above to go to settings.
       </P>
