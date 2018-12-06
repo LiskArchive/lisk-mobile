b62acd9b56dda6e75185928dbf7edf6ec8a1c85e
Altay Aydemir <altay.aydemir@lightcurve.io>
2018-12-06 08:55:57 +0100

    Add react-native-haptic-feedback package

--- android/app/src/main/java/io/lisk/mobile/MainApplication.java
+++ android/app/src/main/java/io/lisk/mobile/MainApplication.java
@@ -3,6 +3,7 @@ package io.lisk.mobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.mkuczera.RNReactNativeHapticFeedbackPackage;
 import com.bluroverly.SajjadBlurOverlayPackage;
 import com.hieuvp.fingerprint.ReactNativeFingerprintScannerPackage;
 import com.oblador.keychain.KeychainPackage;
@@ -39,6 +40,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
+            new RNReactNativeHapticFeedbackPackage(),
         new SajjadBlurOverlayPackage(),
         new ReactNativeFingerprintScannerPackage(),
         new KeychainPackage(),
