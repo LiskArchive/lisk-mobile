6c37ced56f1e6c42f11920af8d59773ed62ff661
Altay Aydemir <altay.aydemir@lightcurve.io>
2018-12-12 15:02:08 +0100

    Add animation to Progress component

--- src/components/send/progress.js
+++ src/components/send/progress.js
@@ -1,16 +1,47 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Animated } from 'react-native';
 import withTheme from '../withTheme';
 import getStyles from './styles';
 
-const Progress = ({ current, total, styles }) => (
-  <View style={[styles.progressContainer, styles.theme.progressContainer]}>
-    <View
-      style={[styles.progress, styles.theme.progress, {
-        width: `${(current / total) * 100}%`,
-      }]}
-    />
-  </View>
-);
+class Progress extends React.Component {
+  state = {
+    progressRatio: new Animated.Value(0),
+  }
+
+  componentDidMount() {
+    this.setProgress(0);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.current !== this.props.current) {
+      this.setProgress(300);
+    }
+  }
+
+  setProgress(duration) {
+    Animated.timing(this.state.progressRatio, {
+      toValue: this.props.current / this.props.total,
+      duration,
+    }).start();
+  }
+
+  render() {
+    const { styles } = this.props;
+    const { progressRatio } = this.state;
+
+    return (
+      <View style={[styles.progressContainer, styles.theme.progressContainer]}>
+        <Animated.View
+          style={[styles.progress, styles.theme.progress, {
+            width: progressRatio.interpolate({
+              inputRange: [0, 1],
+              outputRange: ['0%', '100%'],
+            }),
+          }]}
+        />
+      </View>
+    );
+  }
+}
 
 export default withTheme(Progress, getStyles());
