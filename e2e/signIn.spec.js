801a1346e58f01a2cb915322500032fcb9d7283c
Altay Aydemir <altay.aydemir@lightcurve.io>
2018-12-21 13:15:14 +0100

    Create basic e2e test for SignIn flow

--- e2e/signIn.spec.js
+++ e2e/signIn.spec.js
@@ -5,17 +5,29 @@ describe('SignIn Flow', () => {
     await device.reloadReactNative();
   });
 
-  describe('Sign In Screen', () => {
-    it('should have the form', async () => {
-      const form = element(by.id('signInForm'));
-      await waitFor(form).toExist().withTimeout(3000);
-      await expect(form).toExist();
-    });
+  const passphrase = 'truly chicken bracket giant lecture coyote undo tourist portion damage mansion together';
 
-    it('should have a tapable button', async () => {
-      const button = element(by.id('signInButton'));
-      await expect(button).toExist();
-      await button.tap();
-    });
+  it('should have the form', async () => {
+    const form = element(by.id('signInForm'));
+    await waitFor(form).toExist().withTimeout(3000);
+    await expect(form).toExist();
+  });
+
+  it('should have a fillable input', async () => {
+    const input = element(by.id('signInPassphraseInput'));
+    await expect(input).toExist();
+    await input.typeText(passphrase);
+  });
+
+  it('should have a tapable button', async () => {
+    const button = element(by.id('signInButton'));
+    await expect(button).toExist();
+    await button.tap();
+  });
+
+  it('should complete sign in process and navigate to the home page', async () => {
+    const accountSummary = element(by.id('accountSummary'));
+    await waitFor(accountSummary).toExist().withTimeout(3000);
+    await expect(accountSummary).toExist();
   });
 });
