b97e3bbe37244f18b3978f66b6ae795f57febb57
Altay Aydemir <altay.aydemir@lightcurve.io>
2019-01-02 13:05:59 +0100

    Force keyboard to be light on SignIn Screen

--- src/components/signIn/form.js
+++ src/components/signIn/form.js
@@ -160,6 +160,7 @@ class Form extends React.Component {
             multiline={Platform.OS === 'ios'}
             secureTextEntry={Platform.OS !== 'ios'}
             error={errorMessage}
+            keyboardAppearance="light"
           />
           {
             passphrase.value === '' ?
