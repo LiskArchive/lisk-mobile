309d5f4956de94d469572313603a6c16acb1ac35
reyraa <ali@lightcurve.io>
2019-01-16 16:05:05 +0100

    Set the device language if it is not already set

--- src/store/middlewares/settings.js
+++ src/store/middlewares/settings.js
@@ -1,15 +1,26 @@
+import { NativeModules } from 'react-native';
 import actionTypes from '../../constants/actions';
 import { storeSettings } from '../../utilities/storage';
 
+const deviceLocale = NativeModules.SettingsManager.settings.AppleLocale;
+
 const settingsMiddleware = store => next => (action) => {
-  next(action);
   const { settings } = store.getState();
 
   switch (action.type) {
-    case actionTypes.settingsUpdated:
-      storeSettings(settings);
+    case actionTypes.settingsRetrieved:
+      if (!settings.locale) {
+        action.data.locale = deviceLocale;
+      }
+      next(action);
       break;
-    default: break;
+    case actionTypes.settingsUpdated:
+      next(action);
+      storeSettings(store.getState().settings);
+      break;
+    default:
+      next(action);
+      break;
   }
 };
 
