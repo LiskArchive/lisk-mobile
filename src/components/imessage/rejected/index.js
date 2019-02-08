a185a2786afd2cc6fff98b3b2190aa6f5e3449a0
Altay Aydemir <altay.aydemir@lightcurve.io>
2019-02-08 08:29:40 +0100

    Remove redundant code from Rejected component

--- src/components/imessage/rejected/index.js
+++ src/components/imessage/rejected/index.js
@@ -1,35 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View, Image } from 'react-native';
 import { B } from '../../toolBox/typography';
-// import { colors } from '../../../constants/styleGuide';
 import styles from './styles';
 
-class Rejected extends Component {
-  state = {
-    errorMessage: false,
-    triggered: false,
-  }
-
-
-  render() {
-    const {
-      sharedData: { amount },
-      status,
-    } = this.props;
-
-    return (
-      <View
-        style={styles.container}
-      >
-        <View style={styles.innerContainer}>
-          <Image source={{ uri: status }} style={{ width: 250, height: 250 }} />
-          <B style={styles.description}>
-            Your request of {amount} LSK has been {status}.
-          </B>
-        </View>
-      </View>
-    );
-  }
-}
+const Rejected = ({ sharedData: { amount }, status }) => (
+  <View style={styles.container}>
+    <View style={styles.innerContainer}>
+      <Image source={{ uri: status }} style={{ width: 250, height: 250 }} />
+      <B style={styles.description}>
+        Your request of {amount} LSK has been {status}.
+      </B>
+    </View>
+  </View>
+);
 
 export default Rejected;
