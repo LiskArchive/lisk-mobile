e2861fd722830b51d3d35ce537797306683af881
yasharAyari <yashar@lightcurve.io>
2019-02-13 12:29:17 +0100

    Add redux-devtools-extension to the redux store configuration

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,9 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import * as reducers from './reducers';
 import middleWares from './middlewares';
 
 const App = combineReducers(reducers);
-const store = createStore(App, applyMiddleware(...middleWares));
+const store = createStore(App, composeWithDevTools(applyMiddleware(...middleWares)));
 
 export default store;
