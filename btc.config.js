b71d99a0328948901db43f1259d0df9e4c389254
yasharAyari <yashar@lightcurve.io>
2019-02-19 18:24:45 +0100

    Fix a conflict in the btc config file

--- btc.config.js
+++ btc.config.js
@@ -1,9 +1,11 @@
 import bitcoin from 'bitcoinjs-lib';
 
-const istestNet = process.env.network === 'testnet';
+const isTestnet = process.env.network === 'testnet';
+
 export default {
-  url: istestNet ? 'https://testnet.blockchain.info' : 'https://blockchain.info',
-  network: istestNet ? bitcoin.networks.testnet : bitcoin.networks.bitcoin,
+  url: isTestnet ? 'https://testnet.blockchain.info' : 'https://blockchain.info',
+  network: isTestnet ? bitcoin.networks.testnet : bitcoin.networks.bitcoin,
+  derivationPath: isTestnet ? "m/44'/1'/0'/0/0" : "m/44'/0'/0'/0/0",
   requestOptions: {
     method: 'GET',
     headers: {
