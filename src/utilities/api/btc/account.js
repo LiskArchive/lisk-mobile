4a5e79ff5afb037a2af9790c4e2fa35b901d70ac
Altay Aydemir <altay.aydemir@lightcurve.io>
2019-02-20 09:26:41 +0100

    Export getDerivedPathFromSeed utility from btc/account

--- src/utilities/api/btc/account.js
+++ src/utilities/api/btc/account.js
@@ -22,7 +22,7 @@ export const getSummary = address => new Promise(async (resolve, reject) => {
   }
 });
 
-const getDerivedPathFromSeed = (passphrase) => {
+export const getDerivedPathFromSeed = (passphrase) => {
   const seed = Lisk.passphrase.Mnemonic.mnemonicToSeed(passphrase);
   return bip32.fromSeed(seed, config.network).derivePath(config.derivationPath);
 };
