d4cb941944f562ba77391a57f53c3b6fba0d6aa8
Altay Aydemir <altay.aydemir@lightcurve.io>
2019-02-20 09:26:21 +0100

    Add minerFeesURL to btc.config

--- btc.config.js
+++ btc.config.js
@@ -4,6 +4,7 @@ const isTestnet = process.env.network === 'testnet';
 
 export default {
   url: isTestnet ? 'https://testnet.blockchain.info' : 'https://blockchain.info',
+  minerFeesURL: 'https://bitcoinfees.earn.com/api/v1/fees/recommended',
   network: isTestnet ? bitcoin.networks.testnet : bitcoin.networks.bitcoin,
   derivationPath: isTestnet ? "m/44'/1'/0'/0/0" : "m/44'/0'/0'/0/0",
   requestOptions: {
