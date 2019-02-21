e5efeb7d2eabe6c37e31d9b4abaa7fc03948e38e
Altay Aydemir <altay.aydemir@lightcurve.io>
2019-02-21 14:16:22 +0100

    Update lisk api client to use new config file

--- src/utilities/api/lisk/apiClient.js
+++ src/utilities/api/lisk/apiClient.js
@@ -1,8 +1,9 @@
 import { APIClient } from '@liskhq/lisk-client';
+import config from '../../../../lsk.config';
 
-const createLiskAPIClient = (network = process.env.network) => {
-  if (network === 'testnet') {
-    return APIClient.createTestnetAPIClient({ node: 'https://testnet.lisk.io' });
+const createLiskAPIClient = () => {
+  if (config.isTestnet) {
+    return APIClient.createTestnetAPIClient({ node: config.nodeURL });
   }
 
   return APIClient.createMainnetAPIClient();
