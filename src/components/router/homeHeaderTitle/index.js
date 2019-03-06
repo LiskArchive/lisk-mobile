b76e286a0278422375ec5e536be2805ac9f832ad
reyraa <ali@lightcurve.io>
2019-03-06 17:48:47 +0100

    Improve the components in homeHeaderTitle. Use correct token sign

--- src/components/router/homeHeaderTitle/index.js
+++ src/components/router/homeHeaderTitle/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Animated, Text, View, Image } from 'react-native';
 import { fromRawLsk } from '../../../utilities/conversions';
 import Avatar from '../../avatar';
+import Icon from '../../toolBox/icon';
 import withTheme from '../../withTheme';
 import getStyles from './styles';
 import darkBig from '../../../assets/images/balanceBlur/darkBig.png';
@@ -15,59 +16,79 @@ const blurs = {
   darkBig, darkMedium, darkSmall, lightBig, lightMedium, lightSmall,
 };
 
-const homeHeaderTitle = ({
-  styles, theme, style, children,
+const ExtendedTitle = ({
+  balance, theme, styles, interpolate = () => 1, address, incognito, type, token,
 }) => {
-  if (typeof children === 'string') {
-    return (
-      <Text style={[styles.main, styles.theme.main, style]}>
-        {children}
-      </Text>
-    );
-  }
-
-  const {
-    interpolate, address, balance = 0, placeHolder, type, incognito,
-  } = children;
-
   let balanceSize = 'Small';
-  if (balance.length > 6) balanceSize = 'Big';
-  else if (balance.length > 2) balanceSize = 'Medium';
+  if (balance > 6) balanceSize = 'Big';
+  else if (balance > 2) balanceSize = 'Medium';
 
   return (
-    <View style={styles.container}>
-      <Animated.Text
-        numberOfLines={1}
-        style={[
-          styles.main,
-          styles.theme[`${type}Main`],
-          style,
-          { opacity: interpolate([0, 100, 130], [1, 1, 0]) },
-        ]}
-        accessibilityTraits="header"
-        allowFontScaling={false}>
-        {placeHolder}
-      </Animated.Text>
-      <Animated.View style={[
-        styles.wrapper,
-        {
-          opacity: interpolate([0, 90, 130], [0, 0, 1]),
-          transform: [{ translateY: interpolate([0, 100, 210], [100, 100, 0]) }],
-        },
-      ]}>
-        <Avatar address={address} size={30} style={styles.avatar} />
-        {incognito ?
-          <Image
-            source={blurs[`${theme}${balanceSize}`]}
-            style={styles[`blur${balanceSize}`]}
-          /> :
-          <Text style={[styles.main, styles.theme[`${type}Main`], style]}>
-            {`${fromRawLsk(balance)} LSK`}
-          </Text>
-        }
-      </Animated.View>
-    </View>
+    <Animated.View style={[
+      styles.wrapper,
+      {
+        opacity: interpolate([0, 90, 130], [0, 0, 1]),
+        transform: [{ translateY: interpolate([0, 100, 210], [100, 100, 0]) }],
+      },
+    ]}>
+      {
+        token === 'LSK' ?
+          <Avatar address={address} size={30} style={styles.avatar} /> :
+          <Icon name='language' size={40} color={'orange'} />
+      }
+      {incognito ?
+        <Image
+          source={blurs[`${theme}${balanceSize}`]}
+          style={styles[`blur${balanceSize}`]}
+        /> :
+        <Text style={[styles.main, styles.theme[`${type}Main`]]}>
+          {`${balance} ${token}`}
+        </Text>
+      }
+    </Animated.View>
   );
 };
 
-export default withTheme(homeHeaderTitle, getStyles());
+const SimpleHeader = ({
+  styles, type, interpolate = () => 1, title,
+}) => (
+  <Animated.Text
+    numberOfLines={1}
+    style={[
+      styles.main,
+      styles.theme[`${type}Main`],
+      { opacity: interpolate([0, 100, 130], [1, 1, 0]) },
+    ]}
+    accessibilityTraits="header"
+    allowFontScaling={false}>
+    {title}
+  </Animated.Text>
+);
+
+const HomeHeaderTitle = ({
+  styles, theme, children: data,
+}) => (
+  <View style={styles.container}>
+    <SimpleHeader
+      styles={styles}
+      type={typeof data === 'string' ? 'home' : data.type}
+      title={typeof data === 'string' ? data : data.placeHolder}
+      interpolate={typeof data === 'string' ? () => 1 : data.interpolate}
+      />
+    {
+      typeof data !== 'string' ?
+      <ExtendedTitle
+        balance={data.balance !== undefined ? fromRawLsk(data.balance) : '0'}
+        theme={theme}
+        token={data.token}
+        styles={styles}
+        interpolate={data.interpolate}
+        address={data.address}
+        incognito={data.incognito}
+        type={data.type}
+        /> : null
+    }
+  </View>
+);
+
+export default withTheme(HomeHeaderTitle, getStyles());
