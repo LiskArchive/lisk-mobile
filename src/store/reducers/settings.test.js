c930d3ee0f184874114c4868054ebfdfb3d21d62
reyraa <ali@lightcurve.io>
2019-03-11 10:33:21 +0100

    Improve the uni test coverage for settings

--- src/store/reducers/settings.test.js
+++ src/store/reducers/settings.test.js
@@ -2,27 +2,46 @@ import settings, { INITIAL_STATE } from './settings';
 import actionTypes from '../../constants/actions';
 
 describe('Reducers: Settings', () => {
-  let state;
-
-  beforeEach(() => {
-    state = {};
-  });
-
   it('should create the empty state initially', () => {
     const createdState = settings();
     expect(createdState).toEqual(INITIAL_STATE);
   });
 
-  it('should return updated state in case of actionTypes.settingsUpdated', () => {
-    const action = {
-      type: actionTypes.settingsUpdated,
-      data: {
+  describe('settingsUpdated', () => {
+    let state;
+    beforeEach(() => {
+      state = {};
+    });
+
+    it('should return updated state in case of actionTypes.settingsUpdated', () => {
+      const action = {
+        type: actionTypes.settingsUpdated,
+        data: {
+          sensorType: 'Face ID',
+        },
+      };
+      const changedState = settings(state, action);
+      expect(changedState).toEqual({
         sensorType: 'Face ID',
-      },
-    };
-    const changedState = settings(state, action);
-    expect(changedState).toEqual({
-      sensorType: 'Face ID',
+      });
+    });
+  });
+
+  describe('settingsRetrieved', () => {
+    it('should should always start with LSK in case of actionTypes.settingsRetrieved', () => {
+      const action = {
+        type: actionTypes.settingsRetrieved,
+        data: {
+          token: {
+            active: 'BTC',
+            list: ['LSK', 'BTC'],
+          },
+        },
+      };
+      const state = {};
+
+      const changedState = settings(state, action);
+      expect(changedState.token.active).toEqual('LSK');
     });
   });
 });
