f380d5c46ce8630d427aa4b86e4d71fc038508cf
Altay Aydemir <altay.aydemir@lightcurve.io>
2019-03-12 11:39:42 +0100

    Update Send/Progress color based on active token

--- src/components/send/progress.js
+++ src/components/send/progress.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { View, Animated } from 'react-native';
+import connect from 'redux-connect-decorator';
+import { tokenMap } from '../../constants/tokens';
 import withTheme from '../withTheme';
 import getStyles from './styles';
+import { colors } from '../../constants/styleGuide';
 
+@connect(state => ({
+  activeToken: state.settings.token.active,
+}))
 class Progress extends React.Component {
   state = {
     progressRatio: new Animated.Value(0),
@@ -26,8 +32,15 @@ class Progress extends React.Component {
   }
 
   render() {
-    const { styles, current, total } = this.props;
+    const {
+      styles, current, total, activeToken,
+    } = this.props;
     const { progressRatio } = this.state;
+
+    let color = colors.light.blue;
+    if (activeToken === tokenMap.BTC.key) {
+      color = colors.light.btc;
+    }
 
     return (
       <View style={[
@@ -36,7 +49,8 @@ class Progress extends React.Component {
         { opacity: current === total ? 0 : 1 },
       ]}>
         <Animated.View
-          style={[styles.progress, styles.theme.progress, {
+          style={[styles.progress, {
+            backgroundColor: color,
             width: progressRatio.interpolate({
               inputRange: [0, 1],
               outputRange: ['0%', '120%'],
