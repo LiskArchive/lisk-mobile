88aedc53da23fa687d89e410406c6d47fce6cfdb
yasharAyari <yashar@lightcurve.io>
2019-04-10 13:12:44 +0200

    Add react-native-shake to the list of ios and android dependencies

--- android/app/src/main/java/io/lisk/mobile/MainApplication.java
+++ android/app/src/main/java/io/lisk/mobile/MainApplication.java
@@ -3,6 +3,7 @@ package io.lisk.mobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.clipsub.RNShake.RNShakeEventPackage;
 import com.reactNativeQuickActions.AppShortcutsPackage;
 import com.mkuczera.RNReactNativeHapticFeedbackPackage;
 import com.wix.interactable.Interactable;
@@ -42,6 +43,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
+            new RNShakeEventPackage(),
             new AppShortcutsPackage(),
         new RNReactNativeHapticFeedbackPackage(),
         new Interactable(),
