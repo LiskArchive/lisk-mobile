712eed4d64bb1651b77485e457e19c446d780505
Altay Aydemir <altay.aydemir@lightcurve.io>
2019-04-12 15:52:35 +0200

    Update api url config for BTC

--- btc.config.js
+++ btc.config.js
@@ -4,7 +4,7 @@ const isTestnet = process.env.network === 'testnet';
 
 export default {
   isTestnet,
-  url: isTestnet ? 'http://localhost:7201' : 'https://blockchain.info',
+  url: isTestnet ? 'https://ams1-bitcoin-test-001.ed148.net' : 'https://ams1-bitcoin-001.ed148.net',
   minerFeesURL: 'https://bitcoinfees.earn.com/api/v1/fees/recommended',
   network: isTestnet ? bitcoin.networks.testnet : bitcoin.networks.bitcoin,
   derivationPath: isTestnet ? "m/44'/1'/0'/0/0" : "m/44'/0'/0'/0/0",
