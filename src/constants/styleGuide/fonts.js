a675093461f06bcfb6741f18444678607b1dc270
Altay Aydemir <altay.aydemir@lightcurve.io>
2019-04-26 10:57:37 +0200

    Update unrevealed passphrase font

--- src/constants/styleGuide/fonts.js
+++ src/constants/styleGuide/fonts.js
@@ -4,7 +4,7 @@ export default {
     context: 'OpenSans-Regular',
     contextBold: 'OpenSans-Bold',
     contextSemiBold: 'OpenSans-SemiBold',
-    passphrase: 'Dots',
+    passphrase: 'Dots-Regular',
     passphraseText: 'PTMono-Regular',
   },
   size: {
