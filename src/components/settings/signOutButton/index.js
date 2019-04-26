763262c986398c228f2b3bb92dc1d231c5b47898
Altay Aydemir <altay.aydemir@lightcurve.io>
2019-04-26 15:49:31 +0200

    Update color of the settings/signOutButton component

--- src/components/settings/signOutButton/index.js
+++ src/components/settings/signOutButton/index.js
@@ -4,7 +4,7 @@ import { NavigationActions } from 'react-navigation';
 import { translate } from 'react-i18next';
 import { removePassphraseFromKeyChain } from '../../../utilities/passphrase';
 import { IconButton } from '../../toolBox/button';
-import { colors } from '../../../constants/styleGuide';
+import { colors, themes } from '../../../constants/styleGuide';
 import withTheme from '../../withTheme';
 import getStyles from './styles';
 
@@ -53,9 +53,9 @@ class SignOutButton extends React.Component {
     return (
       <IconButton
         icon='logout'
-        iconSize={18}
+        iconSize={16}
         title={t('Sign out')}
-        color={colors[theme].blue}
+        color={theme === themes.light ? colors.light.ultramarineBlue : colors.dark.blue}
         onClick={this.onClick}
         titleStyle={[styles.title, styles.theme.title]}
         style={styles.button}
