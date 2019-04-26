b6e59af877ac0cab2a4cb41deb5f59424ba7158f
Altay Aydemir <altay.aydemir@lightcurve.io>
2019-04-26 15:30:38 +0200

    Update toolbox/switchButton component for new design

--- src/components/toolBox/switchButton.js
+++ src/components/toolBox/switchButton.js
@@ -18,8 +18,8 @@ const switchButton = ({
       style={style}
       width={43}
       onSyncPress={onPress}
-      backgroundActive={theme === themes.light ? colors.light.blue : colors.dark.blue}
-      backgroundInactive={colors.dark.gray3}
+      backgroundActive={theme === themes.light ? colors.light.ultramarineBlue : colors.dark.blue}
+      backgroundInactive={theme === themes.light ? colors.light.platinum : colors.dark.gray3}
     />
   );
 };
