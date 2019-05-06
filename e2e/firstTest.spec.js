7882f1c6af233a50463f69ffb661ed66ee3703c1
andreiklimenok <jaguarislove@gmail.com>
2019-05-06 15:15:42 +0200

    initial detox setup

---
+++ e2e/firstTest.spec.js
@@ -0,0 +1,10 @@
+/* eslint no-undef: 0 */
+describe('Example', () => {
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should have welcome screen', async () => {
+    await expect(element(by.id('welcome'))).toBeVisible();
+  });
+});
