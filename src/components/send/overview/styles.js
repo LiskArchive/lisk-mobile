3c82a882975c403980f2f8d1796ee333a3ea8e62
Sergio Colado <sergio.colado@lightcurve.io>
2019-05-10 10:02:54 +0200

    Change address style on dark mode

--- src/components/send/overview/styles.js
+++ src/components/send/overview/styles.js
@@ -124,7 +124,7 @@ export default () => ({
       color: colors.dark.white,
     },
     address: {
-      color: colors.dark.white,
+      color: colors.light.platinum,
     },
     label: {
       color: colors.dark.gray4,
