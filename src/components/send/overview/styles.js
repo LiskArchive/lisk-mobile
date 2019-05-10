d96e21689dfc336cc7d87a1e2cc124dd946477fc
Sergio Colado <sergio.colado@lightcurve.io>
2019-05-10 09:55:15 +0200

    Revert "Change address style on dark mode"
    This reverts commit 5acd4d745db2330bf0047fc47ca55097ca987203.

--- src/components/send/overview/styles.js
+++ src/components/send/overview/styles.js
@@ -124,7 +124,7 @@ export default () => ({
       color: colors.dark.white,
     },
     address: {
-      color: colors.dark.gray6,
+      color: colors.dark.white,
     },
     label: {
       color: colors.dark.gray4,
