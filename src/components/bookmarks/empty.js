c6539b74a00bb747ceec7c7d75f10d7bdcf1cbbb
Sergio Colado <sergio.colado@lightcurve.io>
2019-05-13 16:40:47 +0200

    Replace illustration for no bookmarks

--- src/components/bookmarks/empty.js
+++ src/components/bookmarks/empty.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { View, Image } from 'react-native';
 import { translate } from 'react-i18next';
-import noActivityLight from '../../assets/images/send/noBookmarks3xLight.png';
-import noActivityDark from '../../assets/images/send/noBookmarks3xDark.png';
+import noBookmarkLightImg from '../../assets/images/send/noBookmarks3xLight.png';
+import noBookmarkDarkImg from '../../assets/images/send/noBookmarks3xDark.png';
 import { P } from '../toolBox/typography';
 import withTheme from '../withTheme';
 import getStyles from './styles';
@@ -12,24 +12,17 @@ const EmptyState = ({
   theme, styles, usedIn, t,
 }) => (
   <View style={styles.emptyState}>
-    <View style={styles.noActivity}>
-      {
-        theme === themes.light ?
-          <Image
-            style={styles.emptyImage}
-            source={noActivityLight}
-          /> :
-          <Image
-            style={styles.emptyImage}
-            source={noActivityDark}
-          />
-      }
+    <View style={styles.imageContainer}>
+      <Image
+        style={styles.noBookmarkImage}
+        source={theme === themes.light ? noBookmarkLightImg : noBookmarkDarkImg}
+      />
     </View>
-    <P style={[styles.emptyTitle, styles.theme.emptyTitle]}>
+    <P style={styles.emptyTitle}>
       {
         usedIn === 'bookmarks' ?
         t('You don’t have any bookmarks. Start adding them by tapping the + button on the top left.') :
-        t('You don’t have any bookmarks. Start adding then through the sending process.')
+        t('You have no bookmarks yet.')
       }
     </P>
   </View>
