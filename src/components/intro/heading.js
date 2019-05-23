be93c346230999d3a9dd69d512bffb8d5b121f61
Sergio Colado <sergio.colado@lightcurve.io>
2019-05-23 14:24:00 +0200

    Remove animation logic

--- src/components/intro/heading.js
+++ src/components/intro/heading.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Animated, View, Image } from 'react-native';
+import { View, Image } from 'react-native';
 import Swiper from 'react-native-swiper';
 import { translate } from 'react-i18next';
 import { H2, P } from '../toolBox/typography';
@@ -11,48 +11,6 @@ import secureAuthenticationImg from '../../assets/images/intro/secureAuthenticat
 import easyAccessImg from '../../assets/images/intro/easyAccess3x.png';
 
 class Heading extends React.Component {
-  state = {
-    bgOpacity: new Animated.Value(0),
-    logoOpacity: new Animated.Value(0),
-    frameOpacity: new Animated.Value(1),
-    activityOpacity: new Animated.Value(1),
-    transactionsOpacity: new Animated.Value(0),
-    bioAuthOpacity: new Animated.Value(0),
-    index: 0,
-  }
-
-  onIndexChanged = (index) => {
-    this.setState({ index });
-    this.animate(index);
-  }
-
-  animate = (index) => {
-    const {
-      frameOpacity, activityOpacity, transactionsOpacity, bioAuthOpacity,
-    } = this.state;
-    const values = [
-      [1, 1, 0, 0],
-      [1, 0, 1, 0],
-      [0, 0, 0, 1],
-    ];
-    Animated.timing(frameOpacity, {
-      toValue: values[index][0],
-      duration: 400,
-    }).start();
-    Animated.timing(activityOpacity, {
-      toValue: values[index][1],
-      duration: 400,
-    }).start();
-    Animated.timing(transactionsOpacity, {
-      toValue: values[index][2],
-      duration: 400,
-    }).start();
-    Animated.timing(bioAuthOpacity, {
-      toValue: values[index][3],
-      duration: 400,
-    }).start();
-  };
-
   render() {
     const { t } = this.props;
 
@@ -86,13 +44,13 @@ class Heading extends React.Component {
     ];
 
     return (
-      <Animated.View style={styles.headingContainer}>
-          <Swiper
-            loop={false}
-            onIndexChanged={this.onIndexChanged}
-            dotColor={colors.light.white}
-            dotStyle={styles.dot}
-            activeDotColor={colors.light.ultramarineBlue}
+      <View style={styles.headingContainer}>
+        <Swiper
+          loop={false}
+          onIndexChanged={this.onIndexChanged}
+          dotColor={colors.light.white}
+          dotStyle={styles.dot}
+          activeDotColor={colors.light.ultramarineBlue}
           paginationStyle={styles.headingPagination}
         >
           {descriptionContent.map((item, index) => (
@@ -100,14 +58,14 @@ class Heading extends React.Component {
               <View style={styles.descriptionWrapper}>
                 <H2 style={[styles.centralized, styles.descriptionH]}>{t(item.title)}</H2>
                 <P style={[styles.centralized, styles.descriptionP]}>{t(item.description)}</P>
-            </View>
+              </View>
               <View style={styles.illustrationWrapper}>
                 <Image resizeMethod={'scale'} source={item.imageSrc} style={styles.illustration} />
               </View>
             </View>
           ))}
-          </Swiper>
-        </Animated.View>
+        </Swiper>
+      </View>
     );
   }
 }
