08f2059c7afde3e69068ca6573b53495f672b436
Sergio Colado <sergio.colado@lightcurve.io>
2019-06-06 17:20:03 +0200

    Change header style

--- src/components/router/headerBackground/index.js
+++ src/components/router/headerBackground/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { View } from 'react-native';
 import withTheme from '../../withTheme';
-import { colors } from '../../../constants/styleGuide';
+import { colors, themes } from '../../../constants/styleGuide';
+import { setColorOpacity } from '../../../utilities/helpers';
 
 const HeaderBackground = ({ theme }) => (
-  <View style={{
-    flex: 1, backgroundColor: colors[theme].headerBg,
-  }}>
-  </View>
+  <View
+    style={{
+      flex: 1,
+      backgroundColor: colors[theme].headerBg,
+      borderBottomWidth: 1,
+      borderBottomColor: theme === themes.light
+        ? colors.light.whiteSmoke
+        : setColorOpacity(colors.light.white, 0.24),
+    }}
+  />
 );
 
 export default withTheme(HeaderBackground, {});
