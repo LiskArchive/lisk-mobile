98be702b25c839115d872c6f78d4f64a277d11b4
Sergio Colado <sergio.colado@lightcurve.io>
2019-07-12 09:55:30 +0200

    Fix typo

--- android/app/src/main/java/io/lisk/mobile/MainActivity.java
+++ android/app/src/main/java/io/lisk/mobile/MainActivity.java
@@ -5,7 +5,7 @@ import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootVie
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
     @Override
