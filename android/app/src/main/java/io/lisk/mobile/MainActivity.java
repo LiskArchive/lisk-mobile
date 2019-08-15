8eae316bedb1384722162b08cec1bdd798648aee
yasharAyari <yashar@lightcurve.io>
2019-08-15 12:17:08 +0200

    Merge a conflict that happens after rabasing to update-rn-and-dependencies

--- android/app/src/main/java/io/lisk/mobile/MainActivity.java
+++ android/app/src/main/java/io/lisk/mobile/MainActivity.java
@@ -33,14 +33,4 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "Lisk";
     }
-
-    @Override
-    protected ReactActivityDelegate createReactActivityDelegate() {
-        return new ReactActivityDelegate(this, getMainComponentName()) {
-            @Override
-            protected ReactRootView createRootView() {
-                return new RNGestureHandlerEnabledRootView(MainActivity.this);
-            }
-        };
-}
 }
