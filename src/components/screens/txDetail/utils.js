5729ce69d3b05ccf0d0a4520d4d177d95a4f0ac3
yasharAyari <yashar@lightcurve.io>
2020-02-20 17:39:36 +0100

    Move some utility methods to a separated util file in txDetail component

---
+++ src/components/screens/txDetail/utils.js
@@ -0,0 +1,42 @@
+
+
+import { Linking } from 'react-native';
+import { getTransactionExplorerURL } from '../../../utilities/api/btc/transactions';
+
+export const goToWallet = (address, { navigation, account, activeToken }) => {
+  if (
+    address !== account[activeToken].address &&
+    address !== 'Unparsed Address'
+  ) {
+    navigation.navigate('Wallet', { address });
+  }
+};
+
+export const getAccountLabel = (address, { t, followedAccounts, activeToken }) => {
+  if (address === 'Unparsed Address') {
+    return t('Unparsed Address');
+  }
+
+  const followedAccount = followedAccounts[activeToken].find(
+    a => a.address === address
+  );
+  if (followedAccount) {
+    return followedAccount.label;
+  }
+
+  return address;
+};
+
+export const openExplorer = (id) => {
+  Linking.openURL(getTransactionExplorerURL(id))
+    // eslint-disable-next-line no-console
+    .catch(err => console.error('An error occurred', err));
+};
+
+export const getAccountTitle = tx => {
+  if (tx.type === 3) return 'Voter';
+  else if (tx.type === 2) return 'Registrant';
+  else if (tx.type !== 0 || tx.recipientAddress === tx.senderAddress)
+    return 'Account address';
+  return 'Sender';
+};
