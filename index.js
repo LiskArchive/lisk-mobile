ef8b07284d4f01e9ef020b1acd54c430d1fc7323
Ali Haghighatkhah <ali@lightcurve.io>
2021-03-12 09:44:59 +0100

    Read app name from app.json

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ import './shim';
 import { AppRegistry, Text } from 'react-native'; // eslint-disable-line
 import 'react-native-gesture-handler'; // eslint-disable-line
 import App from './src/App';
+import { name as appName } from './app.json';
 
 if (Text.defaultProps == null) Text.defaultProps = {};
 Text.defaultProps.allowFontScaling = false;
 
-AppRegistry.registerComponent('Lisk', () => App);
+AppRegistry.registerComponent(appName, () => App);
