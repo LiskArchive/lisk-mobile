78353956c60e8a49734ab533e6e2d84d522a7601
Ali Haghighatkhah <ali@lightcurve.io>
2021-03-14 08:07:03 +0100

    Fix component prop names

--- src/components/screens/router/tabBarIcon/index.js
+++ src/components/screens/router/tabBarIcon/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Icon from '../../../shared/toolBox/icon';
 
-const TabBarIcon = ({ name, tintColor, focused }) => (
-  <Icon size={20} name={focused ? `${name}-filled` : name} color={tintColor} />
+const TabBarIcon = ({ name, color, focused }) => (
+  <Icon size={20} name={focused ? `${name}-filled` : name} color={color} />
 );
 
 export default TabBarIcon;
