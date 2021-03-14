987a7ce909ff294825848acb2fbb717a565412d0
Ali Haghighatkhah <ali@lightcurve.io>
2021-03-14 08:09:40 +0100

    Retun a function definition

--- src/components/screens/router/headerPlaceholderButton/index.js
+++ src/components/screens/router/headerPlaceholderButton/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import { IconButton } from '../../../shared/toolBox/button';
 
-export default <IconButton color="transparent" icon="back" />;
+export default () => <IconButton color="transparent" icon="back" />;
