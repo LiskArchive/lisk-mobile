401382330f40b449bb658479e447e2d9e7eaba41
Ali Haghighatkhah <ali@lightcurve.io>
2021-03-14 08:14:04 +0100

    Import safeAreaView from react-native-safe-area-context

--- src/components/screens/intro/index.js
+++ src/components/screens/intro/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View } from 'react-native';
 import connect from 'redux-connect-decorator';
 import SplashScreen from 'react-native-splash-screen';
-import { SafeAreaView } from 'react-navigation';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { translate } from 'react-i18next';
 import Splash from './splash';
 import Heading from './heading';
