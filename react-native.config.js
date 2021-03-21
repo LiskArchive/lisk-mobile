f7469f1b10b04b2c99c57c04c0bc6eb22894472e
Ali Haghighatkhah <ali@lightcurve.io>
2021-03-21 22:08:27 +0100

    Use single quote

--- react-native.config.js
+++ react-native.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-    project: {
-      ios: {},
-      android: {},
-    },
-    assets: ["./src/assets/fonts/"],
-  };
-  
+  project: {
+    ios: {},
+    android: {},
+  },
+  assets: ['./src/assets/fonts/'],
+};
