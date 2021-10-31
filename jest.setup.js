2ed70fcf638efe4b37da59805f3414f6a4828964
Daniel Adepoju <adebalanced02@gmail.com>
2021-10-31 21:58:21 +0100

    :white_check_mark: Test setup for components testing

--- jest.setup.js
+++ jest.setup.js
@@ -3,9 +3,27 @@ import { NativeModules } from 'react-native';
 NativeModules.RNCNetInfo = {
   getCurrentState: jest.fn(() => Promise.resolve()),
   addListener: jest.fn(),
-  removeListeners: jest.fn(),
+  removeListeners: jest.fn()
 };
 
 jest.mock('react-native-appearance', () => ({
-  Appearance: { getColorScheme: jest.fn() },
+  Appearance: { getColorScheme: jest.fn() }
+}));
+
+jest.mock('react-native-gesture-handler', () => ({
+  TouchableOpacity: jest.fn().mockImplementation(({ children }) => children)
+}));
+
+jest.mock('@react-navigation/stack', () => ({
+  useHeaderHeight: jest.fn(),
+  createStackNavigator: () => ({
+    Navigator: jest.fn(),
+    Screen: jest.fn()
+  })
+}));
+
+jest.mock('@react-navigation/compat', () => ({ withNavigation: (component) => component }));
+
+jest.mock('react-native-keyboard-aware-scroll-view', () => ({
+  KeyboardAwareScrollView: jest.fn().mockImplementation(({ children }) => children)
 }));
