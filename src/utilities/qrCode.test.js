2cd69c3df6e15ef68102c32e0c32466bb5bb2784
Daniel Adepoju <adebalanced02@gmail.com>
2022-01-13 08:07:01 +0100

    :white_check_mark: Update test for qr code utils

--- src/utilities/qrCode.test.js
+++ src/utilities/qrCode.test.js
@@ -10,6 +10,15 @@ describe('QR Code Helper', () => {
     });
   });
 
+  it('decodes QR data that contains URL and reference with space', () => {
+    const data = 'lisk://wallet?recipient=1L&amount=1&reference=test%20mobile';
+    expect(qrCode.decodeLaunchUrl(data)).toEqual({
+      address: '1L',
+      amount: '1',
+      reference: 'test mobile',
+    });
+  });
+
   it('decodes QR data just address', () => {
     const data = '1L';
     expect(qrCode.decodeLaunchUrl(data)).toEqual({
