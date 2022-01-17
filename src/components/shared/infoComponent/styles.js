a44ca1aa9077d7f021a8d83f31fa46d407555e4a
Daniel Adepoju <adebalanced02@gmail.com>
2022-01-17 09:40:46 +0100

    :house: Fix lint issue

--- src/components/shared/infoComponent/styles.js
+++ src/components/shared/infoComponent/styles.js
@@ -1,4 +1,4 @@
-import { themes, colors, fonts } from '../../../constants/styleGuide';
+import { themes, colors } from '../../../constants/styleGuide';
 import { setColorOpacity } from '../../../utilities/helpers';
 
 export default () => ({
