914f7a1f7819484595a961fb21b3814661415417
Daniel Adepoju <adebalanced02@gmail.com>
2022-01-21 16:05:48 +0100

    :house: illustration size for screens

--- src/components/screens/banners/BtcRemoval.js
+++ src/components/screens/banners/BtcRemoval.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-nested-ternary */
 import React from 'react';
 import { translate } from 'react-i18next';
 import { Linking, View, TouchableOpacity } from 'react-native';
@@ -8,20 +9,23 @@ import { B, H2 } from '../../shared/toolBox/typography';
 import withTheme from '../../shared/withTheme';
 import getStyles from './styles';
 import packageJson from '../../../../package.json';
+import { deviceHeight } from '../../../utilities/device';
 
 const BTCRemoval = ({ styles, t, closeBanner }) => {
+  const height = deviceHeight();
   const readMore = () =>
     Linking.openURL('https://lisk.com/blog/development/lisk-desktop-220-release');
   return (
     <View style={styles.container}>
+      <H2 style={styles.title}>
+        {t('banners.btcRemoval.title', { release: packageJson.version })}
+      </H2>
       <View style={styles.content}>
-        <H2 style={styles.title}>{t('banners.btcRemoval.title', { release: packageJson.version })}</H2>
-        <BTCRemovalSVG />
+        <View style={styles.svgContainer}>
+          <BTCRemovalSVG size={height < 700 ? 0.7 : height > 850 ? 1 : 0.8} />
+        </View>
       </View>
-      <TouchableOpacity
-        style={styles.readMoreButton}
-        onPress={readMore}
-      >
+      <TouchableOpacity style={styles.readMoreButton} onPress={readMore}>
         <B style={styles.readMoreText}>{t('banners.btcRemoval.buttons.readMore')}</B>
         <View style={styles.icon}>
           <LinkSvg />
--- src/components/screens/banners/styles.js
+++ src/components/screens/banners/styles.js
@@ -1,33 +1,41 @@
 import { colors } from '../../../constants/styleGuide';
+import { deviceHeight } from '../../../utilities/device';
+
+const height = deviceHeight();
 
 export default () => ({
   common: {
     container: {
       flex: 1,
+      paddingTop: height > 700 ? 50 : 20
     },
     content: {
       flex: 1,
       alignItems: 'center',
-      justifyContent: 'space-around'
+      justifyContent: 'center',
+      paddingBottom: 20
+    },
+    svgContainer: {
+      padding: 10
     },
     title: {
       textAlign: 'center',
       color: colors.light.white,
-      paddingBottom: 30,
+      padding: 20
     },
     readMoreButton: {
-      marginBottom: 10,
+      marginBottom: 20,
       backgroundColor: 'transparent',
       flexDirection: 'row',
       alignItems: 'center',
       justifyContent: 'center',
-      padding: 10,
+      padding: 10
     },
     icon: {
-      paddingLeft: 10,
+      paddingLeft: 10
     },
     readMoreText: {
-      color: colors.light.white,
+      color: colors.light.white
     },
     gotItText: {
       color: colors.light.ultramarineBlue
