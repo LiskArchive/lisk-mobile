9d13bc45d819aa63d03da29a53f967a28da13892
Daniel Adepoju <adebalanced02@gmail.com>
2022-01-24 18:48:57 +0100

    :nail_care title position fix across decives and screens

--- src/components/screens/router/headerBackButton/styles.js
+++ src/components/screens/router/headerBackButton/styles.js
@@ -3,7 +3,7 @@ import { deviceType } from '../../../../utilities/device';
 
 const type = deviceType();
 let normalMarginTop = type === 'iOSx' ? -3 : 0;
-let safeAreaMarginTop = type === 'iOSx' ? 50 : 20;
+let safeAreaMarginTop = type === 'iOSx' ? 45 : 20;
 
 if (type === 'android') {
   normalMarginTop = 0;
