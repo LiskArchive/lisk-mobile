e62fa0598bc86551c50988cd6e118359f197e84b
Daniel Adepoju <adebalanced02@gmail.com>
2022-04-25 08:30:54 +0100

    :bug: Fix import error

--- src/packages/Accounts/utils/index.js
+++ src/packages/Accounts/utils/index.js
@@ -1,4 +1,67 @@
 import moment from 'moment';
+import ModalHolder from 'utilities/modal';
+import { tokenMap } from 'constants/tokens';
+import { fromRawLsk } from 'utilities/conversions';
+import IntroModal from '../introModal';
+import InitializationModal from '../initializationModal';
+
+export const showIntroModal = ({ btcIntroShown, settingsUpdated }) => {
+  if (!btcIntroShown) {
+    ModalHolder.open({
+      title: 'We’ve got good news!',
+      component: IntroModal,
+    });
+    settingsUpdated({ btcIntroShown: true });
+  }
+};
+
+export const loadMore = ({
+  activeToken,
+  account,
+  transactionsLoaded,
+  transactions,
+}) => {
+  if (account[activeToken]) {
+    transactionsLoaded({
+      address: account[activeToken].address,
+      offset: transactions.confirmed.length,
+    });
+  }
+};
+
+export const showInitializationModal = ({
+  account, activeToken, transactions, navigation
+}) => {
+  const balance = parseFloat(fromRawLsk(account[tokenMap.LSK.key].balance));
+
+  if (
+    activeToken === tokenMap.LSK.key
+    && !account[activeToken].initialized
+    && (!transactions || transactions.pending?.length < 1)
+    && balance >= 0.2
+  ) {
+    ModalHolder.open({
+      title: 'Initialize your account',
+      component: InitializationModal,
+      callback: () =>
+        navigation.navigate({ name: 'Send', params: { initialize: true } }),
+    });
+  }
+};
+
+export const resetTxAndFetch = ({
+  transactionsReset,
+  transactionsLoaded,
+  account,
+  activeToken,
+}) => {
+  transactionsReset();
+  // giving some time for the transition animations to settle
+  transactionsLoaded({
+    address: account[activeToken].address,
+    offset: 0,
+  });
+};
 
 export const getPendingTime = (unvoteHeight, unlockHeight) => {
   const awaitingBlocks = unlockHeight - unvoteHeight;
