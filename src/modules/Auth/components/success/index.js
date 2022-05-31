2a0efe017df30face69573f4546a00fe9a4c123f
Daniel Adepoju <adebalanced02@gmail.com>
2022-05-31 21:09:52 +0100

    :seedling: implement password success screen

---
+++ src/modules/Auth/components/success/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import withTheme from 'components/shared/withTheme';
+import { PrimaryButton } from 'components/shared/toolBox/button';
+import getStyles from './styles';
+
+const SuccessScreen = ({
+  Illustration, title, description, styles, onContinue, buttonText, children
+}) => <View style={[styles.wrapper, styles.theme.wrapper]} >
+    <View style={[styles.container]} >
+      <View style={styles.illustration} >
+        <Illustration />
+      </View>
+      <Text style={[styles.title, styles.theme.title]} >{title}</Text>
+      <Text style={[styles.description, styles.theme.description]} >{description}</Text>
+      {children}
+    </View>
+    <PrimaryButton
+      title={buttonText}
+      style={styles.continueButton}
+      noTheme
+      onPress={onContinue}
+    />
+  </View>;
+
+export default withTheme(SuccessScreen, getStyles());
