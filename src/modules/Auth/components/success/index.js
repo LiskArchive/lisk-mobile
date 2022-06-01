4dbb06dc592eb8d635c3b36844436b31440623d3
Daniel Adepoju <adebalanced02@gmail.com>
2022-06-01 12:02:13 +0100

    :house: PR Review suggestion

--- src/modules/Auth/components/success/index.js
+++ src/modules/Auth/components/success/index.js
@@ -5,20 +5,20 @@ import { PrimaryButton } from 'components/shared/toolBox/button';
 import getStyles from './styles';
 
 const SuccessScreen = ({
-  Illustration, title, description, styles, onContinue, buttonText, children
+  illustration, title, description, styles, onContinue, buttonText, children
 }) => <View style={[styles.wrapper, styles.theme.wrapper]} >
     <View style={[styles.container]} >
       <View style={styles.illustration} >
-        <Illustration />
+        {illustration}
       </View>
       <Text style={[styles.title, styles.theme.title]} >{title}</Text>
       <Text style={[styles.description, styles.theme.description]} >{description}</Text>
       {children}
     </View>
     <PrimaryButton
+      noTheme
       title={buttonText}
       style={styles.continueButton}
-      noTheme
       onPress={onContinue}
     />
   </View>;
