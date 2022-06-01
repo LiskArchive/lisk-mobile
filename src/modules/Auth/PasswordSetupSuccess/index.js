69c221aa4b4e4c1f7af2ebe0d9b591778ef2c0d0
Daniel Adepoju <adebalanced02@gmail.com>
2022-06-01 16:19:41 +0100

    :house: implement download json file

--- src/modules/Auth/PasswordSetupSuccess/index.js
+++ src/modules/Auth/PasswordSetupSuccess/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import withTheme from 'components/shared/withTheme';
 import FlowerSuccessSvg from 'assets/svgs/FlowerSuccessSvg';
@@ -6,26 +6,40 @@ import FileSvg from 'assets/svgs/FileSvg';
 import DownloadSvg from 'assets/svgs/DownloadSvg';
 import SuccessScreen from '../components/success';
 import getStyles from './styles';
+import { downloadJSON } from '../utils';
 
-const PasswordSetupSuccess = ({ styles }) => <SuccessScreen
-  illustration={<FlowerSuccessSvg />}
-  title="Perfect! You're all set"
-  description="You can now download encrypted secret recovery phrase."
-  buttonText="Continue to Dashboard" >
-  <View>
-    <View style={[styles.downloadFile]} >
-      <View style={[styles.file]} >
-        <FileSvg />
+const jsonData = { dummy: 'Dummy data' };
+
+const PasswordSetupSuccess = ({ styles }) => {
+  const [downloaded, setDownloaded] = useState(false);
+  const downloadFile = () => downloadJSON(jsonData, (e) => {
+    if (!e) {
+      setDownloaded(true);
+    }
+  });
+
+  return <SuccessScreen
+    illustration={<FlowerSuccessSvg />}
+    title="Perfect! You're all set"
+    description="You can now download encrypted secret recovery phrase."
+    buttonText="Continue to Dashboard"
+    disabled={!downloaded}
+  >
+    <View>
+      <View style={[styles.downloadFile]} >
+        <View style={[styles.file]} >
+          <FileSvg />
+        </View>
+        <Text style={[styles.text, styles.theme.text]} >encrypted_secret_recovery_phrase.json</Text>
       </View>
-      <Text style={[styles.text, styles.theme.text]} >encrypted_secret_recovery_phrase.json</Text>
-    </View>
-    <TouchableOpacity style={[styles.downloadFile]}>
+      <TouchableOpacity style={[styles.downloadFile]} onPress={downloadFile} >
         <Text style={[styles.download]}>Download</Text>
-      <View style={[styles.file]} >
-        <DownloadSvg style={[styles.file]} />
-      </View>
-    </TouchableOpacity>
-  </View>
-</SuccessScreen>;
+        <View style={[styles.file]} >
+          <DownloadSvg style={[styles.file]} />
+        </View>
+      </TouchableOpacity>
+    </View>
+  </SuccessScreen>;
+};
 
 export default withTheme(PasswordSetupSuccess, getStyles());
