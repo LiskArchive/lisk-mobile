2f7263f43b597923588ae0717bd46569cb85b93a
ClementeSerrano <clementeserranosutil@gmail.com>
2022-07-14 17:02:59 +0200

    :necktie: Add useBlockchainApplications hook

---
+++ src/modules/BlockchainApplication/hooks/useBlockchainApplications.js
@@ -0,0 +1,75 @@
+import { useCallback, useEffect, useState } from 'react';
+
+const APPLICATIONS_MOCK = [
+  {
+    name: 'Lisk',
+    chainID: 'aq02qkbb35u4jdq8szo3pnsq',
+    state: 'active',
+    serviceURLs: ['https://service.lisk.com'],
+    lastUpdated: 123456789,
+    isDefault: true,
+  },
+  {
+    name: 'Colecti',
+    chainID: 'mi34vyyd12g2lkf0rza1irws',
+    state: 'active',
+    serviceURLs: ['https://service.colecti.com'],
+    lastUpdated: 123789456,
+    isDefault: true,
+  },
+  {
+    name: 'Enevti',
+    chainID: 'aq86llsb35u4syc8aet7xenf',
+    state: 'active',
+    serviceURLs: ['https://service.enevti.com'],
+    lastUpdated: 456123789,
+    isDefault: true,
+  },
+  {
+    name: 'DoEdu',
+    chainID: 'aq96eeqk77r4syc8aet9fcey',
+    state: 'active',
+    serviceURLs: ['https://service.doedu.com'],
+    lastUpdated: 789123456,
+    isDefault: false,
+  },
+  {
+    name: 'Kalipo',
+    chainID: 'aq25derd17a4syc8aet3pryt',
+    state: 'active',
+    serviceURLs: ['https://service.kalipo.com'],
+    lastUpdated: 789456123,
+    isDefault: false,
+  },
+  {
+    name: 'Lisk DEX',
+    chainID: 'dz38fkbb35u4jdq8szo3pnsq',
+    state: 'active',
+    serviceURLs: ['https://service.liskdex.com'],
+    lastUpdated: 123456789,
+    isDefault: true,
+  },
+];
+
+export function useBlockchainApplications() {
+  const [data, setData] = useState(undefined);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(undefined);
+
+  const query = useCallback(() => {
+    new Promise((resolve) => {
+      setTimeout((() => {
+        resolve({ data: APPLICATIONS_MOCK });
+      }), 2000);
+    }).then(res => {
+      setData(res.data);
+      setLoading(false);
+    }).catch(err => setError(err));
+  }, []);
+
+  useEffect(() => {
+    query();
+  }, []);
+
+  return { data, loading, error };
+}
