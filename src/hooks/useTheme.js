c994b6cf353c827a0e6fcd5f2cc1f89d252052ed
ClementeSerrano <clementeserranosutil@gmail.com>
2022-07-18 17:37:00 +0200

    :memo: Add docs for useTheme hook

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -3,6 +3,12 @@ import { useContext } from 'react';
 import ThemeContext from '../contexts/theme';
 import { createThemedStyles } from '../utilities/helpers';
 
+/**
+ * Hook for consuming the app theme.
+ * @param {styles} styles - Optional style declarations to be themed.
+ * @param {boolean} noTheme - Flag to disable theme in case of needed.
+ * @returns {Object} - The theme stored by context and the themed styles created based on it.
+ */
 export function useTheme({ noTheme, styles: baseStyles }) {
   const theme = useContext(ThemeContext);
 
