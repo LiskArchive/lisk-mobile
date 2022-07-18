e4a10a7a2ae1b04c6b50942818f42a9d141ee54d
Adepoju Daniel <adebalanced02@gmail.com>
2022-07-18 12:33:36 +0100

    :seedling: Application exploring stats component

---
+++ src/modules/Applications/components/applicationStats/index.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import { View, Dimensions } from 'react-native';
+import PieChart from 'react-native-pie-chart';
+import { translate } from 'react-i18next';
+import { H2, P } from 'components/shared/toolBox/typography';
+import withTheme from 'components/shared/withTheme';
+import { colors } from 'constants/styleGuide';
+import TotalSupplySvg from 'assets/svgs/TotalSupplySvg';
+import StakedSvg from 'assets/svgs/StakedSvg';
+import getStyles from './styles';
+
+const { width } = Dimensions.get('window');
+
+/**
+ * @param {Object} props
+ * @param {Number} props.totalSupply
+ * @param {Number} props.staked
+ * @param {Object[]} props.stats
+ * @param {String} props.stats.label
+ * @param {Number} props.stats.amount
+ */
+
+const widthAndHeight = width / 2.5;
+const { ultramarineBlue, ufoGreen, zodiacBlue } = colors.light;
+const sliceColor = [ultramarineBlue, ufoGreen, zodiacBlue];
+
+const LegendItem = ({
+  styles, label, amount, t
+}) => (
+  <View style={styles.legend}>
+    <View style={styles.legendItem}>
+      <View style={[styles.legendIcon, styles[`${label}Icon`]]}></View>
+      <P style={[styles[label]]}>
+        <P style={[styles.legendAmount]}>{amount} </P>
+        {t(`application.stats.${label}`)}
+      </P>
+    </View>
+  </View>
+);
+
+const ApplicationStats = ({
+  t, styles, totalSupply, staked, stats
+}) => {
+  const series = [stats.registered, stats.active, stats.terminated];
+
+  return (
+    <View style={[styles.container, styles.theme.container]}>
+      <H2>{t('application.stats.title')}</H2>
+      <View style={styles.chartContainer}>
+        <PieChart
+          widthAndHeight={widthAndHeight}
+          series={series}
+          sliceColor={sliceColor}
+          doughnut={true}
+          coverRadius={0.7}
+          coverFill={'#FFF'}
+        />
+        <View style={styles.legend}>
+          <LegendItem
+            styles={styles}
+            label={'registered'}
+            amount={stats.registered}
+            t={t}
+          />
+          <LegendItem
+            styles={styles}
+            label={'active'}
+            amount={stats.active}
+            t={t}
+          />
+          <LegendItem
+            styles={styles}
+            label={'terminated'}
+            amount={stats.terminated}
+            t={t}
+          />
+        </View>
+      </View>
+      <View style={[styles.card]}>
+        <View>
+          <P style={[styles.cardTitle]}>{t('application.stats.totalSupply')}</P>
+          <P style={[styles.amount]}>{totalSupply.toLocaleString()}</P>
+        </View>
+        <TotalSupplySvg />
+      </View>
+      <View style={[styles.card, styles.staked]}>
+        <View>
+          <P style={[styles.cardTitle, styles.blackText]}>
+            {t('application.stats.staked')}
+          </P>
+          <P style={[styles.amount, styles.blackText]}>
+            {staked.toLocaleString()}
+          </P>
+        </View>
+        <StakedSvg />
+      </View>
+    </View>
+  );
+};
+
+export default withTheme(translate()(ApplicationStats), getStyles());
---
+++ src/modules/Applications/components/applicationStats/styles.js
@@ -0,0 +1,80 @@
+import {
+  themes, colors, boxes, fonts
+} from 'constants/styleGuide';
+
+export default () => ({
+  common: {
+    container: {
+      padding: boxes.boxPadding,
+    },
+    chartContainer: {
+      marginVertical: 10,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center'
+    },
+    legendAmount: {
+      fontFamily: fonts.family.contextSemiBold
+    },
+    legendItem: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginVertical: 5,
+    },
+    legendIcon: {
+      width: 15,
+      height: 15,
+      backgroundColor: colors.light.ultramarineBlue,
+      borderRadius: 8,
+      marginRight: 10,
+    },
+    registeredIcon: {
+      backgroundColor: colors.light.ultramarineBlue,
+    },
+    activeIcon: {
+      backgroundColor: colors.light.ufoGreen,
+    },
+    terminatedIcon: {
+      backgroundColor: colors.light.zodiacBlue,
+    },
+    flex: {
+      flex: 1,
+    },
+    card: {
+      borderRadius: 15,
+      backgroundColor: colors.light.ultramarineBlue,
+      padding: boxes.boxPadding,
+      marginVertical: 10,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between'
+    },
+    cardTitle: {
+      fontFamily: fonts.family.context,
+      fontSize: fonts.size.base,
+      color: colors.light.white,
+    },
+    amount: {
+      paddingVertical: 5,
+      fontSize: fonts.size.h2,
+      color: colors.light.white,
+      letterSpacing: 1,
+    },
+    staked: {
+      backgroundColor: colors.light.yellowCopacabana,
+    },
+    blackText: {
+      color: colors.light.zodiacBlue,
+    },
+  },
+  [themes.light]: {
+    container: {
+      backgroundColor: colors.light.white,
+    },
+  },
+  [themes.dark]: {
+    container: {
+      backgroundColor: colors.dark.bg,
+    },
+  },
+});
