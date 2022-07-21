a851da00e534ec69d5c818fa725316ce33f3fb50
ClementeSerrano <clementeserranosutil@gmail.com>
2022-07-21 19:38:15 +0200

    :seedling: Update blockchain application mocks
    Update the logos and names

--- src/modules/BlockchainApplication/mocks/index.js
+++ src/modules/BlockchainApplication/mocks/index.js
@@ -1,53 +1,112 @@
-const LISK_BLOCKCHAIN_APPLICATION = {
-  name: 'Lisk',
-  chainID: 'aq02qkbb35u4jdq8szo3pnsq',
-  title: 'Lisk',
-  description: 'A blockchain application platform',
-  network: 'mainnet',
-  isDefault: true,
-  genesisBlock: 'https://downloads.lisk.com/lisk/mainnet/genesis_block.json.tar.gz',
-  apis: {
-    rest: ['https://service.lisk.com'],
-    rpc: ['wss://service.lisk.com'],
-  },
-  explorers: ['https://lisk.observer', 'https://explorer.lisk.io'],
-  images: {
-    logo: {
-      png: 'https://avatars.githubusercontent.com/u/16600915?s=200&v=4', // URL
-      svg: '', // URL
-    },
-    background: '#FFFFFF1A', // URL
-  },
-}
-
-export const BLOCKCHAIN_APPLICATIONS_MOCK = [LISK_BLOCKCHAIN_APPLICATION].concat(
-  [...new Array(5)].map((_, index) => ({
-    name: `Application ${index}`,
-    chainID: `app${index}chainID`,
-    title: `Application ${index} title`,
-    description: `Application ${index} description`,
+export const BLOCKCHAIN_APPLICATIONS_MOCK = [
+  {
+    name: 'Lisk',
+    chainID: 'aq02qkbb35u4jdq8szo3pnsq',
+    title: 'Lisk',
+    description: 'A blockchain application platform',
     network: 'mainnet',
-    isDefault: false,
-    genesisBlock: `https://downloads.app${index}.com/app${index}/mainnet/genesis_block.json.tar.gz`,
+    isDefault: true,
+    genesisBlock: 'https://downloads.lisk.com/lisk/mainnet/genesis_block.json.tar.gz',
     apis: {
-      rest: [`https://service.app${index}.com`],
-      rpc: [`wss://service.app${index}.com`],
+      rest: ['https://service.lisk.com'],
+      rpc: ['wss://service.lisk.com'],
     },
-    explorers: [`https://app${index}.observer`, `https://explorer.app${index}.io`],
+    explorers: ['https://lisk.observer', 'https://explorer.lisk.io'],
     images: {
       logo: {
-        png: `https://picsum.photos/id/${index}/20/20`, // URL
+        png: 'https://avatars.githubusercontent.com/u/16600915?s=200&v=4', // URL
         svg: '', // URL
       },
-      background: '#FFFFFF', // URL
+      background: '#FFFFFF1A', // URL
     },
-  }))
-)
-
-export const MAPPED_BLOCKCHAIN_APPLICATIONS_MOCK = BLOCKCHAIN_APPLICATIONS_MOCK.reduce(
-  (obj, val) => {
-    obj[val.chainID] = val
-    return obj
   },
-  {}
-)
+  {
+    name: 'Coleti',
+    chainID: 'mi34vyyd12g2lkf0rza1irws',
+    title: 'Coleti',
+    description: 'A Lisk-based NFT marketplace',
+    network: 'mainnet',
+    isDefault: true,
+    genesisBlock: 'https://downloads.coleti.com/lisk/mainnet/genesis_block.json.tar.gz',
+    apis: {
+      rest: ['https://service.coleti.com'],
+      rpc: ['wss://service.coleti.com'],
+    },
+    explorers: ['https://coleti.observer', 'https://explorer.coleti.io'],
+    images: {
+      logo: {
+        png: 'https://lisk.com/sites/default/files/2022-02/colecti-logo.jpeg', // URL
+        svg: '', // URL
+      },
+      background: '#FFFFFF1A', // URL
+    },
+  },
+  {
+    name: 'DoEdu',
+    chainID: 'aq96eeqk77r4syc8aet9fcey',
+    title: 'DoEdu',
+    description: 'An educational platform built with Lisk SDK',
+    network: 'mainnet',
+    isDefault: true,
+    genesisBlock: 'https://downloads.lisk.com/lisk/mainnet/genesis_block.json.tar.gz',
+    apis: {
+      rest: ['https://service.lisk.com'],
+      rpc: ['wss://service.lisk.com'],
+    },
+    explorers: ['https://lisk.observer', 'https://explorer.lisk.io'],
+    images: {
+      logo: {
+        png: 'https://lisk.com/sites/default/files/2022-02/doedu-logo.jpg', // URL
+        svg: '', // URL
+      },
+      background: '#FFFFFF1A', // URL
+    },
+  },
+  {
+    name: 'Enevti',
+    chainID: 'aq86llsb35u4syc8aet7xenf',
+    title: 'Enevti',
+    description: 'A decentralized social media NFT platform',
+    network: 'mainnet',
+    isDefault: true,
+    genesisBlock: 'https://downloads.lisk.com/lisk/mainnet/genesis_block.json.tar.gz',
+    apis: {
+      rest: ['https://service.lisk.com'],
+      rpc: ['wss://service.lisk.com'],
+    },
+    explorers: ['https://lisk.observer', 'https://explorer.lisk.io'],
+    images: {
+      logo: {
+        png: 'https://lisk.com/sites/default/files/2022-02/enevti-logo.png', // URL
+        svg: '', // URL
+      },
+      background: '#FFFFFF1A', // URL
+    },
+  },
+  {
+    name: 'Kalipo',
+    chainID: 'aq25derd17a4syc8aet3pryt',
+    title: 'Kalipo',
+    description: 'A platform to support Decentralized Autonomous Organizations (DAOs)',
+    network: 'mainnet',
+    isDefault: true,
+    genesisBlock: 'https://downloads.lisk.com/lisk/mainnet/genesis_block.json.tar.gz',
+    apis: {
+      rest: ['https://service.lisk.com'],
+      rpc: ['wss://service.lisk.com'],
+    },
+    explorers: ['https://lisk.observer', 'https://explorer.lisk.io'],
+    images: {
+      logo: {
+        png: 'https://lisk.com/sites/default/files/2022-07/Kalipo.jpg', // URL
+        svg: '', // URL
+      },
+      background: '#FFFFFF1A', // URL
+    },
+  },
+]
+
+export const MAPPED_BLOCKCHAIN_APPLICATIONS_MOCK = BLOCKCHAIN_APPLICATIONS_MOCK.reduce((obj, val) => {
+  obj[val.chainID] = val
+  return obj
+}, {})
