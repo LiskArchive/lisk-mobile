e3a7b685ceb596affe76d4fd89865c23b4898004
Adepoju Daniel <adebalanced02@gmail.com>
2022-07-25 10:18:06 +0100

    Update src/modules/BlockchainApplication/store/actionTypes.js
    Co-authored-by: Eniola Olatunji <eniolam1000752@gmail.com>

--- src/modules/BlockchainApplication/store/actionTypes.js
+++ src/modules/BlockchainApplication/store/actionTypes.js
@@ -1,6 +1,6 @@
 const actionTypes = {
   toggleApplicationPin: 'TOGGLE_APPLICATION_PIN',
-  addApplicationByChainId: 'addApplication_BY_CHAIN_ID',
+  addApplicationByChainId: 'ADD_APPLICATION_BY_CHAIN_ID',
   deleteApplicationByChainId: 'DELETE_APPLICATION_BY_CHAIN_ID',
   setCurrentApplication: 'SET_CURRENT_APPLICATION',
 };
