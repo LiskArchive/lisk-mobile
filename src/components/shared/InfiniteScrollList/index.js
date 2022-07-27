bcce8f97c2a9c3dd3516b443004f4165e49c42fd
ClementeSerrano <clementeserranosutil@gmail.com>
2022-07-27 10:21:10 +0200

    :memo: Add docs for InfiniteScrollList component
    Specification of props and component scope

--- src/components/shared/InfiniteScrollList/index.js
+++ src/components/shared/InfiniteScrollList/index.js
@@ -5,6 +5,26 @@ import { useTheme } from 'hooks/useTheme';
 
 import { getInfiniteScrollListStyles } from './styles';
 
+/**
+ * Infinite scroll list component for rendering API paginated data.
+ * @param {Array<Object>} data - List of distinct items to render.
+ * @param {Function?} keyExtractor - Function that extracts a unique key for a given
+ * item at the specified index. Key is used for caching and as the react key to track
+ * item re-ordering. The default extractor checks item.key, then falls back to
+ * using the index, like React does.
+ * @param {Function} renderItem - Function that takes an item from data and renders
+ * it into the list.
+ * @param {boolean} hasNextPage - Flag that indicates if there is more data
+ * to fetch from the server.
+ * @param {Function} fetchNextPage - Function to fetch the data of the next page.
+ * @param {boolean} isFetchingNextPage - Flag that indicates if the api data fetcher is
+ * requesting data.
+ * @param {Function?} renderSpinner - Function to render a loader spinner during fetching
+ * next page state.
+ * @param {number?} onEndReachedThreshold - How far from the end (in units of visible
+ * length of the list) the bottom edge of the list must be from the end of the content to
+ * trigger the onEndReached callback. Default is 0.2.
+ */
 export default function InfiniteScrollList(props) {
   const { styles } = useTheme({ styles: getInfiniteScrollListStyles() });
 
