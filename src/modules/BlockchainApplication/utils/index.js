0aa0a6fdf59894ea766081777d947335ccbd4a76
Adepoju Daniel <adebalanced02@gmail.com>
2022-08-09 08:48:08 +0100

    :nail_care: Updated documentation for round robin

--- src/modules/BlockchainApplication/utils/index.js
+++ src/modules/BlockchainApplication/utils/index.js
@@ -1,4 +1,7 @@
 /**
+ * Access next and previous items of an array in a round form
+ * If current item is the last item of the array, next item would be the first item;
+ * Also, if current item is the first item, previous would be the last item
  * @param {Array} arr - Array of data
  * @param {Number} index - Current index of current active data
  * @param {'next' | 'prev'} direction - Direction to move pointer for next data
