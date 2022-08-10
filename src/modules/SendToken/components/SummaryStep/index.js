063c17ab98fdb2bb56c824c4b663d32c14e9a733
ClementeSerrano <clementeserranosutil@gmail.com>
2022-08-10 15:24:25 +0200

    :lipstick: Add styles to send token summary components

--- src/modules/SendToken/components/SummaryStep/index.js
+++ src/modules/SendToken/components/SummaryStep/index.js
@@ -1,12 +1,16 @@
 /* eslint-disable max-statements */
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 import { useTheme } from 'hooks/useTheme';
 import { PrimaryButton, Button } from 'components/shared/toolBox/button';
+import { P, } from 'components/shared/toolBox/typography';
+import TokenSvg from 'assets/svgs/TokenSvg';
+import { stringShortener } from 'utilities/helpers';
+import CopyToClipboard from 'components/shared/copyToClipboard';
+import { PRIORITY_NAMES_MAP } from '../../constants';
 
 import getSendTokenSummaryStepStyles from './styles';
-import { PRIORITY_NAMES_MAP } from '../../constants';
 import { useSendTokenSummary } from './hooks';
 
 export default function SendTokenSummaryStep({
@@ -23,49 +27,107 @@ export default function SendTokenSummaryStep({
   return (
     <View style={[styles.wrapper, styles.theme.wrapper]}>
       <View style={[styles.container]}>
-        <View style={[styles.row]}>
-          <Text>From Application</Text>
-          <Text>{summary.senderApplication?.name}</Text>
+        <View style={[styles.fieldRow]}>
+          <Text style={[styles.label]}>From Application</Text>
+
+          <View style={[styles.row]}>
+            <Text style={[styles.valueText, styles.theme.valueText]}>
+              {summary.senderApplication?.name}
+            </Text>
+
+            <Image
+              source={{ uri: summary.senderApplication?.images.logo.png }}
+              style={[styles.applicationLogoImage]}
+            />
+          </View>
         </View>
 
-        <View style={[styles.row]}>
-          <Text>To Application</Text>
-          <Text>{summary.recipientApplication?.name}</Text>
+        <View style={[styles.fieldRow]}>
+          <Text style={[styles.label]}>To Application</Text>
+
+          <View style={[styles.row]}>
+            <Text style={[styles.valueText, styles.theme.valueText]}>
+              {summary.recipientApplication?.name}
+            </Text>
+
+            <Image
+              source={{ uri: summary.recipientApplication?.images.logo.png }}
+              style={[styles.applicationLogoImage]}
+            />
+          </View>
         </View>
 
-        <View style={[styles.row]}>
-          <Text>Recipient</Text>
-          <Text>{summary.recipientAccount.metadata.name}</Text>
+        <View style={[styles.fieldRow]}>
+          <Text style={[styles.label]}>Recipient</Text>
+
+          <CopyToClipboard
+            style={[styles.valueText, styles.theme.valueText]}
+            labelStyle={[styles.valueText, styles.theme.valueText, { marginRight: 8 }]}
+            showIcon
+            iconSize={18}
+            value={summary.recipientAccount.metadata.address}
+            type={P}
+            label={stringShortener(summary.recipientAccount.metadata.address, 5, 5)}
+          />
+
         </View>
 
-        <View style={[styles.row]}>
-          <Text>Token</Text>
-          <Text>{summary.token?.symbol}</Text>
+        <View style={[styles.fieldRow]}>
+          <Text style={[styles.label]}>Token</Text>
+
+          <View style={[styles.row]}>
+            <Text style={[styles.valueText, styles.theme.valueText]}>{summary.token?.symbol}</Text>
+
+            <TokenSvg symbol={summary.token?.symbol} style={styles.tokenSvg} />
+          </View>
         </View>
 
-        <View style={[styles.row]}>
-          <Text>Amount</Text>
-          <Text>{summary.amount}</Text>
+        <View style={[styles.fieldRow]}>
+          <Text style={[styles.label]}>Amount</Text>
+
+          <Text
+            style={[styles.valueText, styles.theme.valueText]}
+          >
+              {summary.amount} {summary.token?.symbol}
+          </Text>
         </View>
 
-        <View style={[styles.row]}>
-          <Text>Message</Text>
-          <Text>{summary.message}</Text>
+        {!!summary.message && (
+          <View style={[styles.messageRow]}>
+            <Text style={[styles.label, { marginBottom: 8 }]}>Message</Text>
+
+            <Text
+              style={[styles.valueText, styles.theme.valueText]}
+            >
+              {summary.message}
+            </Text>
+          </View>
+        )}
+
+        <View style={[styles.fieldRow]}>
+          <Text style={[styles.label]}>Priority</Text>
+
+          <Text
+            style={[styles.valueText, styles.theme.valueText]}
+          >
+            {PRIORITY_NAMES_MAP[summary.priority]}
+          </Text>
         </View>
 
-        <View style={[styles.row]}>
-          <Text>Priority</Text>
-          <Text>{PRIORITY_NAMES_MAP[summary.priority]}</Text>
+        <View style={[styles.fieldRow]}>
+          <Text style={[styles.label]}>Transaction fee</Text>
+
+          <Text style={[styles.valueText, styles.theme.valueText]}>
+            {summary.transactionFee?.data} {summary.token?.symbol}
+          </Text>
         </View>
 
-        <View style={[styles.row]}>
-          <Text>Transaction fee</Text>
-          <Text>{summary.transactionFee?.data}</Text>
-        </View>
+        <View style={[styles.fieldRow, { borderBottomColor: 'transparent' }]}>
+          <Text style={[styles.label]}>CCM fee</Text>
 
-        <View style={[styles.row]}>
-          <Text>CCM fee</Text>
-          <Text>{summary.initializationFee?.data}</Text>
+          <Text style={[styles.valueText, styles.theme.valueText]}>
+            {summary.initializationFee?.data} {summary.token?.symbol}
+          </Text>
         </View>
       </View>
 
