83861f89c5f272762633010476a740570839f52e
Adepoju Daniel <adebalanced02@gmail.com>
2022-08-24 21:37:42 +0100

    :white_check_mark: Remove unused tests

--- src/utilities/api/lisk/apiClient.test.js
+++ src/utilities/api/lisk/apiClient.test.js
@@ -49,41 +49,6 @@ describe('apiClient', () => {
     });
   });
 
-  describe('getFees', () => {
-    const fees = {
-      low: 0.0001,
-      medium: 0.0003,
-      high: 0.0008
-    };
-
-    it('Retrieves the latest fees', async () => {
-      global.fetch.mockReturnValue(
-        Promise.resolve({
-          ok: true,
-          status: 200,
-          json: () => ({ data: fees })
-        })
-      );
-      const { data: result } = await apiClient.getFees();
-      expect(result).toEqual(fees);
-      expect(fetch).toHaveBeenCalledWith('https://service.lisk.com/api/v3/fees', expect.anything());
-    });
-
-    it('Throw error for all other errors', async () => {
-      global.fetch.mockReturnValue(
-        Promise.resolve({
-          ok: false,
-          status: 500
-        })
-      );
-      try {
-        await apiClient.getFees();
-      } catch (e) {
-        expect(e.message).toEqual('Failed to request fees from server.');
-      }
-    });
-  });
-
   describe('getLatestBlock', () => {
     const data = {
       height: 2912982123,
