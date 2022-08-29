d2dcf52a859a9e569379809f82098a332f07b716
Adepoju Daniel <adebalanced02@gmail.com>
2022-08-29 16:58:21 +0100

    :nail_care: used custom query instaed of useQuery

--- src/modules/BlockchainApplication/hooks/useBlockchainApplicationStats.js
+++ src/modules/BlockchainApplication/hooks/useBlockchainApplicationStats.js
@@ -1,17 +1,18 @@
-import { useQuery } from '@tanstack/react-query';
+import { useCustomQuery } from 'utilities/api/hooks/useCustomQuery';
 import {
-  METHOD,
-  API_METHOD,
   API_URL,
   API_BASE_URL
 } from 'utilities/api/constants';
 
 export function useBlockchainApplicationStats() {
-  const query = useQuery(['application-stats'], () => API_METHOD[METHOD]({
-    baseURL: API_BASE_URL,
-    url: `${API_URL}/blockchain/apps/statistics`,
-    method: 'get',
-  }));
+  const query = useCustomQuery({
+    keys: ['application-stats'],
+    config: {
+      baseURL: API_BASE_URL,
+      url: `${API_URL}/blockchain/apps/statistics`,
+      method: 'get',
+    }
+  });
 
   return {
     ...query,
--- src/modules/SendToken/api/useGetFeesByPriorityQuery.js
+++ src/modules/SendToken/api/useGetFeesByPriorityQuery.js
@@ -1,19 +1,17 @@
-import { useQuery } from '@tanstack/react-query';
 import { useMemo } from 'react';
-import { useCurrentBlockchainApplication } from 'modules/BlockchainApplication/hooks/useCurrentBlockchainApplication';
+import { useCustomQuery } from 'utilities/api/hooks/useCustomQuery';
 import {
-  METHOD,
-  API_METHOD,
   API_URL
 } from 'utilities/api/constants';
 
 export function useGetFeesByPriorityQuery() {
-  const [currApp] = useCurrentBlockchainApplication();
-
-  const query = useQuery([`priorityFees-${currApp.chainID}`], () => API_METHOD[METHOD]({
-    url: `${API_URL}/fees`,
-    method: 'get',
-  }));
+  const query = useCustomQuery({
+    keys: ['priorityFees'],
+    config: {
+      url: `${API_URL}/fees`,
+      method: 'get',
+    }
+  });
 
   const result = useMemo(() => {
     return query.data?.data?.feeEstimatePerByte;
