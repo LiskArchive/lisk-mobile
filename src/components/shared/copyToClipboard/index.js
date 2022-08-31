4f0cfafef77615b40e5e43d55ba3180a8700ba0d
ClementeSerrano <clementeserranosutil@gmail.com>
2022-08-31 13:14:28 +0200

    :recycle: Make CopyToClipboard component functional

--- src/components/shared/copyToClipboard/index.js
+++ src/components/shared/copyToClipboard/index.js
@@ -1,60 +1,61 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Text, Clipboard } from 'react-native';
-import { translate } from 'react-i18next';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+
+import { useTheme } from 'hooks/useTheme';
 import { colors } from 'constants/styleGuide';
 import Icon from '../toolBox/icon';
-import withTheme from '../withTheme';
+
 import getStyles from './styles';
 
-class CopyToClipBoard extends React.Component {
-  state = { copied: false };
+export default function CopyToClipBoard({
+  value,
+  type,
+  label,
+  iconSize,
+  style,
+  iconStyle,
+  labelStyle,
+  iconColor,
+}) {
+  const [copied, setCopied] = useState(false);
 
-  copy = () => {
-    this.setState({ copied: !this.state.copied });
-    Clipboard.setString(this.props.value);
-    this.Timeout = setTimeout(() => {
-      this.setState({ copied: !this.state.copied });
+  let timeout = useRef();
+
+  const { styles } = useTheme({ styles: getStyles() });
+
+  function handleCopy() {
+    setCopied(prevState => !prevState);
+
+    Clipboard.setString(value);
+
+    timeout = setTimeout(() => {
+      setCopied(prevState => !prevState);
     }, 4000);
-  };
-
-  componentWillUnmount() {
-    clearTimeout(this.Timeout);
   }
 
-  render() {
-    const {
-      value,
-      type,
-      label,
-      iconSize,
-      styles,
-      style,
-      iconStyle,
-      labelStyle,
-      iconColor,
-    } = this.props;
-    const Element = type || Text;
-    const text = label || value;
+  useEffect(() => () => clearTimeout(timeout), []);
 
-    const color = iconColor || colors.light.blueGray;
+  const Element = type || Text;
 
-    return (
-      <TouchableOpacity style={[styles.container, style]} onPress={this.copy}>
-        <Element style={[styles.label, labelStyle]}>
-          {text}
-        </Element>
-        <TouchableOpacity onPress={this.copy}>
-          <Icon
-            name={this.state.copied ? 'checkmark' : 'copy'}
-            color={this.state.copied ? colors.light.ufoGreen : color}
-            size={iconSize || 16}
-            style={[styles.copyIcon, iconStyle]}
-            />
-          </TouchableOpacity>
+  const text = label || value;
+
+  const color = iconColor || colors.light.blueGray;
+
+  return (
+    <TouchableOpacity style={[styles.container, style]} onPress={handleCopy}>
+      <Element style={[styles.label, labelStyle]}>
+        {text}
+      </Element>
+
+      <TouchableOpacity onPress={handleCopy}>
+        <Icon
+          name={copied ? 'checkmark' : 'copy'}
+          color={copied ? colors.light.ufoGreen : color}
+          size={iconSize || 16}
+          style={[styles.copyIcon, iconStyle]}
+        />
       </TouchableOpacity>
-    );
-  }
+    </TouchableOpacity>
+  );
 }
-
-export default withTheme(translate()(CopyToClipBoard), getStyles());
