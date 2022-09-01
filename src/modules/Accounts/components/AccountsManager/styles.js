6c556d11f3d5ffb6e98931756632efe44c90b9c1
ClementeSerrano <clementeserranosutil@gmail.com>
2022-09-01 17:43:32 +0200

    :lipstick: Remove unused code on accounts manager styles

--- src/modules/Accounts/components/AccountsManager/styles.js
+++ src/modules/Accounts/components/AccountsManager/styles.js
@@ -1,5 +1,5 @@
 import {
-  colors, themes, boxes, fonts
+  colors, themes
 } from 'constants/styleGuide';
 
 export default function getAccountsManagerStyles() {
@@ -9,93 +9,17 @@ export default function getAccountsManagerStyles() {
         flex: 1,
         width: '100%',
       },
-      title: {
-        textAlign: 'center',
-        marginBottom: 8,
-      },
-      description: {
-        textAlign: 'center',
-        marginBottom: 24,
-        fontSize: 12
-      },
-      body: {
-        paddingTop: boxes.boxPadding
-      },
-      button: {
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginVertical: 5,
-      },
-      outline: {
-        borderWidth: 1,
-        borderRadius: 5,
-        minHeight: 48,
-      },
-      row: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-      addressContainer: {
-        marginTop: 24,
-        marginBottom: 16,
-      },
-      filenameContainer: {
-        marginTop: 16,
-      },
-      addressText: {
-        fontSize: 14,
-        marginLeft: 8
-      },
-      downloadFileIcon: {
-        marginLeft: 8,
-      },
-      text: {
-        fontFamily: fonts.family.heading,
-        fontSize: fonts.size.small
-      },
     },
     [themes.light]: {
       container: {
         backgroundColor: colors.dark.white
       },
-      outline: {
-        borderColor: colors.light.platinumGray,
-      },
-      title: {
-        color: colors.light.zodiacBlue,
-      },
-      description: {
-        color: colors.light.zodiacBlue,
-      },
-      text: {
-        color: colors.light.zodiacBlue
-      },
-      addressText: {
-        color: colors.light.blueGray
-      }
     },
 
     [themes.dark]: {
       container: {
         backgroundColor: colors.dark.black
       },
-      outline: {
-        borderColor: colors.light.volcanicSand,
-      },
-      title: {
-        color: colors.dark.white,
-      },
-      description: {
-        color: colors.dark.ghost,
-      },
-      text: {
-        color: colors.light.white
-      },
-      addressText: {
-        color: colors.light.ghost
-      }
     }
   };
 }
