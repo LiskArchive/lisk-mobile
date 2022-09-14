24ea5c2c98a842b94705f5f166629450351e3522
ClementeSerrano <clementeserranosutil@gmail.com>
2022-09-14 17:08:07 +0200

    :sparkles: Connect ExternalBlockchainApplicationsList with pairings

--- src/modules/BlockchainApplication/components/ApplicationList/components.js
+++ src/modules/BlockchainApplication/components/ApplicationList/components.js
@@ -1,29 +1,103 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, FlatList } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import i18next from 'i18next';
 
-export function AllBlockchainApplicationsList({ applications, Component, onItemPress, ...props }) {
+import { useTheme } from 'hooks/useTheme';
+import DataRenderer from 'components/shared/DataRenderer';
+import { P } from 'components/shared/toolBox/typography';
+import usePairings from '../../../../../libs/wcm/hooks/usePairings';
+
+import getBlockchainApplicationsListStyles from './styles';
+
+export function AllBlockchainApplicationsList({
+  applications,
+  Component,
+  onItemPress,
+  style,
+  ...props
+}) {
   const navigation = useNavigation;
 
+  const { styles } = useTheme({
+    styles: getBlockchainApplicationsListStyles(),
+  });
+
   return (
-    <FlatList
-      data={applications}
-      keyExtractor={(item) => item.chainID}
-      renderItem={({ item }) => (
-        <Component
-          application={item}
-          navigation={navigation}
-          key={item.chainID}
-          image={item.logo.png}
-          showPinned={true}
-          onPress={() => onItemPress(item)}
-          {...props}
+    <DataRenderer
+      data={applications.data}
+      isLoading={applications.isLoading}
+      error={applications.error}
+      renderData={(data) => (
+        <FlatList
+          data={data}
+          keyExtractor={(item) => item.chainID}
+          renderItem={({ item }) => (
+            <Component
+              application={item}
+              navigation={navigation}
+              key={item.chainID}
+              image={item.logo.png}
+              showPinned={true}
+              onPress={() => onItemPress(item)}
+              {...props}
+            />
+          )}
         />
+      )}
+      renderLoading={() => (
+        <P
+          style={[
+            styles.applicationNameLabel,
+            styles.theme.applicationNameLabel,
+            style?.applicationNameLabel,
+          ]}
+        >
+          {i18next.t('application.explore.applicationList.loadingText')}
+        </P>
       )}
     />
   );
 }
 
-export function ExternalBlockchainApplicationsList() {
-  return <Text style={{ flex: 1 }}>External connections</Text>;
+export function ExternalBlockchainApplicationsList({ style }) {
+  const { pairings } = usePairings();
+  const [loading, setLoading] = useState(true);
+
+  const { styles } = useTheme({
+    styles: getBlockchainApplicationsListStyles(),
+  });
+
+  useEffect(() => {
+    if (Array.isArray(pairings)) {
+      setLoading(false);
+    }
+  }, [pairings]);
+
+  console.log({ data: pairings.slice(1), loading });
+
+  return (
+    <DataRenderer
+      data={pairings.slice(1)}
+      isLoading={loading}
+      renderData={(data) => (
+        <FlatList
+          data={data}
+          keyExtractor={(item) => item.topic}
+          renderItem={({ item }) => <Text>name: {item.peerMetadata.name}</Text>}
+        />
+      )}
+      renderLoading={() => (
+        <P
+          style={[
+            styles.applicationNameLabel,
+            styles.theme.applicationNameLabel,
+            style?.applicationNameLabel,
+          ]}
+        >
+          {i18next.t('application.explore.applicationList.loadingText')}
+        </P>
+      )}
+    />
+  );
 }
--- src/modules/BlockchainApplication/components/ApplicationList/index.js
+++ src/modules/BlockchainApplication/components/ApplicationList/index.js
@@ -1,10 +1,7 @@
 import React, { useState } from 'react';
 import { View } from 'react-native';
-import i18next from 'i18next';
 
 import { useTheme } from 'hooks/useTheme';
-import { P } from 'components/shared/toolBox/typography';
-import DataRenderer from 'components/shared/DataRenderer';
 import Tabs from 'components/shared/Tabs';
 
 import { AllBlockchainApplicationsList, ExternalBlockchainApplicationsList } from './components';
@@ -25,43 +22,26 @@ export default function BlockchainApplicationList({
 
   return (
     <View style={[styles.container, styles.theme.container, style?.container]}>
-      <DataRenderer
-        data={applications.data}
-        isLoading={applications.isLoading}
-        error={applications.error}
-        renderData={(data) => (
-          <>
-            <Tabs value={activeTab} onClick={(tab) => setActiveTab(tab)}>
-              <Tabs.Tab value="allApplications">All applications</Tabs.Tab>
-              <Tabs.Tab value="externalApplications">External connections</Tabs.Tab>
-            </Tabs>
+      <>
+        <Tabs value={activeTab} onClick={(tab) => setActiveTab(tab)}>
+          <Tabs.Tab value="allApplications">All applications</Tabs.Tab>
+          <Tabs.Tab value="externalApplications">External connections</Tabs.Tab>
+        </Tabs>
 
-            <Tabs.Panel index="allApplications" value={activeTab}>
-              <AllBlockchainApplicationsList
-                applications={data}
-                Component={Component}
-                onItemPress={onItemPress}
-                {...props}
-              />
-            </Tabs.Panel>
+        <Tabs.Panel index="allApplications" value={activeTab}>
+          <AllBlockchainApplicationsList
+            applications={applications}
+            Component={Component}
+            onItemPress={onItemPress}
+            style={style}
+            {...props}
+          />
+        </Tabs.Panel>
 
-            <Tabs.Panel index="externalApplications" value={activeTab}>
-              <ExternalBlockchainApplicationsList />
-            </Tabs.Panel>
-          </>
-        )}
-        renderLoading={() => (
-          <P
-            style={[
-              styles.applicationNameLabel,
-              styles.theme.applicationNameLabel,
-              style?.applicationNameLabel,
-            ]}
-          >
-            {i18next.t('application.explore.applicationList.loadingText')}
-          </P>
-        )}
-      />
+        <Tabs.Panel index="externalApplications" value={activeTab}>
+          <ExternalBlockchainApplicationsList />
+        </Tabs.Panel>
+      </>
     </View>
   );
 }
