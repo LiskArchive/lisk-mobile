f1274033099ff21a3dd6a33b9e882e3ca6cd942a
Adepoju Daniel <adebalanced02@gmail.com>
2022-09-15 14:36:57 +0100

    Initiate wallet connection - Closes #1480 (#1492)
    * :seedling: Connect hardware wallet
    
    * :seedling: Added wallet connect context
    
    * :seedling: Added wallet connect context
    
    * :seedling: Implement Fab Component
    
    * :nail_care: Removed unused props for simplicity
    
    * :nail_care: Removed unused Fab
    
    * :nail_care: Update stylings and code structure
    
    * :nail_care: Fix deep scan ish
    
    * :nail_care: Update props to svg

---
+++ src/components/shared/Fab/styles.js
@@ -0,0 +1,54 @@
+import { StyleSheet, Platform, Dimensions } from 'react-native';
+
+const DEVICE_WIDTH = Dimensions.get('window').width;
+const DEVICE_HEIGHT = Dimensions.get('window').height;
+
+export default StyleSheet.create({
+  centerActions: {
+    left: -1000,
+  },
+  rightActionsVisible: {
+    right: 0,
+  },
+  leftActionsVisible: {
+    left: 0,
+  },
+  centerActionsVisible: {
+    left: DEVICE_WIDTH / 2 - 30,
+  },
+  overlay: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    elevation: 0,
+    zIndex: 0,
+    top: 0,
+    height: DEVICE_HEIGHT,
+    backgroundColor: 'transparent',
+  },
+  overlayColor: { backgroundColor: 'rgba(12, 21, 46, 0.4)' },
+  buttonContainer: {
+    overflow: Platform.OS === 'ios' ? 'visible' : 'hidden',
+    zIndex: 2,
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    elevation: 5,
+    position: 'absolute',
+    right: 30,
+    bottom: 0,
+  },
+  button: {
+    zIndex: 3,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  centerButton: {
+    left: DEVICE_WIDTH / 2 - 28,
+  },
+  buttonTextContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
