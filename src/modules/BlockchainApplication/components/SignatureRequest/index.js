b986358f347a6082962bab57609e54f87f79c9ed
Adepoju Daniel <adebalanced02@gmail.com>
2022-09-27 00:36:53 +0100

    :seedling: Receive signature request

---
+++ src/modules/BlockchainApplication/components/SignatureRequest/index.js
@@ -0,0 +1,87 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { View, Image } from 'react-native';
+import i18next from 'i18next';
+import { extractAddressFromPublicKey } from 'modules/Wallet/utils/account';
+import { H3, P } from 'components/shared/toolBox/typography';
+import { PrimaryButton } from 'components/shared/toolBox/button';
+import { useTheme } from 'hooks/useTheme';
+import UrlSvg from 'assets/svgs/UrlSvg';
+import { stringShortener } from 'utilities/helpers';
+import Avatar from 'components/shared/avatar';
+import getSignatureStyles from './styles';
+import { rejectLiskRequest } from '../../../../../libs/wcm/utils/requestHandlers';
+import ConnectionContext from '../../../../../libs/wcm/context/connectionContext';
+import { EVENTS } from '../../../../../libs/wcm/constants/lifeCycle';
+
+const SignatureRequest = ({ session, onFinish }) => {
+  const { events } = useContext(ConnectionContext);
+  const { styles } = useTheme({ styles: getSignatureStyles });
+  const [request, setRequest] = useState(null);
+
+  const selectedAccountAddress = extractAddressFromPublicKey(session.peer.publicKey);
+
+  useEffect(() => {
+    const event = events.find((e) => e.name === EVENTS.SESSION_REQUEST);
+    if (event) {
+      setRequest(event.meta);
+    }
+  }, [events]);
+
+  const connectHandler = async () => {
+    onFinish();
+  };
+
+  const rejectHandler = () => {
+    rejectLiskRequest(request);
+    onFinish();
+  };
+
+  if (!session) {
+    return null;
+  }
+
+  return (
+    <>
+      <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          <Image source={{ uri: session.peer.metadata.icons[0] }} style={styles.image} />
+        </View>
+        <H3 style={styles.title}>{session.peer.metadata.name}</H3>
+        <View style={styles.urlContainer}>
+          <UrlSvg />
+          <P style={styles.url}>{session.peer.metadata.url}</P>
+        </View>
+      </View>
+      <View style={styles.horizontalLine} />
+
+      <View style={styles.container}>
+        <P style={styles.label}>{i18next.t('application.signing.information')}:</P>
+        <P style={styles.description}>
+          {i18next.t('application.signing.description', { appName: session.peer.metadata.name })}
+        </P>
+      </View>
+      <View style={styles.horizontalLine} />
+      <View style={styles.container}>
+        <View style={styles.accountItem}>
+          <Avatar address={selectedAccountAddress} size={35} />
+          <View style={styles.accountContent}>
+            <P style={styles.address}>{stringShortener(selectedAccountAddress, 7, 4)}</P>
+          </View>
+        </View>
+      </View>
+      <View style={styles.horizontalLine} />
+      <View style={[styles.container, styles.buttonContainer]}>
+        <PrimaryButton style={[styles.button, styles.outlineButton]} onPress={rejectHandler}>
+          <P style={[styles.buttonText, styles.outlineButtonText]}>
+            {i18next.t('commons.buttons.cancel')}
+          </P>
+        </PrimaryButton>
+        <PrimaryButton style={styles.button} onPress={connectHandler}>
+          <P style={[styles.buttonText]}>{i18next.t('commons.buttons.continue')}</P>
+        </PrimaryButton>
+      </View>
+    </>
+  );
+};
+
+export default SignatureRequest;
---
+++ src/modules/BlockchainApplication/components/SignatureRequest/styles.js
@@ -0,0 +1,78 @@
+import { boxes, colors, fonts } from 'constants/styleGuide';
+
+export default {
+  common: {
+    container: {
+      padding: boxes.boxPadding,
+      flex: 1,
+    },
+    imageContainer: {
+      alignItems: 'center',
+    },
+    image: {
+      width: 50,
+      height: 50,
+      marginBottom: 10,
+      borderRadius: 25,
+    },
+    title: {
+      textAlign: 'center',
+      marginBottom: 10,
+    },
+    urlContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginBottom: 5,
+    },
+    accountItem: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginVertical: 5,
+    },
+    accountContent: {
+      paddingLeft: 10,
+    },
+    url: {
+      marginLeft: 5,
+      color: colors.light.ultramarineBlue,
+      fontFamily: fonts.family.contextSemiBold,
+    },
+    label: {
+      color: colors.light.blueGray,
+      marginRight: 5,
+    },
+    description: {
+      fontFamily: fonts.family.regular,
+      marginVertical: 5,
+    },
+    value: {
+      fontSize: fonts.size.input,
+    },
+    permissions: {
+      color: colors.light.blueGray,
+    },
+    horizontalLine: {
+      borderBottomColor: colors.light.platinumGray,
+      borderBottomWidth: 1,
+    },
+    buttonContainer: {
+      flexDirection: 'row',
+    },
+    button: {
+      flex: 1,
+      marginHorizontal: 5,
+    },
+    outlineButton: {
+      backgroundColor: 'transparent',
+      borderWidth: 2,
+      borderColor: colors.light.platinumGray,
+    },
+    outlineButtonText: {
+      color: colors.light.zodiacBlue,
+    },
+    buttonText: {
+      fontFamily: fonts.family.contextSemiBold,
+    },
+  },
+};
