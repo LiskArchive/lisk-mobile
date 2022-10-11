bb8320fb86db88b7989899f4514e7005ae4b8ce6
Adepoju Daniel <adebalanced02@gmail.com>
2022-10-11 16:11:30 +0100

    :nail_care: improve modal mounting

--- src/components/shared/BottomModal/index.js
+++ src/components/shared/BottomModal/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { TouchableOpacity, View, Modal, ScrollView } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { TouchableOpacity, View, Modal, ScrollView, Animated, Easing } from 'react-native';
 import Icon from 'components/shared/toolBox/icon';
 import { useTheme } from 'hooks/useTheme';
 import { colors } from 'constants/styleGuide';
@@ -7,25 +7,50 @@ import getStyles from './styles';
 
 const BottomModal = ({ showClose = true, show, toggleShow, children }) => {
   const { styles } = useTheme({ styles: getStyles() });
+  const animation = useRef(new Animated.Value(0)).current;
+
+  const opacity = animation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 0.7],
+    easing: Easing.elastic(),
+  });
+
+  useEffect(() => {
+    if (show) {
+      Animated.spring(animation, {
+        toValue: 1,
+        duration: 1000,
+        delay: 50,
+      }).start();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [show]);
+
+  const closeModal = () => {
+    animation.setValue(0);
+    toggleShow(false);
+  };
 
   return (
-    <Modal transparent visible={show} onRequestClose={() => toggleShow(false)}>
-      <View style={[styles.overlay, styles.theme.overlay]}>
-        <View style={[styles.container, styles.theme.container]}>
-          <View style={[styles.horizontalLine, styles.theme.horizontalLine]} />
+    <Animated.View style={[show && styles.overlay, styles.theme.overlay, { opacity }]}>
+      <Modal transparent visible={show} onRequestClose={closeModal} animationType="slide">
+        <View style={[styles.content]}>
+          <View style={[styles.container, styles.theme.container]}>
+            <View style={[styles.horizontalLine, styles.theme.horizontalLine]} />
 
-          {showClose && (
-            <TouchableOpacity
-              style={[styles.closeButtonContainer, styles.theme.closeButtonContainer]}
-              onPress={() => toggleShow(false)}
-            >
-              <Icon name="cross" color={colors.light.ultramarineBlue} size={20} />
-            </TouchableOpacity>
-          )}
-          <ScrollView>{children}</ScrollView>
+            {showClose && (
+              <TouchableOpacity
+                style={[styles.closeButtonContainer, styles.theme.closeButtonContainer]}
+                onPress={closeModal}
+              >
+                <Icon name="cross" color={colors.light.ultramarineBlue} size={20} />
+              </TouchableOpacity>
+            )}
+            <ScrollView>{children}</ScrollView>
+          </View>
         </View>
-      </View>
-    </Modal>
+      </Modal>
+    </Animated.View>
   );
 };
 
