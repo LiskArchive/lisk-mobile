ef1d5a117691e58cc1edde9cf66314ca60e61756
Adepoju Daniel <adebalanced02@gmail.com>
2022-10-26 21:18:07 +0100

    :seedling: upgrade done for iOS, tested and working

--- ios/Lisk/AppDelegate.m
+++ ios/Lisk/AppDelegate.m
@@ -3,7 +3,6 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-#import "RNQuickActionManager.h"
 #import <React/RCTLinkingManager.h>
 
 #ifdef FB_SONARKIT_ENABLED
