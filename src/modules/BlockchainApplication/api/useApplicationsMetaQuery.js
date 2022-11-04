cdc976c99639d78f1dfe6e29ec1eaf017d56daec
ClementeSerrano <clementeserranosutil@gmail.com>
2022-11-04 17:12:03 +0100

    :clown_face: Add mocks for broken get apps endpoints

--- src/modules/BlockchainApplication/api/useApplicationsMetaQuery.js
+++ src/modules/BlockchainApplication/api/useApplicationsMetaQuery.js
@@ -1,7 +1,8 @@
-import { LIMIT, API_URL, METHOD } from 'utilities/api/constants';
-import { GET_APPLICATIONS_QUERY, APPLICATION } from 'utilities/api/queries';
-import { useCustomInfiniteQuery } from 'utilities/api/hooks/useCustomInfiniteQuery';
-import applicationsAPIClient from 'utilities/api/ApplicationsAPIClient';
+// import { LIMIT, API_URL, METHOD } from 'utilities/api/constants';
+// import { GET_APPLICATIONS_QUERY, APPLICATION } from 'utilities/api/queries';
+// import { useCustomInfiniteQuery } from 'utilities/api/hooks/useCustomInfiniteQuery';
+// import applicationsAPIClient from 'utilities/api/ApplicationsAPIClient';
+import { mockApplicationsMeta } from '../__fixtures__';
 
 /**
  * Fetch list of blockchain applications metadata (off-chain data) in paginated mode.
@@ -11,19 +12,26 @@ import applicationsAPIClient from 'utilities/api/ApplicationsAPIClient';
  * @returns - The query state of the API call. Includes the data
  * (applications), loading state, error state, and more.
  */
-export function useApplicationsMetaQuery({ config: customConfig = {}, options = {} } = {}) {
-  const config = {
-    url: `${API_URL}/blockchain/apps/meta`,
-    method: 'get',
-    event: 'get.blockchain.apps.meta',
-    ...customConfig,
-    params: {
-      limit: LIMIT,
-      ...(customConfig?.params || {}),
-    },
+export function useApplicationsMetaQuery() {
+  // { config: customConfig = {}, options = {} } = {}
+  // const config = {
+  //   url: `${API_URL}/blockchain/apps/meta`,
+  //   method: 'get',
+  //   event: 'get.blockchain.apps.meta',
+  //   ...customConfig,
+  //   params: {
+  //     limit: LIMIT,
+  //     ...(customConfig?.params || {}),
+  //   },
+  // };
+
+  // const keys = [GET_APPLICATIONS_QUERY, config, APPLICATION, METHOD];
+
+  // return useCustomInfiniteQuery({ config, options, keys, client: applicationsAPIClient });
+
+  return {
+    data: { data: mockApplicationsMeta },
+    isLoading: false,
+    isError: false,
   };
-
-  const keys = [GET_APPLICATIONS_QUERY, config, APPLICATION, METHOD];
-
-  return useCustomInfiniteQuery({ config, options, keys, client: applicationsAPIClient });
 }
--- src/modules/BlockchainApplication/api/useApplicationsQuery.js
+++ src/modules/BlockchainApplication/api/useApplicationsQuery.js
@@ -1,7 +1,8 @@
-import { useCustomInfiniteQuery } from 'utilities/api/hooks/useCustomInfiniteQuery';
-import { LIMIT, API_URL, METHOD } from 'utilities/api/constants';
-import { GET_APPLICATIONS_QUERY, APPLICATION } from 'utilities/api/queries';
-import applicationsAPIClient from 'utilities/api/ApplicationsAPIClient';
+// import { useCustomInfiniteQuery } from 'utilities/api/hooks/useCustomInfiniteQuery';
+// import { LIMIT, API_URL, METHOD } from 'utilities/api/constants';
+// import { GET_APPLICATIONS_QUERY, APPLICATION } from 'utilities/api/queries';
+// import applicationsAPIClient from 'utilities/api/ApplicationsAPIClient';
+import { mockApplications } from '../__fixtures__';
 
 /**
  * Fetch list of blockchain applications on-chain data.
@@ -11,17 +12,24 @@ import applicationsAPIClient from 'utilities/api/ApplicationsAPIClient';
  * @returns - The query state of the API call. Includes the data
  * (applications), loading state, error state, and more.
  */
-export function useApplicationsQuery({ config: customConfig = {}, options = {} } = {}) {
-  const config = {
-    baseURL: process.env.SERVICE_API_BASE_URL,
-    url: `${API_URL}/blockchain/apps`,
-    method: 'get',
-    event: 'get.blockchain.apps',
-    ...customConfig,
-    params: { limit: LIMIT, ...customConfig.params },
+export function useApplicationsQuery() {
+  // { config: customConfig = {}, options = {} } = {}
+  // const config = {
+  //   baseURL: process.env.SERVICE_API_BASE_URL,
+  //   url: `${API_URL}/blockchain/apps`,
+  //   method: 'get',
+  //   event: 'get.blockchain.apps',
+  //   ...customConfig,
+  //   params: { limit: LIMIT, ...customConfig.params },
+  // };
+
+  // const keys = [GET_APPLICATIONS_QUERY, config, APPLICATION, METHOD];
+
+  // return useCustomInfiniteQuery({ config, options, keys, client: applicationsAPIClient });
+
+  return {
+    data: { data: mockApplications },
+    isLoading: false,
+    isError: false,
   };
-
-  const keys = [GET_APPLICATIONS_QUERY, config, APPLICATION, METHOD];
-
-  return useCustomInfiniteQuery({ config, options, keys, client: applicationsAPIClient });
 }
