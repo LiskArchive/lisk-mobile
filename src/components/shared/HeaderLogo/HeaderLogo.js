04cf819a54522940794051ddcaeee0c2b1fc7040
ClementeSerrano <clementeserranosutil@gmail.com>
2023-01-25 14:44:37 +0100

    :memo: Add docs for HeaderLogo

--- src/components/shared/HeaderLogo/HeaderLogo.js
+++ src/components/shared/HeaderLogo/HeaderLogo.js
@@ -8,6 +8,13 @@ import LiskMobileLogoSvg from 'assets/svgs/LiskMobileLogoSvg';
 
 import { getHeaderLogoStyles } from './HeaderLogo.styles';
 
+/**
+ * Renders Lisk logo and project description in a single header component.
+ * @param {Object} style - Custom styles for internal components (optional).
+ * @param {React.CSSProperties} style.container - Custom styles for component main container.
+ * @param {React.CSSProperties} style.logo - Custom styles for the logo.
+ * @param {React.CSSProperties} style.title - Custom styles for the title.
+ */
 export default function HeaderLogo({ style }) {
   const { styles } = useTheme({ styles: getHeaderLogoStyles() });
 
