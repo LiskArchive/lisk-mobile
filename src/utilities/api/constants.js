5522116121fcff51d478c3ea089dc7e1963c193e
ClementeSerrano <clementeserranosutil@gmail.com>
2023-01-25 17:04:45 +0100

    :fire: Remove process env hardcodes on mocks

--- src/utilities/api/constants.js
+++ src/utilities/api/constants.js
@@ -1,10 +1,10 @@
-export const NETWORK = process.env.NETWORK || 'devnet';
+export const NETWORK = process.env.NETWORK;
 
-export const API_BASE_URL = process.env.SERVICE_API_BASE_URL || 'http://localhost:9901';
+export const API_BASE_URL = process.env.SERVICE_API_BASE_URL;
 
-export const WS_BASE_URL = process.env.SERVICE_WS_BASE_URL || 'wss://localhost:9901/blockchain';
+export const WS_BASE_URL = process.env.SERVICE_WS_BASE_URL;
 
-export const API_VERSION = process.env.SERVICE_API_VERSION || 'v3';
+export const API_VERSION = process.env.SERVICE_API_VERSION;
 
 export const API_URL = `/api/${API_VERSION}`;
 
