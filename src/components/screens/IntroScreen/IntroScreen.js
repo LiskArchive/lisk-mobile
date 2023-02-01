8fb90f8b90a30319675e438f602542416d969832
ClementeSerrano <clementeserranosutil@gmail.com>
2023-02-01 13:54:25 +0100

    :sparkles: Add accept terms to intro screen

--- src/components/screens/IntroScreen/IntroScreen.js
+++ src/components/screens/IntroScreen/IntroScreen.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Linking } from 'react-native';
+import Switch from 'react-native-switch-pro';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation } from '@react-navigation/native';
+import { useDispatch } from 'react-redux';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import i18next from 'i18next';
 
@@ -9,15 +11,24 @@ import i18next from 'i18next';
 // import secureAuthenticationImg from 'assets/images/intro/secureAuthentication3x.png';
 // import easyAccessImg from 'assets/images/intro/easyAccess3x.png';
 
+import { settingsUpdated as settingsUpdatedAction } from 'modules/Settings/actions';
 import Slider from 'components/shared/__Slider/Slider';
 import { PrimaryButton, Button } from 'components/shared/toolBox/button';
+import { P, A } from 'components/shared/toolBox/typography';
 import AddressIllustrationSvg from 'assets/svgs/AddressIllustrationSvg';
+import CoinIllustrationSvg from 'assets/svgs/CoinIllustrationSvg';
 import ActivityHistoryIllustrationSvg from 'assets/svgs/ActivityHistoryIllustrationSvg';
+import URLs from 'constants/URLs';
+import { colors } from 'constants/styleGuide';
 
 import styles from './IntroScreen.styles';
 
 export default function IntroScreen() {
+  const [acceptedTerms, setAcceptedTerms] = useState(false);
+
   const navigation = useNavigation();
+
+  const dispatch = useDispatch();
 
   const slides = [
     {
@@ -34,7 +45,7 @@ export default function IntroScreen() {
       description: i18next.t(
         'Transfer your tokens easily to other accounts, by simply scanning QR Codes.'
       ),
-      body: <AddressIllustrationSvg />,
+      body: <CoinIllustrationSvg />,
     },
     {
       id: 3,
@@ -52,10 +63,14 @@ export default function IntroScreen() {
     },
   ];
 
-  const renderFooter = ({ index, handleGoNextIndex, dataCount }) => {
+  const handleTermsAndConditionsClick = () => Linking.openURL(URLs.liskTermsAndConditions);
+
+  const renderFooter = ({ index, handleGoNextIndex, handleGoToLastIndex, dataCount }) => {
     const isLastSlide = index === dataCount - 1;
 
     const handleSkipClick = () => {
+      dispatch(settingsUpdatedAction({ showedIntro: true }));
+
       AsyncStorage.setItem('@lisk-mobile-intro', 'true');
       navigation.push('AuthMethod', { signOut: true });
     };
@@ -66,15 +81,39 @@ export default function IntroScreen() {
           Next
         </PrimaryButton>
 
-        <Button onClick={handleSkipClick}>Skip</Button>
+        <Button onClick={handleGoToLastIndex}>Skip</Button>
       </>
     );
 
     if (isLastSlide) {
       children = (
-        <PrimaryButton onClick={handleSkipClick} style={{ marginBottom: 16 }}>
-          Continue to add account
-        </PrimaryButton>
+        <>
+          <View style={styles.switchContainer}>
+            <Switch
+              height={26}
+              width={43}
+              onSyncPress={(status) => setAcceptedTerms(status)}
+              backgroundActive={colors.light.ultramarineBlue}
+              backgroundInactive={colors.light.platinum}
+              testID="sliderButton"
+            />
+            <P style={styles.confirmationText}>
+              {i18next.t('I have read and agreed with the')}
+
+              <A onPress={handleTermsAndConditionsClick} style={styles.link}>
+                &nbsp;{i18next.t('terms and conditions.')}
+              </A>
+            </P>
+          </View>
+
+          <PrimaryButton
+            onClick={handleSkipClick}
+            disabled={!acceptedTerms}
+            style={{ marginBottom: 16 }}
+          >
+            Continue to add account
+          </PrimaryButton>
+        </>
       );
     }
 
