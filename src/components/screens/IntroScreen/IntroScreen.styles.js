893e3c2236e3f7c462d1cfae680c8f9b52c49a52
ClementeSerrano <clementeserranosutil@gmail.com>
2023-02-01 13:54:25 +0100

    :sparkles: Add accept terms to intro screen

--- src/components/screens/IntroScreen/IntroScreen.styles.js
+++ src/components/screens/IntroScreen/IntroScreen.styles.js
@@ -1,6 +1,9 @@
 import { StyleSheet } from 'react-native';
 
 import { colors, boxes } from 'constants/styleGuide';
+import { deviceHeight, SCREEN_HEIGHTS } from 'utilities/device';
+
+const isSmallScreen = deviceHeight() < SCREEN_HEIGHTS.SM;
 
 const styles = {
   container: {
@@ -10,6 +13,22 @@ const styles = {
   },
   footer: {
     padding: boxes.boxPadding,
+  },
+  switchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  confirmationText: {
+    marginLeft: 13.5,
+    flex: 1,
+    color: colors.light.blueGray,
+    flexWrap: 'wrap',
+    fontSize: isSmallScreen ? 11 : 13,
+  },
+  link: {
+    color: colors.light.ultramarineBlue,
+    fontSize: isSmallScreen ? 11 : 13,
   },
 };
 
