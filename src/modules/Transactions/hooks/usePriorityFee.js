ee4037c0b42ef739e02fcc0ea3d09fbc18510c8c
ClementeSerrano <clementeserranosutil@gmail.com>
2023-02-08 14:31:21 +0100

    :memo: Add docs for usePriorityFee

--- src/modules/Transactions/hooks/usePriorityFee.js
+++ src/modules/Transactions/hooks/usePriorityFee.js
@@ -1,5 +1,9 @@
 import { useTransactionFeeEstimateQuery } from '../api/useTransactionFeeEstimateQuery';
 
+/**
+ * Gets the priority fees estimate from the network based on demand.
+ * @returns {Object} Query result state: data, isLoading, isError, among others.
+ */
 export function usePriorityFee() {
   const { data: transactionFeeEstimateData, ...queryResult } = useTransactionFeeEstimateQuery();
 
