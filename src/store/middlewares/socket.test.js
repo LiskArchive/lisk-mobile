4f3d5fe82691a4c817e25d880956f41d5a64fe1d
ClementeSerrano <clementeserranosutil@gmail.com>
2023-02-21 14:01:17 +0100

    :rotating_light: Fix linting issues on redux socket tests

--- src/store/middlewares/socket.test.js
+++ src/store/middlewares/socket.test.js
@@ -1,28 +1,11 @@
-import { INITIAL_STATE as settings } from 'modules/Settings/reducer';
-import { tokenKeys } from 'constants/tokens';
 import socketMiddleware from './socket';
 
 describe('Middleware: Accounts', () => {
-  const account = {
-    address: '1234567890L',
-    publicKey: 'sample_public_Key_A',
-    balance: 10000000,
-  };
   const next = jest.fn();
-  const store = {
-    dispatch: jest.fn(),
-    getState: () => ({
-      accounts: {
-        info: { [tokenKeys[0]]: account },
-        followed: [],
-      },
-      settings,
-    }),
-  };
 
   it('should pass the action', () => {
     const action = { type: 'ANY_ACTION' };
-    socketMiddleware(store)(next)(action);
+    socketMiddleware()(next)(action);
     expect(next).toBeCalledWith(action);
   });
 });
