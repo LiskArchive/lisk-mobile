a5a7278e861f57b0628bbb7a10ec84653094f85c
ClementeSerrano <clementeserranosutil@gmail.com>
2023-03-13 11:39:11 +0100

    :rotating_light: Fix linting error on unnecessary isTransactionSuccess validation

--- src/modules/SendToken/hooks/useSendTokenForm.js
+++ src/modules/SendToken/hooks/useSendTokenForm.js
@@ -186,7 +186,6 @@ export default function useSendTokenForm({ transaction, isTransactionSuccess, in
     }
 
     if (
-      isTransactionSuccess &&
       isCrossChainTransfer &&
       messageFeeData?.data?.fee &&
       recipientApplicationChainChannelData?.data?.messageFeeTokenID
