0027a7e3b03771a9f693c3eaea528b8b385f797a
ClementeSerrano <clementeserranosutil@gmail.com>
2023-03-13 14:36:59 +0100

    :necktie: Update APIClient instance on current application change

--- src/modules/BlockchainApplication/hooks/useCurrentApplication.js
+++ src/modules/BlockchainApplication/hooks/useCurrentApplication.js
@@ -1,7 +1,14 @@
+import apiClient from 'utilities/api/APIClient';
 import { useApplications } from '../context/ApplicationsContext';
 
 export function useCurrentApplication() {
   const { currentApplication } = useApplications();
 
-  return [currentApplication, currentApplication.setData];
+  const handleSetData = (selectedApplication) => {
+    apiClient.create(selectedApplication.serviceURL);
+
+    currentApplication.setData(selectedApplication);
+  };
+
+  return [currentApplication, handleSetData];
 }
