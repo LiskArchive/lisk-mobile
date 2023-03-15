24f4c68003ac847afd2cbc1ccc58227ff031d81a
Adepoju Daniel <adebalanced02@gmail.com>
2023-03-15 13:19:48 +0000

    Fix encrypting issue on IOS (#1678)
    * :bug: Fix encrypting issue on iOS
    
    * :nail_care: Cleanup package.json to allow encryption on iOS

--- shim.js
+++ shim.js
@@ -24,7 +24,7 @@ if (typeof process === 'undefined') {
 }
 
 if (!global.WebAssembly) {
-  global.WebAssembly = require('react-native-wasm');
+  require('react-native-wasm');
 }
 
 for (var p in env) {
