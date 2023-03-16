c2b0072bee311568c375f90551b362d4de31a0a2
Adepoju Daniel <adebalanced02@gmail.com>
2023-03-16 20:19:50 +0000

    :white_check_mark: added test for validateDerivationpath function

--- src/modules/Accounts/utils/accounts.utils.test.js
+++ src/modules/Accounts/utils/accounts.utils.test.js
@@ -1,7 +1,8 @@
 import apiClient from 'utilities/api/APIClient';
 import { mockTokens, mockTokensMeta, mockTokensFullData } from 'modules/Transactions/__fixtures__';
 
-import { addTokensMetaData } from './accounts.utils';
+import { defaultDerivationPath } from 'utilities/explicitBipKeyDerivation';
+import { addTokensMetaData, validateDerivationPath } from './accounts.utils';
 
 jest.spyOn(apiClient, 'call').mockImplementation(() => Promise.resolve({ data: mockTokensMeta }));
 
@@ -17,6 +18,15 @@ describe('accounts utils', () => {
       const tokensFullData = await addTokensMetaData([]);
 
       expect(tokensFullData).toStrictEqual([]);
+    });
+  });
+
+  describe('validateDerivationPath util', () => {
+    it('should throw error for invalid derivation path', () => {
+      expect(validateDerivationPath('invalid-path')).toStrictEqual('Invalid path format');
+    });
+    it('should return undefined for valid derivation path', () => {
+      expect(validateDerivationPath(defaultDerivationPath)).toBeUndefined();
     });
   });
 });
