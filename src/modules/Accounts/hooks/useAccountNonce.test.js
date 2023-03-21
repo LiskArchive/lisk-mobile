12f6c9b1b78d09bde4bdade7a9457ef97e95e994
ClementeSerrano <clementeserranosutil@gmail.com>
2023-03-21 13:30:27 +0100

    :white_check_mark: Update useAccountNonce UT

--- src/modules/Accounts/hooks/useAccountNonce.test.js
+++ src/modules/Accounts/hooks/useAccountNonce.test.js
@@ -1,109 +1,122 @@
 import { renderHook } from '@testing-library/react-hooks';
+import { act } from 'react-test-renderer';
 
 import { useAuthQuery } from 'modules/Auth/api/useAuthQuery';
 import { useAccountPoolTransactionsQuery } from '../api/useAccountPoolTransactionsQuery';
+import { mockSavedAccounts } from '../__fixtures__';
 import { useAccountNonce } from './useAccountNonce';
 
 jest.mock('modules/Auth/api/useAuthQuery');
 jest.mock('../api/useAccountPoolTransactionsQuery');
 
 describe('useAccountNonce', () => {
+  const address = mockSavedAccounts[0].metadata.address;
+  const onChainNonce = 42;
+  const poolMaxNonce = 43;
+
   const mockAuthQueryResult = {
-    data: { data: { nonce: '1' } },
+    data: { data: { nonce: onChainNonce.toString() } },
     isLoading: false,
     isSuccess: true,
     isError: false,
     error: null,
+    refetch: jest.fn(),
   };
 
   const mockPoolTransactionsQueryResult = {
-    data: { data: [{ nonce: '2' }, { nonce: '3' }] },
+    data: { data: [{ nonce: poolMaxNonce.toString() }] },
     isLoading: false,
     isSuccess: true,
     isError: false,
     error: null,
+    refetch: jest.fn(),
   };
 
-  it('returns the on-chain nonce if there are no transactions in the pool', async () => {
-    useAuthQuery.mockReturnValueOnce(mockAuthQueryResult);
+  beforeEach(() => {
+    useAuthQuery.mockReturnValue(mockAuthQueryResult);
+    useAccountPoolTransactionsQuery.mockReturnValue(mockPoolTransactionsQueryResult);
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should compute the nonce from on-chain and pool data', () => {
+    const { result } = renderHook(() => useAccountNonce(address));
+
+    expect(result.current.data).toBe(poolMaxNonce.toString());
+  });
+
+  it('should fallback to on-chain nonce if pool data is empty', () => {
     useAccountPoolTransactionsQuery.mockReturnValueOnce({
       ...mockPoolTransactionsQueryResult,
       data: { data: [] },
     });
 
-    const { result } = renderHook(() => useAccountNonce('0x1234567890abcdef'));
+    const { result } = renderHook(() => useAccountNonce(address));
 
-    expect(result.current).toEqual({
-      data: 1,
-      isLoading: false,
-      isSuccess: true,
-      isError: false,
-      error: null,
-    });
+    expect(result.current.data).toBe(onChainNonce.toString());
   });
 
-  it('returns the maximum nonce in the pool if there are transactions in the pool', async () => {
-    useAuthQuery.mockReturnValueOnce(mockAuthQueryResult);
-    useAccountPoolTransactionsQuery.mockReturnValueOnce(mockPoolTransactionsQueryResult);
-
-    const { result } = renderHook(() => useAccountNonce('0x1234567890abcdef'));
-
-    expect(result.current).toEqual({
-      data: 3,
-      isLoading: false,
-      isSuccess: true,
-      isError: false,
-      error: null,
-    });
-  });
-
-  it('returns null if the on-chain nonce and transaction pool are both empty', async () => {
+  it('should return null if both on-chain and pool data are empty', () => {
     useAuthQuery.mockReturnValueOnce({
       ...mockAuthQueryResult,
       data: null,
     });
+
     useAccountPoolTransactionsQuery.mockReturnValueOnce({
       ...mockPoolTransactionsQueryResult,
       data: { data: [] },
     });
 
-    const { result } = renderHook(() => useAccountNonce('0x1234567890abcdef'));
+    const { result } = renderHook(() => useAccountNonce(address));
 
-    expect(result.current).toEqual({
-      data: null,
-      isLoading: false,
-      isSuccess: true,
-      isError: false,
-      error: null,
-    });
+    expect(result.current.data).toBeNull();
   });
 
-  it('returns an error object if either query encounters an error', async () => {
-    const mockError = new Error('Something went wrong');
-
+  it('should return loading state while fetching data', () => {
     useAuthQuery.mockReturnValueOnce({
       ...mockAuthQueryResult,
-      data: null,
-      isError: true,
-      isSuccess: false,
-      error: mockError,
+      isLoading: true,
     });
-    useAccountPoolTransactionsQuery.mockReturnValueOnce({
+
+    const { result } = renderHook(() => useAccountNonce(address));
+
+    expect(result.current.isLoading).toBe(true);
+  });
+
+  it('should return error state if one of the queries has an error', () => {
+    useAuthQuery.mockReturnValueOnce({
+      ...mockAuthQueryResult,
+      isError: true,
+      error: new Error('Auth query error'),
+    });
+
+    const { result } = renderHook(() => useAccountNonce(address));
+
+    expect(result.current.isError).toBe(true);
+    expect(result.current.error).toBeDefined();
+  });
+
+  it('should return the correct new data on refetch', async () => {
+    const newData = '44';
+
+    mockAuthQueryResult.refetch.mockResolvedValueOnce({
+      ...mockAuthQueryResult,
+      data: { data: { nonce: '3' } },
+    });
+
+    mockPoolTransactionsQueryResult.refetch.mockResolvedValueOnce({
       ...mockPoolTransactionsQueryResult,
-      data: null,
-      isError: true,
-      isSuccess: false,
-      error: mockError,
+      data: { data: [{ nonce: newData }] },
     });
 
-    const { result } = renderHook(() => useAccountNonce('0x1234567890abcdef'));
+    const { result } = renderHook(() => useAccountNonce(address));
 
-    expect(result.current).toEqual({
-      data: null,
-      isLoading: false,
-      isSuccess: false,
-      isError: true,
-      error: mockError,
+    await act(async () => {
+      const updatedData = await result.current.refetch();
+
+      expect(updatedData).toEqual(newData);
     });
   });
 });
