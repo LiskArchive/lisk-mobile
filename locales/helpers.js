1e49f355b94bf0de348c0d514ed484da1cc52c44
ClementeSerrano <clementeserranosutil@gmail.com>
2023-03-23 16:30:28 +0100

    :necktie: Add locales/helpers to reuse the 't' helper function

---
+++ locales/helpers.js
@@ -0,0 +1,11 @@
+/**
+ * Dummy function to help i18n scanner understand i18next translations.
+ * We can remove this as soon as react-navigation supports i18n or
+ * we change the router to another lib with i18n support.
+ *
+ * @param {String} str - String to translate.
+ * @returns {String} Provided input string.
+ */
+export function t(str) {
+  return str;
+}
--- src/modules/Transactions/constants.js
+++ src/modules/Transactions/constants.js
@@ -1,14 +1,4 @@
-/**
- * Since react-navigation doesn't support i18n
- * I've created this dummy function to help i18n scanner
- * understand about these titles.
- * We can remove this as soon as react-navigation supports i18n or
- * we change the router to another lib with i18n support.
- *
- * @param {String} str
- * @returns {String} same as the input string
- */
-const t = (str) => str;
+import { t } from 'locales/helpers';
 
 const MODULES = {
   token: 'token',
