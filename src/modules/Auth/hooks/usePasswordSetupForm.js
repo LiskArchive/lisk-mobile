80b289622ab5c7282a71835429883bdcdf2b9ef4
ClementeSerrano <clementeserranosutil@gmail.com>
2023-03-24 14:53:26 +0100

    :children_crossing: Validate setup password form on change
    This way we show users validation error before they submit the form

--- src/modules/Auth/hooks/usePasswordSetupForm.js
+++ src/modules/Auth/hooks/usePasswordSetupForm.js
@@ -64,6 +64,7 @@ export function usePasswordSetupForm(passphrase, derivationPath) {
       isAgreed: false,
     },
     resolver: yupResolver(validationSchema),
+    mode: 'all',
   });
 
   const handleSubmit = baseHandleSubmit(async (values) => {
