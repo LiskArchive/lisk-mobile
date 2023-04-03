f28f1d338a7289e583b06bf902fb32cf67df8ec9
ClementeSerrano <clementeserranosutil@gmail.com>
2023-04-03 12:32:14 +0200

    :necktie: Add fromDisplayToBaseDenom and fromLskToBeddows helpers

--- src/utilities/conversions.utils.js
+++ src/utilities/conversions.utils.js
@@ -28,11 +28,11 @@ export const includeFee = (value, fee, asRawLsk = false) => {
 
 /**
  * Parses from base-denomination to display-denomination an amount of a given token.
- * @param {BigInt} amount - Amount to convert (in base denomination).
- * @param {String} displayDenom - Display denomination ID.
- * @param {Array.<{denom: String, decimals: Number}>} denomUnits - Denominations equivalence configs.
- * @param {String} symbol - Symbol of the token. Is added as suffix in the result if the param withSymbol=true.
- * @param {Boolean} withSymbol - Flag that indicates if the response should or not include the symbol of the
+ * @param {string} amount - Amount to convert (in base denomination).
+ * @param {string} displayDenom - Display denomination ID.
+ * @param {Array.<{denom: string, decimals: number}>} denomUnits - Denominations equivalence configs.
+ * @param {string} symbol - Symbol of the token. Is added as suffix in the result if the param withSymbol=true.
+ * @param {boolean} withSymbol - Flag that indicates if the response should or not include the symbol of the
  * token (optional, default: false).
  * @returns {string} Converted value in string format.
  */
@@ -65,11 +65,36 @@ export function fromBaseToDisplayDenom({
 }
 
 /**
- * Parses from base-denomination to display-denomination an amount of LSK.
- * @param {BigInt} amount - Amount to convert (in beddows).
- * @param {Boolean} withSymbol - Flag that indicates if the response should or not include the symbol of the
- * token (optional, default: false).
+ * Parses from display-denomination to base-denomination an amount of a given token.
+ * @param {string} amount - Amount to convert (in display denomination).
+ * @param {string} displayDenom - Display denomination ID.
+ * @param {Array.<{denom: string, decimals: number}>} denomUnits - Denominations equivalence configs.
  * @returns {string} Converted value in string format.
+ */
+export function fromDisplayToBaseDenom({ amount, displayDenom, denomUnits }) {
+  const bigAmount = new BigNumber(amount);
+
+  const conversionUnit = denomUnits.find((unit) => unit.denom === displayDenom);
+
+  if (!conversionUnit) {
+    throw new Error('Display denomination not found on units.');
+  }
+
+  const conversionDecimals = conversionUnit.decimals;
+
+  const conversionFactor = new BigNumber(10).pow(conversionDecimals);
+
+  const convertedAmount = bigAmount.multipliedBy(conversionFactor).toFixed();
+
+  return convertedAmount;
+}
+
+/**
+ * Parses from base-denomination to display-denomination an amount of LSK.
+ * @param {string} amount - Amount to convert (in beddows).
+ * @param {boolean} withSymbol - Flag that indicates if the response should or not include the symbol of the
+ * token (optional, default: false).
+ * @returns {string} Converted value in LSK.
  */
 export function fromBeddowsToLsk(amount, withSymbol = false) {
   return fromBaseToDisplayDenom({
@@ -78,5 +103,18 @@ export function fromBeddowsToLsk(amount, withSymbol = false) {
     denomUnits: [{ decimals: 8, denom: 'lsk' }],
     symbol: 'LSK',
     withSymbol,
+  });
+}
+
+/**
+ * Parses from display-denomination to base-denomination an amount of LSK.
+ * @param {string} amount - Amount to convert (in LSK).
+ * @returns {string} Converted value in beddows.
+ */
+export function fromLskToBeddows(amount) {
+  return fromDisplayToBaseDenom({
+    amount,
+    displayDenom: 'lsk',
+    denomUnits: [{ decimals: 8, denom: 'lsk' }],
   });
 }
