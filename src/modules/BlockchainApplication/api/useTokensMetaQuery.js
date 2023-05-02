bfa06e25c9f7faec66cc6adbe5d3a6a2ab69837a
ClementeSerrano <clementeserranosutil@gmail.com>
2023-05-02 15:45:51 +0200

    :necktie: Remove network param from token queries when ID is provided

--- src/modules/BlockchainApplication/api/useTokensMetaQuery.js
+++ src/modules/BlockchainApplication/api/useTokensMetaQuery.js
@@ -1,6 +1,6 @@
 import { useCustomInfiniteQuery } from 'utilities/api/hooks/useCustomInfiniteQuery';
 import { GET_TOKENS_METADATA_QUERY } from 'utilities/api/queries';
-import { LIMIT, API_URL, NETWORK } from 'utilities/api/constants';
+import { LIMIT, API_URL } from 'utilities/api/constants';
 import { useQueryKeys } from 'utilities/api/hooks/useQueryKeys';
 import liskAPIClient from 'utilities/api/LiskAPIClient';
 
@@ -10,7 +10,7 @@ export function getTokensMetaQueryConfig(customConfig = {}) {
     method: 'get',
     event: 'get.blockchain.apps.meta.tokens',
     ...customConfig,
-    params: { network: NETWORK, limit: LIMIT, ...customConfig.params },
+    params: { limit: LIMIT, ...customConfig.params },
   };
 }
 
