564c83d1b48ebe522b7b23701e227e9b7d7027ce
ClementeSerrano <clementeserranosutil@gmail.com>
2023-05-12 15:56:50 +0200

    :necktie: Add isTransactionAmountValid helper

--- src/utilities/validators.test.js
+++ src/utilities/validators.test.js
@@ -1,4 +1,4 @@
-import { validateAddress } from './validators';
+import { validateAddress, isTransactionAmountValid } from './validators';
 
 describe('Address validator', () => {
   it('returns -1 if no address is passed', () => {
@@ -11,5 +11,24 @@ describe('Address validator', () => {
 
   it('returns 1 for in-valid  lsk address', () => {
     expect(validateAddress('13746538517771550559')).toBe(1);
+  });
+});
+
+describe('isTransactionAmountValid', () => {
+  it('returns true for valid transaction amounts', () => {
+    expect(isTransactionAmountValid('123')).toBe(true);
+    expect(isTransactionAmountValid('123.456')).toBe(true);
+  });
+
+  it('returns false for transaction amounts with invalid format', () => {
+    expect(isTransactionAmountValid('123.')).toBe(false);
+    expect(isTransactionAmountValid('.456')).toBe(false);
+    expect(isTransactionAmountValid('123.456.789')).toBe(false);
+    expect(isTransactionAmountValid('abc')).toBe(false);
+  });
+
+  it('returns false for non-positive transaction amounts', () => {
+    expect(isTransactionAmountValid('-123')).toBe(false);
+    expect(isTransactionAmountValid('0')).toBe(false);
   });
 });
