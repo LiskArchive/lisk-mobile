c93af5187246ddf8df4ec89b3de1a0bb2c2759ea
ClementeSerrano <clementeserranosutil@gmail.com>
2023-05-15 15:46:04 +0200

    :necktie: Remove harcoded base tx schema from transaction class business logic

--- src/modules/Transactions/utils/constants.js
+++ src/modules/Transactions/utils/constants.js
@@ -1,46 +1,5 @@
 import { t } from 'locales/helpers';
 
-// TODO: Use from service endpoint/elements.
-// (details on https://github.com/LiskHQ/lisk-mobile/issues/1612).
-export const BASE_TRANSACTION_SCHEMA = {
-  $id: '/lisk/baseTransaction',
-  type: 'object',
-  required: ['module', 'command', 'nonce', 'fee', 'senderPublicKey', 'params'],
-  properties: {
-    module: {
-      dataType: 'string',
-      fieldNumber: 1,
-    },
-    command: {
-      dataType: 'string',
-      fieldNumber: 2,
-    },
-    nonce: {
-      dataType: 'uint64',
-      fieldNumber: 3,
-    },
-    fee: {
-      dataType: 'uint64',
-      fieldNumber: 4,
-    },
-    senderPublicKey: {
-      dataType: 'bytes',
-      fieldNumber: 5,
-    },
-    params: {
-      dataType: 'bytes',
-      fieldNumber: 6,
-    },
-    signatures: {
-      type: 'array',
-      items: {
-        dataType: 'bytes',
-      },
-      fieldNumber: 7,
-    },
-  },
-};
-
 export const PRIORITY_NAMES_MAP = {
   low: t('sendToken.tokenSelect.lowPriorityLabel'),
   medium: t('sendToken.tokenSelect.mediumPriorityLabel'),
