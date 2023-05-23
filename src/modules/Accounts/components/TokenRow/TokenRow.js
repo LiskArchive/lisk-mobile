9e6d1057f76b5ada949e56a0743295274d2c3a22
ClementeSerrano <clementeserranosutil@gmail.com>
2023-05-23 14:15:23 +0200

    :sparkles: Include reload button and update styles of token balances error fallback

--- src/modules/Accounts/components/TokenRow/TokenRow.js
+++ src/modules/Accounts/components/TokenRow/TokenRow.js
@@ -4,9 +4,7 @@ import { View, Image } from 'react-native';
 import { useSelector } from 'react-redux';
 
 import { useTheme } from 'contexts/ThemeContext';
-import { useTokenMetaQuery } from 'modules/BlockchainApplication/api/useTokenMetaQuery';
 import { useTokenAmountInCurrency } from 'modules/SendToken/components/SelectTokenStep/hooks';
-import DataRenderer from 'components/shared/DataRenderer';
 import { P, H3 } from 'components/shared/toolBox/typography';
 import DiscreteModeComponent from 'components/shared/DiscreteModeComponent';
 import { fromBaseToDisplayDenom } from 'utilities/conversions.utils';
@@ -24,21 +22,13 @@ export default function TokenRow({ token }) {
 
   const accountSettings = useSelector((state) => state.settings);
 
-  const {
-    data: tokenMetaData,
-    isLoading: isTokenMetaLoading,
-    error: errorOnTokenMeta,
-  } = useTokenMetaQuery(token.tokenID);
-
-  const tokenMeta = tokenMetaData?.data[0];
-
   const tokenAmountInCurrency = useTokenAmountInCurrency({
     tokenAmount: fromBaseToDisplayDenom({
       amount: token.availableBalance,
       displayDenom: token.displayDenom,
       denomUnits: token.denomUnits,
     }),
-    tokenSymbol: tokenMeta?.symbol,
+    tokenSymbol: token?.symbol,
   });
 
   const { styles } = useTheme({ styles: getTokenRowStyles() });
@@ -46,43 +36,27 @@ export default function TokenRow({ token }) {
   return (
     <View style={[styles.container, styles.theme.container]}>
       <View style={[styles.row, styles.alignCenter]}>
-        <DataRenderer
-          data={tokenMeta}
-          isLoading={isTokenMetaLoading}
-          error={errorOnTokenMeta}
-          renderData={(data) => (
-            <>
-              <Image source={{ uri: data.logo.png }} style={styles.logo} />
+        <Image source={{ uri: token.logo.png }} style={styles.logo} />
 
-              <P style={[styles.title, styles.theme.title]}>{data.chainName}</P>
-            </>
-          )}
-        />
+        <P style={[styles.title, styles.theme.title]}>{token.chainName}</P>
       </View>
 
       <View style={[styles.balanceContainer]}>
         <DiscreteModeComponent data={balance} blurVariant="balance">
           <H3 style={[styles.theme.title]}>
-            {balance} {tokenMeta?.symbol}
+            {balance} {token?.symbol}
           </H3>
         </DiscreteModeComponent>
 
         {tokenAmountInCurrency.amount && (
-          <DataRenderer
-            data={tokenMeta}
-            isLoading={isTokenMetaLoading}
-            error={errorOnTokenMeta}
-            renderData={() => (
-              <DiscreteModeComponent data={balance} blurVariant="balance">
-                <P
-                  style={[styles.theme.currency]}
-                  testID={`token-currency-${accountSettings.currency}`}
-                >
-                  {tokenAmountInCurrency.amount} {tokenAmountInCurrency.currency}
-                </P>
-              </DiscreteModeComponent>
-            )}
-          />
+          <DiscreteModeComponent data={balance} blurVariant="balance">
+            <P
+              style={[styles.theme.currency]}
+              testID={`token-currency-${accountSettings.currency}`}
+            >
+              {tokenAmountInCurrency.amount} {tokenAmountInCurrency.currency}
+            </P>
+          </DiscreteModeComponent>
         )}
       </View>
     </View>
