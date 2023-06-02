a636db61c10c7930c4c7e9cac1c64a7a38d1e36f
ClementeSerrano <clementeserranosutil@gmail.com>
2023-06-02 17:17:45 +0200

    :globe_with_meridians: Format translation of the about screen

--- src/components/screens/AboutScreen/AboutScreen.js
+++ src/components/screens/AboutScreen/AboutScreen.js
@@ -3,8 +3,9 @@ import { View, Linking } from 'react-native';
 import i18next from 'i18next';
 
 import { useTheme } from 'contexts/ThemeContext';
-import URLs from 'constants/URLs';
 import { H4, P, A } from 'components/shared/toolBox/typography';
+import DropDownHolder from 'utilities/alert';
+import URLs from 'constants/URLs';
 import LiskIsotypeSvg from 'assets/svgs/LiskIsotypeSvg';
 import packageJson from '../../../../package.json';
 
@@ -15,11 +16,10 @@ export default function AboutScreen() {
     styles: getStyles(),
   });
 
-  const handleOpenLiskWebsite = () => {
-    Linking.openURL(URLs.liskHomepage)
-      // eslint-disable-next-line no-console
-      .catch((err) => console.error('An error occurred', err));
-  };
+  const handleOpenLiskWebsite = () =>
+    Linking.openURL(URLs.liskHomepage).catch(() =>
+      DropDownHolder.error('Error', 'Error opening link. Please try again later.')
+    );
 
   return (
     <View style={[styles.container, styles.theme.container]} testID="about-screen">
@@ -29,10 +29,12 @@ export default function AboutScreen() {
 
       <H4 style={[styles.appTitle, styles.theme.appTitle]}>Lisk</H4>
 
-      <P style={[styles.version, styles.theme.version]}>{`Version ${packageJson.version}`}</P>
+      <P style={[styles.version, styles.theme.version]}>
+        {i18next.t('settings.about.versionDescription', { version: packageJson.version })}
+      </P>
 
       <A onPress={handleOpenLiskWebsite} style={[styles.link, styles.theme.link]}>
-        {i18next.t('Read more on the Lisk website')}
+        {i18next.t('settings.about.openWebsiteButtonTitle')}
       </A>
     </View>
   );
