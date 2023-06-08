243c161610a8fd9ea08014a32d9bebd62ef94de6
Adepoju Daniel <adebalanced02@gmail.com>
2023-06-08 22:06:55 +0100

    :bug: show error for invalid file selected

--- src/modules/Auth/utils/documentPicker.js
+++ src/modules/Auth/utils/documentPicker.js
@@ -1,10 +1,14 @@
 import DocumentPicker from 'react-native-document-picker';
 import RNFS from 'react-native-fs';
 
-export const selectEncryptedFile = async () => {
-  const file = await DocumentPicker.pickSingle({
-    type: DocumentPicker.types.allFiles,
-  });
-  const encryptedData = await RNFS.readFile(file.uri);
-  return encryptedData;
+export const selectEncryptedFile = async (onError) => {
+  try {
+    const file = await DocumentPicker.pickSingle({
+      type: DocumentPicker.types.allFiles,
+    });
+    const encryptedData = await RNFS.readFile(file.uri);
+    return encryptedData;
+  } catch (error) {
+    return onError();
+  }
 };
