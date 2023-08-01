8f345dfc865c9e4590914ce3af0aeec2d608ccab
ClementeSerrano <clementeserranosutil@gmail.com>
2023-08-01 12:20:58 +0200

    :white_check_mark: Fix broken apps UT

--- src/modules/BlockchainApplication/__fixtures__/mockDefaultApplication.js
+++ src/modules/BlockchainApplication/__fixtures__/mockDefaultApplication.js
@@ -1,45 +1,14 @@
 export const mockDefaultApplication = {
   chainName: 'lisk_mainchain',
-  displayName: 'Lisk',
   chainID: '02000000',
-  title: 'Lisk - Betanet',
-  description: 'Metadata configuration for the Lisk blockchain (mainchain) in betanet',
-  networkType: 'betanet',
-  isDefault: true,
   status: 'active',
-  genesisURL: 'https://downloads.lisk.com/lisk/betanet/genesis_block.json.tar.gz',
-  projectPage: 'https://lisk.com',
-  serviceURLs: [
+  address: 'lskguo9kqnea2zsfo3a6qppozsxsg92nuuma3p7ad',
+  lastCertificateHeight: 1460,
+  lastUpdated: 1689782730,
+  escrow: [
     {
-      http: 'https://betanet-service.lisk.com',
-      ws: 'wss://betanet-service.lisk.com',
-      apiCertificatePublicKey:
-        '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtz6qaTSCNwtGrdPJTori\n/CQCG5/ozu6IRIwAwNeB4YpYcUti3a16braLtC+9ElZ5S+S1hPS+nix3hlzuXm2z\ndpsgfHBuw16NO/9Hk1IbNMJOQD3KDY7GTeBebyBSVC9T5JEAQyTBm6elhIGvGlxZ\naQLV2vvZ2nR8T3UhECGOjtBurrCem5bLtJvg+XZXiTn27xQ1nEwUtm0rH1CJgSt7\nhVjQcAerNLaP58f1O521vgHr79UrJWLxqhjdNIccDZ4kWRCBdvnDBaKz2yI6sYhU\nD8OuPxvwXkrT7DX8GvODMOgf6NYqJIb8TD3wjoR/wWJHUG2Bc9v6eQScAFmCc/FL\n+wIDAQAB\n-----END PUBLIC KEY-----',
+      tokenID: '0400000000000000',
+      amount: '100010000000',
     },
   ],
-  logo: {
-    png: 'https://raw.githubusercontent.com/LiskHQ/app-registry/main/betanet/Lisk/images/application/lisk.png',
-    svg: 'https://raw.githubusercontent.com/LiskHQ/app-registry/main/betanet/Lisk/images/application/lisk.svg',
-  },
-  explorers: [
-    {
-      url: 'https://betanet.liskscan.com',
-      txnPage: 'https://betanet.liskscan.com/transactions',
-    },
-  ],
-  appNodes: [
-    {
-      url: 'https://betanet.lisk.com',
-      apiCertificatePublicKey:
-        '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtz6qaTSCNwtGrdPJTori\n/CQCG5/ozu6IRIwAwNeB4YpYcUti3a16braLtC+9ElZ5S+S1hPS+nix3hlzuXm2z\ndpsgfHBuw16NO/9Hk1IbNMJOQD3KDY7GTeBebyBSVC9T5JEAQyTBm6elhIGvGlxZ\naQLV2vvZ2nR8T3UhECGOjtBurrCem5bLtJvg+XZXiTn27xQ1nEwUtm0rH1CJgSt7\nhVjQcAerNLaP58f1O521vgHr79UrJWLxqhjdNIccDZ4kWRCBdvnDBaKz2yI6sYhU\nD8OuPxvwXkrT7DX8GvODMOgf6NYqJIb8TD3wjoR/wWJHUG2Bc9v6eQScAFmCc/FL\n+wIDAQAB\n-----END PUBLIC KEY-----',
-      maintainer: 'Lightcurve GmbH',
-    },
-    {
-      url: 'wss://betanet.lisk.com',
-      apiCertificatePublicKey:
-        '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtz6qaTSCNwtGrdPJTori\n/CQCG5/ozu6IRIwAwNeB4YpYcUti3a16braLtC+9ElZ5S+S1hPS+nix3hlzuXm2z\ndpsgfHBuw16NO/9Hk1IbNMJOQD3KDY7GTeBebyBSVC9T5JEAQyTBm6elhIGvGlxZ\naQLV2vvZ2nR8T3UhECGOjtBurrCem5bLtJvg+XZXiTn27xQ1nEwUtm0rH1CJgSt7\nhVjQcAerNLaP58f1O521vgHr79UrJWLxqhjdNIccDZ4kWRCBdvnDBaKz2yI6sYhU\nD8OuPxvwXkrT7DX8GvODMOgf6NYqJIb8TD3wjoR/wWJHUG2Bc9v6eQScAFmCc/FL\n+wIDAQAB\n-----END PUBLIC KEY-----',
-      maintainer: 'Lightcurve GmbH',
-    },
-  ],
-  backgroundColor: '#f7f9fb',
 };
