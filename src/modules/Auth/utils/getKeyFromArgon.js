3051640edbc024265c4539969c8c83a499858e9e
Daniel Ayomide <adebalanced02@gmail.com>
2023-08-25 10:28:30 +0100

    :bug: Fix encryption issue on iOS

--- src/modules/Auth/utils/getKeyFromArgon.js
+++ src/modules/Auth/utils/getKeyFromArgon.js
@@ -2,14 +2,13 @@ import { NativeModules } from 'react-native';
 
 const { RNArgon2 } = NativeModules;
 
-// Define the function
 export const getKeyFromPasswordWithArgon2 = async (options) => {
-  const { parallelism, password, iterations } = options;
+  const { parallelism, password } = options;
   const salt = options.salt.toString('hex');
 
   const result = await RNArgon2.argon2(password, salt, {
-    iterations,
-    memory: 64 * 1024,
+    iterations: 3,
+    memory: 65536,
     parallelism,
     hashLength: 32,
     mode: 'argon2id',
