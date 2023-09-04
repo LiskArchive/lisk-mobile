ac6c6a4d558c0ad9503096f82e9d2465b4cd53ba
ClementeSerrano <clementeserranosutil@gmail.com>
2023-09-04 12:57:35 +0200

    :necktie: Add RecoveryPhraseSecurityAdviceCard component and integrate it with all CopyRecoveryPhraseToClipboard

--- src/modules/Auth/components/RegisterForm/RecoveryPhraseSafeKeepingScreen/RecoveryPhraseSafeKeepingScreen.styles.js
+++ src/modules/Auth/components/RegisterForm/RecoveryPhraseSafeKeepingScreen/RecoveryPhraseSafeKeepingScreen.styles.js
@@ -62,6 +62,9 @@ export default function getRegisterSafeKeepingStyles() {
         color: colors.light.blueGray,
         fontSize: fonts.size.input,
       },
+      securityAdviceCard: {
+        marginBottom: 32,
+      },
     },
     [themes.light]: {
       container: {
