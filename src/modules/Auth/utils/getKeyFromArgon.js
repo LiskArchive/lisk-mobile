c2f9076fa6d27150b7094077171a5f9f2c5a9f28
Daniel Ayomide <adebalanced02@gmail.com>
2023-09-07 15:02:32 +0100

    :arrow-up: #1996 - Bump lisk client version

--- src/modules/Auth/utils/getKeyFromArgon.js
+++ src/modules/Auth/utils/getKeyFromArgon.js
@@ -3,14 +3,14 @@ import { NativeModules } from 'react-native';
 const { RNArgon2 } = NativeModules;
 
 export const getKeyFromPasswordWithArgon2 = async (options) => {
-  const { parallelism, password } = options;
+  const { parallelism, password, iterations, memory, hashLength } = options;
   const salt = options.salt.toString('hex');
 
   const result = await RNArgon2.argon2(password, salt, {
-    iterations: 3,
-    memory: 65536,
+    iterations,
+    memory,
     parallelism,
-    hashLength: 32,
+    hashLength,
     mode: 'argon2id',
   });
   const key = Buffer.from(result.rawHash, 'hex');
