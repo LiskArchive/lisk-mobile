b0efa15444fa857e842f8e8505c4dd3774500a76
ClementeSerrano <clementeserranosutil@gmail.com>
2023-10-10 12:26:55 +0200

    :globe_with_meridians: Add connectivity translations

--- src/store/middlewares/socket.js
+++ src/store/middlewares/socket.js
@@ -3,7 +3,7 @@ import NetInfo from '@react-native-community/netinfo';
 import Toast from 'react-native-toast-message';
 
 import actionTypes from 'modules/Accounts/actionTypes';
-import i18n from '../../../locales';
+import i18next from '../../../locales';
 
 /** To-Do We have to disable socket connection because of
  * Lisk core problem. we will enable socket connection
@@ -18,8 +18,8 @@ const handleConnectivityChange = (connectionInfo) => {
   if (!connectionInfo.isConnected) {
     Toast.show({
       type: 'error',
-      text1: i18n.t('No internet connection!'),
-      text2: i18n.t('Your connection seems to be down, try again later.'),
+      text1: i18next.t('commons.connectivity.noInternetConnectionTitle'),
+      text2: i18next.t('commons.connectivity.noInternetConnectionDescription'),
     });
   }
 };
