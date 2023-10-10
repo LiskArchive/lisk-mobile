2060101851222aa90f7883b4512d3305e8cad6dc
ClementeSerrano <clementeserranosutil@gmail.com>
2023-10-10 15:32:33 +0200

    :coffin: Remove auth validators dead code

--- src/modules/Auth/validators/index.js
+++ src/modules/Auth/validators/index.js
@@ -1,10 +1,3 @@
 export const passwordValidationRegex = new RegExp(
   /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/g
 );
-
-/**
- *
- * @param {string} str
- * @returns boolean
- */
-export const passwordValidator = (str = '') => passwordValidationRegex.test(str);
