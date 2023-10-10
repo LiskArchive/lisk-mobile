fece9c0e3de8f3920bc7caf0a2023b619302cff5
ClementeSerrano <clementeserranosutil@gmail.com>
2023-10-10 15:07:27 +0200

    :coffin: Remove dead shared components

--- src/components/shared/tabBarIcon/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Icon from 'components/shared/toolBox/icon';
-
-const TabBarIcon = ({ name, color, focused }) => (
-  <Icon size={20} name={focused ? `${name}-filled` : name} color={color} />
-);
-
-export default TabBarIcon;
