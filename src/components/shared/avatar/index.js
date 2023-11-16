65b71632ea7fe89ded80ba5cc307095498cba48a
ClementeSerrano <clementeserranosutil@gmail.com>
2023-11-16 16:18:35 +0100

    :recycle: Refactor avatar component to rerender on props change and use hooks

--- src/components/shared/avatar/index.js
+++ src/components/shared/avatar/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-statements */
 import React from 'react';
 import { View, Animated } from 'react-native';
 import Svg, { G } from 'react-native-svg';
@@ -13,112 +14,90 @@ import {
   replaceUrlByHashOnScheme,
 } from './utils';
 import Icon from '../toolBox/icon';
-import withTheme from '../withTheme';
+import { useTheme } from 'contexts/ThemeContext';
+
 import getStyles from './styles';
 
-class Avatar extends React.Component {
-  shouldComponentUpdate(nextProps) {
-    return nextProps.address !== this.props.address;
-  }
+export default function Avatar({ address, size, scale, translate, style }) {
+  const { styles, theme } = useTheme({ styles: getStyles() });
+  const uniqueSvgUrlHash = randomId();
 
-  // eslint-disable-next-line camelcase
-  UNSAFE_componentWillMount() {
-    this.uniqueSvgUrlHash = randomId();
-  }
+  let Wrapper = View;
+  const scaleAttr = {};
 
-  // eslint-disable-next-line max-statements
-  render() {
-    const { styles, address, size, scale, translate, theme } = this.props;
-
-    let Wrapper = View;
-
-    const scaleAttr = {};
-    if (scale) {
-      Wrapper = Animated.View;
-      scaleAttr.transform = [
-        { scaleX: scale },
-        { scaleY: scale },
-        { translateY: translate },
-        { translateX: translate },
-      ];
-    }
-    if (validateAddress(address) !== 0) {
-      return (
-        <Wrapper
-          style={[
-            styles.figure,
-            this.props.style,
-            { width: size, borderRadius: size / 2 },
-            scaleAttr,
-          ]}
-        >
-          <Icon
-            style={[styles.avatar, { width: '100%', height: '100%' }]}
-            name="avatar-placeholder"
-            size={size}
-            color={theme === themes.light ? colors.light.blueGray : colors.dark.mountainMist}
-          />
-        </Wrapper>
-      );
-    }
-    const canvasSize = 200;
-
-    const addressHashChunks = getHashChunks(address);
-    const gradientScheme =
-      gradientSchemes[addressHashChunks[0].substring(1, 3) % gradientSchemes.length];
-
-    const gradientsSchemesUrlsHashed = {
-      primary: gradientScheme.primary.map((...rest) =>
-        replaceUrlByHashOnScheme(this.uniqueSvgUrlHash, ...rest)
-      ),
-      secondary: gradientScheme.secondary.map((...rest) =>
-        replaceUrlByHashOnScheme(this.uniqueSvgUrlHash, ...rest)
-      ),
-    };
-    const primaryGradients = pickTwo(addressHashChunks[1], gradientsSchemesUrlsHashed.primary);
-    const secondaryGradients = pickTwo(addressHashChunks[2], gradientsSchemesUrlsHashed.secondary);
-    const shapes = [
-      getBackgroundCircle(canvasSize, primaryGradients[0]),
-      getShape(addressHashChunks[1], canvasSize, primaryGradients[1], 1),
-      getShape(addressHashChunks[2], canvasSize, secondaryGradients[0], 0.23),
-      getShape(addressHashChunks[3], canvasSize, secondaryGradients[1], 0.18),
+  if (scale) {
+    Wrapper = Animated.View;
+    scaleAttr.transform = [
+      { scaleX: scale },
+      { scaleY: scale },
+      { translateY: translate },
+      { translateX: translate },
     ];
+  }
+
+  if (validateAddress(address) !== 0) {
     return (
-      <Wrapper
-        style={[
-          styles.figure,
-          this.props.style,
-          { width: size, borderRadius: size / 2 },
-          scaleAttr,
-        ]}
-      >
-        <Svg
-          viewBox={`0 0 ${canvasSize} ${canvasSize}`}
-          preserveAspectRatio="none"
-          height={size}
-          width={size}
-          style={[
-            styles.avatar,
-            {
-              width: '100%',
-              height: '100%',
-              borderRadius: size / 2,
-            },
-          ]}
-        >
-          <Gradients scheme={gradientsSchemesUrlsHashed} />
-          <G>
-            {shapes.map((shape, i) => (
-              <shape.component
-                {...shape.props}
-                key={`${i}-${shape.component.displayName}-${Math.random()}`}
-              />
-            ))}
-          </G>
-        </Svg>
+      <Wrapper style={[styles.figure, style, { width: size, borderRadius: size / 2 }, scaleAttr]}>
+        <Icon
+          style={[styles.avatar, { width: '100%', height: '100%' }]}
+          name="avatar-placeholder"
+          size={size}
+          color={theme === themes.light ? colors.light.blueGray : colors.dark.mountainMist}
+        />
       </Wrapper>
     );
   }
-}
 
-export default withTheme(Avatar, getStyles());
+  const canvasSize = 200;
+  const addressHashChunks = getHashChunks(address);
+  const gradientScheme =
+    gradientSchemes[addressHashChunks[0].substring(1, 3) % gradientSchemes.length];
+
+  const gradientsSchemesUrlsHashed = {
+    primary: gradientScheme.primary.map((...rest) =>
+      replaceUrlByHashOnScheme(uniqueSvgUrlHash, ...rest)
+    ),
+    secondary: gradientScheme.secondary.map((...rest) =>
+      replaceUrlByHashOnScheme(uniqueSvgUrlHash, ...rest)
+    ),
+  };
+
+  const primaryGradients = pickTwo(addressHashChunks[1], gradientsSchemesUrlsHashed.primary);
+  const secondaryGradients = pickTwo(addressHashChunks[2], gradientsSchemesUrlsHashed.secondary);
+
+  const shapes = [
+    getBackgroundCircle(canvasSize, primaryGradients[0]),
+    getShape(addressHashChunks[1], canvasSize, primaryGradients[1], 1),
+    getShape(addressHashChunks[2], canvasSize, secondaryGradients[0], 0.23),
+    getShape(addressHashChunks[3], canvasSize, secondaryGradients[1], 0.18),
+  ];
+
+  return (
+    <Wrapper style={[styles.figure, style, { width: size, borderRadius: size / 2 }, scaleAttr]}>
+      <Svg
+        viewBox={`0 0 ${canvasSize} ${canvasSize}`}
+        preserveAspectRatio="none"
+        height={size}
+        width={size}
+        style={[
+          styles.avatar,
+          {
+            width: '100%',
+            height: '100%',
+            borderRadius: size / 2,
+          },
+        ]}
+      >
+        <Gradients scheme={gradientsSchemesUrlsHashed} />
+        <G>
+          {shapes.map((shape, i) => (
+            <shape.component
+              {...shape.props}
+              key={`${i}-${shape.component.displayName}-${Math.random()}`}
+            />
+          ))}
+        </G>
+      </Svg>
+    </Wrapper>
+  );
+}
