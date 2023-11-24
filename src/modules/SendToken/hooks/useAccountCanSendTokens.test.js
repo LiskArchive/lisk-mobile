a674bdedd39f927bb21e9c4937901990d3d64336
Daniel Ayomide <adebalanced02@gmail.com>
2023-11-24 09:52:36 +0000

    :nail_care: Fix failing unit test

--- src/modules/SendToken/hooks/useAccountCanSendTokens.test.js
+++ src/modules/SendToken/hooks/useAccountCanSendTokens.test.js
@@ -4,6 +4,7 @@ import { useAccountTokensQuery } from 'modules/Accounts/api/useAccountTokensQuer
 import { mockSavedAccounts } from 'modules/Accounts/__fixtures__';
 
 import { useAccountCanSendTokens } from './useAccountCanSendTokens';
+import { applicationsWrapper } from '../../../tests/applicationsWrapper';
 
 jest.mock('modules/Accounts/api/useAccountTokensQuery');
 
@@ -15,60 +16,66 @@ describe('useAccountCanSendTokens', () => {
   const mockedAddress = mockSavedAccounts[0].metadata.address;
 
   it('should return loading state when account tokens query is loading', () => {
+    const wrapper = ({ children }) => applicationsWrapper({ children });
+
     useAccountTokensQuery.mockReturnValue({
       isLoading: true,
       data: null,
       isError: false,
     });
-    const { result } = renderHook(() => useAccountCanSendTokens(mockedAddress));
+    const { result } = renderHook(() => useAccountCanSendTokens(mockedAddress), { wrapper });
     expect(result.current.isLoading).toBe(true);
     expect(result.current.data).toBeUndefined();
     expect(result.current.isError).toBe(false);
   });
 
   it('should return error state when account tokens query returns an error', () => {
+    const wrapper = ({ children }) => applicationsWrapper({ children });
     useAccountTokensQuery.mockReturnValue({
       isLoading: false,
       data: null,
       isError: true,
     });
-    const { result } = renderHook(() => useAccountCanSendTokens(mockedAddress));
+    const { result } = renderHook(() => useAccountCanSendTokens(mockedAddress), { wrapper });
     expect(result.current.isLoading).toBe(false);
     expect(result.current.data).toBeUndefined();
     expect(result.current.isError).toBe(true);
   });
 
   it('should return false when account tokens query returns empty data array', () => {
+    const wrapper = ({ children }) => applicationsWrapper({ children });
     useAccountTokensQuery.mockReturnValue({
       isLoading: false,
       data: { data: [] },
       isError: false,
     });
-    const { result } = renderHook(() => useAccountCanSendTokens(mockedAddress));
+    const { result } = renderHook(() => useAccountCanSendTokens(mockedAddress), { wrapper });
     expect(result.current.isLoading).toBe(false);
     expect(result.current.data).toBe(false);
     expect(result.current.isError).toBe(false);
   });
 
   it('should return false when all account tokens have no available balance', () => {
+    const wrapper = ({ children }) => applicationsWrapper({ children });
     useAccountTokensQuery.mockReturnValue({
       isLoading: false,
       data: { data: [{ availableBalance: 0 }] },
       isError: false,
     });
-    const { result } = renderHook(() => useAccountCanSendTokens(mockedAddress));
+    const { result } = renderHook(() => useAccountCanSendTokens(mockedAddress), { wrapper });
     expect(result.current.isLoading).toBe(false);
     expect(result.current.data).toBe(false);
     expect(result.current.isError).toBe(false);
   });
 
   it('should return true when at least one account token has available balance', () => {
+    const wrapper = ({ children }) => applicationsWrapper({ children });
     useAccountTokensQuery.mockReturnValue({
       isLoading: false,
       data: { data: [{ availableBalance: 0 }, { availableBalance: 100 }] },
       isError: false,
     });
-    const { result } = renderHook(() => useAccountCanSendTokens(mockedAddress));
+    const { result } = renderHook(() => useAccountCanSendTokens(mockedAddress), { wrapper });
     expect(result.current.isLoading).toBe(false);
     expect(result.current.data).toBe(true);
     expect(result.current.isError).toBe(false);
