a15b3054c4c3c8dd0db3a02543aa532ba98b2c35
Daniel Ayomide <adebalanced02@gmail.com>
2023-11-27 12:58:57 +0000

    :bug: Stop app from picking the https route, so it's redirected to browser

--- src/navigation/navigation.linking.js
+++ src/navigation/navigation.linking.js
@@ -11,7 +11,7 @@ const navigationLinkingConfig = {
 };
 
 const navigationLinking = {
-  prefixes: ['lisk://', 'https://lisk.com'],
+  prefixes: ['lisk://'],
   config: navigationLinkingConfig,
   getStateFromPath: getNavigationStateFromPath,
   getPathFromState,
