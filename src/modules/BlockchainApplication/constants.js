828b91c0bc9dcc5c8baf8167b4cbb5abcf3ce673
ClementeSerrano <clementeserranosutil@gmail.com>
2023-12-07 14:15:44 +0100

    :rewind: Revert conditional app storage key calculation based on mainnet or not

--- src/modules/BlockchainApplication/constants.js
+++ src/modules/BlockchainApplication/constants.js
@@ -1,11 +1,6 @@
 import { NETWORK } from 'utilities/api/constants';
 
-const BASE_APPLICATIONS_STORAGE_KEY = '@blockchainApplications';
-
-export const APPLICATIONS_STORAGE_KEY =
-  NETWORK === 'mainnet'
-    ? BASE_APPLICATIONS_STORAGE_KEY
-    : `${BASE_APPLICATIONS_STORAGE_KEY}-${NETWORK}`;
+export const APPLICATIONS_STORAGE_KEY = `@blockchainApplications-${NETWORK}`;
 
 export const PINNED_APPLICATIONS_STORAGE_KEY = '@blockchainPinnedApplications';
 
